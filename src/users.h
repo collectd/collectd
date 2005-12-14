cef4d8e02560b1a750f81bca94748a7e7faa49e0
niki <niki>
2005-12-14 13:48:25 +0000

    autoconf info page mentions that including config.h has to be done this way. maybe we should start including common.h instead and do all fancy #if/#endif there.

--- src/users.h
+++ src/users.h
@@ -23,7 +23,9 @@
 #ifndef USERS_H
 #define USERS_H 1
 
-#include "config.h"
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
 
 #if !defined(HAVE_UTMPX_H) || !defined(HAVE_GETUTXENT)
 #undef HAVE_UTMPX_H
