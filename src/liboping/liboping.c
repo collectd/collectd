c5e84aabf023d1e7e8bdba97717e046c91b7b338
octo <octo>
2006-05-09 09:14:10 +0000

    octo@huhu:~/collectd $ svn merge -r717:718 trunk tags/collectd-3.9.2

--- src/liboping/liboping.c
+++ src/liboping/liboping.c
@@ -213,7 +213,7 @@ static pinghost_t *ping_receive_ipv4 (pinghost_t *ph, char *buffer, size_t buffe
 	for (ptr = ph; ptr != NULL; ptr = ptr->next)
 	{
 		dprintf ("hostname = %s, ident = 0x%04x, seq = %i\n",
-				ptr->hostname, ptr->ident, ptr->sequence - 1);
+				ptr->hostname, ptr->ident, ((ptr->sequence - 1) & 0xFFFF));
 
 		if (ptr->addrfamily != AF_INET)
 			continue;
@@ -224,7 +224,7 @@ static pinghost_t *ping_receive_ipv4 (pinghost_t *ph, char *buffer, size_t buffe
 		if (ptr->ident != ident)
 			continue;
 
-		if ((ptr->sequence - 1) != seq)
+		if (((ptr->sequence - 1) & 0xFFFF) != seq)
 			continue;
 
 		dprintf ("Match found: hostname = %s, ident = 0x%04x, seq = %i\n",
@@ -276,7 +276,7 @@ static pinghost_t *ping_receive_ipv6 (pinghost_t *ph, char *buffer, size_t buffe
 	for (ptr = ph; ptr != NULL; ptr = ptr->next)
 	{
 		dprintf ("hostname = %s, ident = 0x%04x, seq = %i\n",
-				ptr->hostname, ptr->ident, ptr->sequence - 1);
+				ptr->hostname, ptr->ident, ((ptr->sequence - 1) & 0xFFFF));
 
 		if (ptr->addrfamily != AF_INET6)
 			continue;
@@ -287,7 +287,7 @@ static pinghost_t *ping_receive_ipv6 (pinghost_t *ph, char *buffer, size_t buffe
 		if (ptr->ident != ident)
 			continue;
 
-		if ((ptr->sequence - 1) != seq)
+		if (((ptr->sequence - 1) & 0xFFFF) != seq)
 			continue;
 
 		dprintf ("Match found: hostname = %s, ident = 0x%04x, seq = %i\n",
