a073c1d52cc236c497f1a38c06433a70a7155616
Florian Forster <octo@leeloo.lan.home.verplant.org>
2006-06-25 10:50:41 +0200

    Applied all the changes that `git-svnimport' missed.

--- src/liboping/liboping.c
+++ src/liboping/liboping.c
@@ -720,8 +720,7 @@ pingobj_t *ping_construct (void)
 
 	if ((obj = (pingobj_t *) malloc (sizeof (pingobj_t))) == NULL)
 		return (NULL);
-
-	obj->head = NULL;
+	memset (obj, '\0', sizeof (pingobj_t));
 
 	obj->timeout    = PING_DEF_TIMEOUT;
 	obj->ttl        = PING_DEF_TTL;
