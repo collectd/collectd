f0c44c295d16ab49ba6e7e36d8ae9aae78e460f4
Florian Forster <octo@huhu.verplant.org>
2007-09-11 11:14:56 +0200

    apcups plugin: Fix the types used in the plugin.
    They still had an `apcups_' prefix which isn't legal anymore.

--- src/apcups.c
+++ src/apcups.c
@@ -387,14 +387,14 @@ static void apc_submit_generic (char *type, char *type_inst, double value)
 
 static void apc_submit (struct apc_detail_s *apcups_detail)
 {
-	apc_submit_generic ("apcups_voltage",    "input",   apcups_detail->linev);
-	apc_submit_generic ("apcups_voltage",    "output",  apcups_detail->outputv);
-	apc_submit_generic ("apcups_voltage",    "battery", apcups_detail->battv);
-	apc_submit_generic ("apcups_charge",     "",        apcups_detail->bcharge);
-	apc_submit_generic ("apcups_charge_pct", "",        apcups_detail->loadpct);
-	apc_submit_generic ("apcups_timeleft",   "",        apcups_detail->timeleft);
-	apc_submit_generic ("apcups_temp",       "",        apcups_detail->itemp);
-	apc_submit_generic ("apcups_frequency",  "input",   apcups_detail->linefreq);
+	apc_submit_generic ("voltage",    "input",   apcups_detail->linev);
+	apc_submit_generic ("voltage",    "output",  apcups_detail->outputv);
+	apc_submit_generic ("voltage",    "battery", apcups_detail->battv);
+	apc_submit_generic ("charge",     "",        apcups_detail->bcharge);
+	apc_submit_generic ("percent",    "charge",  apcups_detail->loadpct);
+	apc_submit_generic ("timeleft",   "",        apcups_detail->timeleft);
+	apc_submit_generic ("temperature", "",       apcups_detail->itemp);
+	apc_submit_generic ("frequency",  "input",   apcups_detail->linefreq);
 }
 
 static int apcups_read (void)
