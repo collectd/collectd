3b915093d8cbc7738b2de59f6ebc38f195131002
Luke Heberling <collectd@c-ware.com>
2007-12-13 08:09:16 +0100

    src/utils_avltree.c: Fix avl_get to work as documented.
    The documentation in the header file for avl_get states that value may be null,
    but the code in the source file asserts otherwise. This patch changes the code
    to reflect the documentation.
    
    Signed-off-by: Florian Forster <octo@huhu.verplant.org>

--- src/utils_avltree.c
+++ src/utils_avltree.c
@@ -581,13 +581,12 @@ int avl_get (avl_tree_t *t, const void *key, void **value)
 {
 	avl_node_t *n;
 
-	assert (value != NULL);
-
 	n = search (t, key);
 	if (n == NULL)
 		return (-1);
 
-	*value = n->value;
+	if (value != NULL)
+		*value = n->value;
 
 	return (0);
 }
