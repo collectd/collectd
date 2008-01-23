61d05a9c6e0f8151d099898bb698d6a53c0e92b0
Florian Forster <octo@leeloo.lan.home.verplant.org>
2008-01-23 17:32:56 +0100

    logfile plugin: Only print the host field (and other fields) of a notification if they hold any information.

--- src/logfile.c
+++ src/logfile.c
@@ -151,20 +151,41 @@ static void logfile_log (int severity, const char *msg)
 	logfile_print (msg, time (NULL));
 } /* void logfile_log (int, const char *) */
 
-int logfile_notification (const notification_t *n)
+static int logfile_notification (const notification_t *n)
 {
-	char msg[1024] = "";
+	char  buf[1024] = "";
+	char *buf_ptr = buf;
+	int   buf_len = sizeof (buf);
 	int status;
 
-	status = snprintf (msg, sizeof (msg), "Notification: %s: message = %s, "
-			"host = %s",
+	status = snprintf (buf_ptr, buf_len, "Notification: severity = %s",
 			(n->severity == NOTIF_FAILURE) ? "FAILURE"
 			: ((n->severity == NOTIF_WARNING) ? "WARNING"
-				: ((n->severity == NOTIF_OKAY) ? "OKAY" : "UNKNOWN")),
-			n->message, n->host);
-	msg[sizeof (msg) - 1] = '\0';
+				: ((n->severity == NOTIF_OKAY) ? "OKAY" : "UNKNOWN")));
+	if (status > 0)
+	{
+		buf_ptr += status;
+		buf_len -= status;
+	}
 
-	logfile_print (msg, n->time);
+#define APPEND(bufptr, buflen, key, value) \
+	if ((buflen > 0) && (strlen (value) > 0)) { \
+		int status = snprintf (bufptr, buflen, ", %s = %s", key, value); \
+		if (status > 0) { \
+			bufptr += status; \
+			buflen -= status; \
+		} \
+	}
+	APPEND (buf_ptr, buf_len, "host", n->host);
+	APPEND (buf_ptr, buf_len, "plugin", n->plugin);
+	APPEND (buf_ptr, buf_len, "plugin_instance", n->plugin_instance);
+	APPEND (buf_ptr, buf_len, "type", n->type);
+	APPEND (buf_ptr, buf_len, "type_instance", n->type_instance);
+	APPEND (buf_ptr, buf_len, "message", n->message);
+
+	buf[sizeof (buf) - 1] = '\0';
+
+	logfile_print (buf, n->time);
 
 	return (0);
 } /* int logfile_notification */
