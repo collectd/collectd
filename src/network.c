b98e88bb5ec95814100f90fd2c77ef0739be79c0
Florian Forster <octo@leeloo.lan.home.verplant.org>
2008-01-23 12:54:41 +0100

    src/{common,network}.[ch]: Updated copyright information.

--- src/network.c
+++ src/network.c
@@ -1,6 +1,6 @@
 /**
  * collectd - src/network.c
- * Copyright (C) 2005-2007  Florian octo Forster
+ * Copyright (C) 2005-2008  Florian octo Forster
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
