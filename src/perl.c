3693eefca257fe28e4b363ecfd39a44571f180ac
Sebastian Harl <sh@tokkee.org>
2008-04-28 16:53:57 +0200

    perl plugin: Work around a compiler warning on FreeBSD.
    On FreeBSD, PERL_SYS_INIT3 expands to some expression which triggers a "value
    computed is not used" warning by gcc. By telling the compiler to ignore this
    value, we can work around this issue. In the long run, this should imho be
    fixed in Perl though.
    
    Signed-off-by: Sebastian Harl <sh@tokkee.org>
    Signed-off-by: Florian Forster <octo@huhu.verplant.org>

--- src/perl.c
+++ src/perl.c
@@ -817,6 +817,11 @@ static int init_pi (int argc, char **argv)
 	}
 #endif /* COLLECT_DEBUG */
 
+#ifdef __FreeBSD__
+	/* On FreeBSD, PERL_SYS_INIT3 expands to some expression which
+	 * triggers a "value computed is not used" warning by gcc. */
+	(void)
+#endif
 	PERL_SYS_INIT3 (&argc, &argv, &environ);
 
 	if (NULL == (perl = perl_alloc ())) {
