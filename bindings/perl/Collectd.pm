3f55088d082ad875ce38262895f92880c8eecba4
Sebastian Harl <sh@tokkee.org>
2008-05-30 00:13:07 +0200

    Collectd.pm: Updated copyright information.
    Signed-off-by: Sebastian Harl <sh@tokkee.org>
    Signed-off-by: Florian Forster <octo@huhu.verplant.org>

--- bindings/perl/Collectd.pm
+++ bindings/perl/Collectd.pm
@@ -1,5 +1,5 @@
 # collectd - Collectd.pm
-# Copyright (C) 2007  Sebastian Harl
+# Copyright (C) 2007, 2008  Sebastian Harl
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
