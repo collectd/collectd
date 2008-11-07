60a77a6f3855f181e723aa3f4f645ea480ddd359
Florian Forster <octo@huhu.verplant.org>
2008-11-07 19:49:52 +0100

    src/utils_rrdcreate.c: srrd_create: Copy the `filename' argument.
    Some versions of librrd, for example the one in Debian Etch, don't have the
    `const' qualifier for the first (filename) argument for `rrd_create_r'. So
    we'll copy the argument first. This sucks big time, but is the only reasonable
    way to get around this.

--- src/utils_rrdcreate.c
+++ src/utils_rrdcreate.c
@@ -253,17 +253,34 @@ static int srrd_create (const char *filename, /* {{{ */
     int argc, const char **argv)
 {
   int status;
+  char *filename_copy;
+
+  if ((filename == NULL) || (argv == NULL))
+    return (-EINVAL);
+
+  /* Some versions of librrd don't have the `const' qualifier for the first
+   * argument, so we have to copy the pointer here to avoid warnings. It sucks,
+   * but what else can we do? :(  -octo */
+  filename_copy = strdup (filename);
+  if (filename_copy == NULL)
+  {
+    ERROR ("srrd_create: strdup failed.");
+    return (-ENOMEM);
+  }
 
   optind = 0; /* bug in librrd? */
   rrd_clear_error ();
 
-  status = rrd_create_r (filename, pdp_step, last_up, argc, (void *) argv);
+  status = rrd_create_r (filename_copy, pdp_step, last_up,
+      argc, (void *) argv);
 
   if (status != 0)
   {
     WARNING ("rrdtool plugin: rrd_create_r (%s) failed: %s",
         filename, rrd_get_error ());
   }
+
+  sfree (filename_copy);
 
   return (status);
 } /* }}} int srrd_create */
