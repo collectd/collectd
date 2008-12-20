f73c5b3069fd10048f1fcdf5266d8da2fcb403e5
Florian Forster <octo@leeloo.lan.home.verplant.org>
2008-12-20 16:55:10 +0100

    src/common.c: Change `sstrdup' to not call `strdup' anymore.

--- src/common.c
+++ src/common.c
@@ -77,15 +77,21 @@ int ssnprintf (char *dest, size_t n, const char *format, ...)
 char *sstrdup (const char *s)
 {
 	char *r;
+	size_t sz;
 
 	if (s == NULL)
 		return (NULL);
 
-	if((r = strdup (s)) == NULL)
+	/* Do not use `strdup' here, because it's not specified in POSIX. It's
+	 * ``only'' an XSI extension. */
+	sz = strlen (s) + 1;
+	r = (char *) malloc (sizeof (char) * sz);
+	if (r == NULL)
 	{
-		ERROR ("Not enough memory.");
+		ERROR ("sstrdup: Out of memory.");
 		exit (3);
 	}
+	memcpy (r, s, sizeof (char) * sz);
 
 	return (r);
 } /* char *sstrdup */
