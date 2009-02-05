9b38f5f412d6b33cd0e5be812d830f09bef72647
Florian Forster <octo@leeloo.lan.home.verplant.org>
2009-02-05 20:21:20 +0100

    src/utils_db_query.[ch]: Allow a callback function to be passed to udb_query_create.
    This allows plugins to handle custom options in the <Query> blocks.

--- src/oracle.c
+++ src/oracle.c
@@ -294,7 +294,8 @@ static int o_config (oconfig_item_t *ci) /* {{{ */
   {
     oconfig_item_t *child = ci->children + i;
     if (strcasecmp ("Query", child->key) == 0)
-      udb_query_create (&queries, &queries_num, child);
+      udb_query_create (&queries, &queries_num, child,
+          /* callback = */ NULL);
     else if (strcasecmp ("Database", child->key) == 0)
       o_config_add_database (child);
     else
