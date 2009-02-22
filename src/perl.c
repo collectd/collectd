3f4dd2401e02446d17df6023933e92e494bfd869
Sebastian Harl <sh@tokkee.org>
2009-02-22 22:53:50 +0100

    Updated various copyright information.

--- src/perl.c
+++ src/perl.c
@@ -1,6 +1,6 @@
 /**
  * collectd - src/perl.c
- * Copyright (C) 2007, 2008  Sebastian Harl
+ * Copyright (C) 2007-2009  Sebastian Harl
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
