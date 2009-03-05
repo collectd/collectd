1cf13b782cb290af46afdc5fc7f8eae59b0b1188
Florian Forster <octo@huhu.verplant.org>
2009-03-05 09:41:35 +0100

    contrib/collection3: Fix stacking with NAN values.
    Stacked values above NAN values were not drawn previously..

--- contrib/collection3/lib/Collectd/Graph/Type/GenericStacked.pm
+++ contrib/collection3/lib/Collectd/Graph/Type/GenericStacked.pm
@@ -1,5 +1,20 @@
 package Collectd::Graph::Type::GenericStacked;
 
+# Copyright (C) 2008,2009  Florian octo Forster <octo at verplant.org>
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; only version 2 of the License is applicable.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 use strict;
 use warnings;
 use base ('Collectd::Graph::Type');
@@ -116,13 +131,13 @@ sub getRRDArgs
     if ($i == (@$idents - 1))
     {
       push (@ret,
-	"CDEF:cdef${i}=avg${i}");
+	"CDEF:cdef${i}=avg${i},UN,0,avg${i},IF");
     }
     else
     {
       my $j = $i + 1;
       push (@ret,
-	"CDEF:cdef${i}=cdef${j},avg${i},+");
+	"CDEF:cdef${i}=avg${i},UN,0,avg${i},IF,cdef${j},+");
     }
   }
 
