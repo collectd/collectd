cc5c9640e4e5b766473e227d38ee24c123d9bea6
Sebastian Harl <sh@tokkee.org>
2009-03-10 20:48:54 +0100

    configfile, types_list: Improved error messages when handling "TypesDB".

--- src/configfile.c
+++ src/configfile.c
@@ -205,13 +205,18 @@ static int dispatch_value_typesdb (const oconfig_item_t *ci)
 
 	cf_default_typesdb = 0;
 
-	if (ci->values_num < 1)
+	if (ci->values_num < 1) {
+		ERROR ("configfile: `TypesDB' needs at least one argument.");
 		return (-1);
+	}
 
 	for (i = 0; i < ci->values_num; ++i)
 	{
-		if (OCONFIG_TYPE_STRING != ci->values[i].type)
+		if (OCONFIG_TYPE_STRING != ci->values[i].type) {
+			WARNING ("configfile: TypesDB: Skipping %i. argument which "
+					"is not a string.", i + 1);
 			continue;
+		}
 
 		read_types_list (ci->values[i].value.string);
 	}
