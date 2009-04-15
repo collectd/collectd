7c028a4a47f8f4b341cca30a193265f4f0d7ff98
Florian Forster <octo@leeloo.lan.home.verplant.org>
2009-04-15 10:46:23 +0200

    apache plugin: Fix whitespace: Indent using tab, remove trailing.

--- src/apache.c
+++ src/apache.c
@@ -119,19 +119,19 @@ static size_t apache_curl_callback (void *buf, size_t size, size_t nmemb,
  *   </Instance>
  *   URL ...
  * </Plugin>
- */ 
+ */
 static int config_set_string (char **ret_string,
 				    oconfig_item_t *ci)
 {
 	char *string;
 
 	if ((ci->values_num != 1)
-            || (ci->values[0].type != OCONFIG_TYPE_STRING))
-        {
-                WARNING ("apache plugin: The `%s' config option "
-                         "needs exactly one string argument.", ci->key);
-                return (-1);
-        }
+			|| (ci->values[0].type != OCONFIG_TYPE_STRING))
+	{
+		WARNING ("apache plugin: The `%s' config option "
+				"needs exactly one string argument.", ci->key);
+		return (-1);
+	}
 
 	string = strdup (ci->values[0].value.string);
 	if (string == NULL)
