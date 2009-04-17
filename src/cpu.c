1d4cdb75eeb13910a640520c2e3192f962045b0d
Florian Forster <octo@huhu.verplant.org>
2009-04-17 18:19:59 +0200

    cpu plugin: Updated copyright information.

--- src/cpu.c
+++ src/cpu.c
@@ -1,6 +1,7 @@
 /**
  * collectd - src/cpu.c
- * Copyright (C) 2005-2007  Florian octo Forster
+ * Copyright (C) 2005-2009  Florian octo Forster
+ * Copyright (C) 2008       Oleg King
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -17,6 +18,7 @@
  *
  * Authors:
  *   Florian octo Forster <octo at verplant.org>
+ *   Oleg King <king2 at kaluga.ru>
  **/
 
 #include "collectd.h"
