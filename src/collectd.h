9e0809db22aa694d9edad9dd74515d655dc70579
Doug MacEachern <dougm@hyperic.com>
2009-05-07 12:50:17 -0700

    configure.in: Use AC_HEADER_STDBOOL
    Signed-off-by: Doug MacEachern <dougm@hyperic.com>
    Signed-off-by: Florian Forster <octo@huhu.verplant.org>

--- src/collectd.h
+++ src/collectd.h
@@ -58,6 +58,18 @@
 #endif
 #if HAVE_STDBOOL_H
 # include <stdbool.h>
+#else
+# ifndef HAVE__BOOL
+#  ifdef __cplusplus
+typedef bool _Bool;
+#  else
+#   define _Bool signed char
+#  endif
+# endif
+# define bool _Bool
+# define false 0
+# define true 1
+# define __bool_true_false_are_defined 1
 #endif
 #if HAVE_UNISTD_H
 # include <unistd.h>
