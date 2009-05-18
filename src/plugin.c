collectd/collectd

commit 65954d9b90c905e74f174d2dacba102b6fe540c6

src/plubin.c: Call `fc_process' instead of calling the write plugins directly.

src/plugin.c
@@ -36,6 +36,7 @@
 #include "utils_llist.h"
 #include "utils_cache.h"
 #include "utils_threshold.h"
+#include "filter_chain.h"
 
 /*
  * Private structures
@@ -705,7 +706,7 @@ int plugin_write (const char *plugin, /* {{{ */
     int success = 0;
     int failure = 0;
 
-    le = llist_head (list_flush);
+    le = llist_head (list_write);
     while (le != NULL)
     {
       callback = le->value;
@@ -725,7 +726,7 @@ int plugin_write (const char *plugin, /* {{{ */
   }
   else /* plugin != NULL */
   {
-    le = llist_head (list_flush);
+    le = llist_head (list_write);
     while (le != NULL)
     {
       if (strcasecmp (plugin, le->key) == 0)
@@ -800,9 +801,6 @@ int plugin_dispatch_values (value_list_t *vl)
 	static c_complain_t no_write_complaint = C_COMPLAIN_INIT_STATIC;
 
 	data_set_t *ds;
-	llentry_t *le;
-
-	int filter = 0;
 
 	if ((vl == NULL) || (*vl->type == '\0')) {
 		ERROR ("plugin_dispatch_values: Invalid value list.");
@@ -865,39 +863,10 @@ int plugin_dispatch_values (value_list_t *vl)
 	escape_slashes (vl->type, sizeof (vl->type));
 	escape_slashes (vl->type_instance, sizeof (vl->type_instance));
 
-	le = llist_head (list_filter);
-	while (le != NULL)
-	{
-		int (*filter_callback) (const data_set_t *, value_list_t *) =
-				(int (*) (const data_set_t *, value_list_t *)) le->value;
-
-		filter |= (*filter_callback) (ds, vl);
-
-		if (filter == FILTER_IGNORE)
-			return (-1);
-
-		le = le->next;
-	}
-
 	/* Update the value cache */
 	uc_update (ds, vl);
 
-	if ((filter & FILTER_NOTHRESHOLD_CHECK) == 0)
-		ut_check_threshold (ds, vl);
-
-	if (filter & FILTER_NOWRITE)
-		return (0);
-
-	le = llist_head (list_write);
-	while (le != NULL)
-	{
-		int (*write_callback) (const data_set_t *, const value_list_t *) =
-				(int (*) (const data_set_t *, const value_list_t *)) le->value;
-
-		(*write_callback) (ds, vl);
-
-		le = le->next;
-	}
+	fc_process (ds, vl);
 
 	return (0);
 } /* int plugin_dispatch_values */

commit b235b6a85baf902d4c8e11095310f20462c103a2

src/plugin.c: Initiate the threshold checking again.
Hi,

I have the same problem in my instalation, exactly the same scenario, I found a
possible explanation in the plugin.c module. While in the 4.5 branch exists a
callback to function ut_check_threshold (defined in utils_threshold.c), in the
4.6.2 (and also in 4.6.1, I think), the callback is missing, so threshold
checking never runs.

In fact, a grep -r ut_check_threshold over src dir, only show the definition of
the function in ut_check_threshold module.

I'm not sure if it's really a bug, but when I patched my code, it works fine
for me :) I'm using the 4.6.2 version from tar.gz.

Best regards,
  Andrés

The regression was introduced in 65954d9b.

Signed-off-by: Florian Forster <octo@huhu.verplant.org>

src/plugin.c
@@ -961,6 +961,9 @@ int plugin_dispatch_values (value_list_t *vl)
 	/* Update the value cache */
 	uc_update (ds, vl);
 
+	/* Initiate threshold checking */
+	ut_check_threshold (ds, vl);
+
 	if (post_cache_chain != NULL)
 	{
 		status = fc_process_chain (ds, vl, post_cache_chain);