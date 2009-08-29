acf568ce4d90d78d63eed0fe74390fdd3348c1c8
Florian Forster <octo@leeloo.lan.home.verplant.org>
2009-08-29 15:25:23 +0200

    zfs_arc plugin: Fix a small typo.

--- src/zfs_arc.c
+++ src/zfs_arc.c
@@ -29,7 +29,7 @@
 static kstat_t *ksp;
 extern kstat_ctl_t *kc;
 
-static void za_submit (const char* type, const char* type_instance, value_t values[], int values_len)
+static void za_submit (const char* type, const char* type_instance, value_t* values, int values_len)
 {
 	value_list_t vl = VALUE_LIST_INIT;
 
@@ -39,7 +39,7 @@ static void za_submit (const char* type, const char* type_instance, value_t valu
 	sstrncpy (vl.host, hostname_g, sizeof (vl.host));
 	sstrncpy (vl.plugin, "zfs_arc", sizeof (vl.plugin));
 	sstrncpy (vl.type, type, sizeof (vl.type));
-	sstrncpy (vl.type_instance, type_instance, sizeof (vl.type));
+	sstrncpy (vl.type_instance, type_instance, sizeof (vl.type_instance));
 
 	plugin_dispatch_values (&vl);
 }
