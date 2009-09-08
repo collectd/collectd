4f628540dda92235841ff070ada671de03d8e962
Florian Forster <octo@huhu.verplant.org>
2009-09-08 17:36:55 +0200

    scale target: Fix copyright information.

--- src/target_scale.c
+++ src/target_scale.c
@@ -1,6 +1,6 @@
 /**
- * collectd - src/target_set.c
- * Copyright (C) 2008  Florian Forster
+ * collectd - src/target_scale.c
+ * Copyright (C) 2008-2009  Florian Forster
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
