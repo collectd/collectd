7f71f1b7a38d7b50af44f403b79aa034b3882f2a
Florian Forster <octo@leeloo.lan.home.verplant.org>
2009-09-14 16:01:48 +0200

    exec plugin: Avoid a warning when freeing meta data.

--- src/exec.c
+++ src/exec.c
@@ -730,7 +730,8 @@ static void *exec_notification_one (void *arg) /* {{{ */
   DEBUG ("exec plugin: Child %i exited with status %i.",
       pid, status);
 
-  plugin_notification_meta_free (n->meta);
+  if (n->meta != NULL)
+    plugin_notification_meta_free (n->meta);
   n->meta = NULL;
   sfree (arg);
   pthread_exit ((void *) 0);
