2c15d340059c210c86441b840c82bfd0ea8d26d7
Florian Forster <octo@leeloo.lan.home.verplant.org>
2009-12-08 11:59:22 +0100

    src/cpython.h: Apparently Python 2.3 doesn't have the Py_RETURN_NONE macro.

--- src/cpython.h
+++ src/cpython.h
@@ -43,6 +43,11 @@
 } while (0)
 #endif
 
+/* Python 2.4 has this macro, older versions do not. */
+#ifndef Py_RETURN_NONE
+# define Py_RETURN_NONE return Py_INCREF(Py_None), Py_None
+#endif
+
 typedef struct {
 	PyObject_HEAD        /* No semicolon! */
 	PyObject *parent;    /* Config */
