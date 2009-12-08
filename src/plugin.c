47f2b4a92d3390e27ce2fa6f6bd1340c9cb0491b
Florian Forster <octo@leeloo.lan.home.verplant.org>
2009-12-08 10:12:58 +0100

    src/plugin.c: Add libtool 1 compatibility code.

--- src/plugin.c
+++ src/plugin.c
@@ -280,6 +280,7 @@ static int plugin_load_file (char *file, uint32_t flags)
 	lt_dlinit ();
 	lt_dlerror (); /* clear errors */
 
+#if LIBTOOL_VERSION == 2
 	if (flags & PLUGIN_FLAGS_GLOBAL) {
 		lt_dladvise advise;
 		lt_dladvise_init(&advise);
@@ -289,6 +290,13 @@ static int plugin_load_file (char *file, uint32_t flags)
 	} else {
         	dlh = lt_dlopen (file);
 	}
+#else /* if LIBTOOL_VERSION == 1 */
+	if (flags & PLUGIN_FLAGS_GLOBAL)
+		ERROR ("plugin_load_file: The global flag is not supported, "
+				"libtool 2 is required for this.");
+	dlh = lt_dlopen (file);
+#endif
+
 	if (dlh == NULL)
 	{
 		const char *error = lt_dlerror ();
