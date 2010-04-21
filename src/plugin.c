a841b5f2882acf431f4fd0783f602f3ae80716c5
Florian Forster <octo@noris.net>
2010-04-21 16:23:46 +0200

    src/plugin.c: plugin_log: Append newline to message …
    … even when no log-plugin was loaded.

--- src/plugin.c
+++ src/plugin.c
@@ -1459,14 +1459,6 @@ void plugin_log (int level, const char *format, ...)
 	va_list ap;
 	llentry_t *le;
 
-	if (list_log == NULL)
-	{
-		va_start (ap, format);
-		vfprintf (stderr, format, ap);
-		va_end (ap);
-		return;
-	}
-
 #if !COLLECT_DEBUG
 	if (level >= LOG_DEBUG)
 		return;
@@ -1476,6 +1468,12 @@ void plugin_log (int level, const char *format, ...)
 	vsnprintf (msg, sizeof (msg), format, ap);
 	msg[sizeof (msg) - 1] = '\0';
 	va_end (ap);
+
+	if (list_log == NULL)
+	{
+		fprintf (stderr, "%s\n", msg);
+		return;
+	}
 
 	le = llist_head (list_log);
 	while (le != NULL)
