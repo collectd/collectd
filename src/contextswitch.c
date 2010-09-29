5faee1b80e2718c7c12801439af1a6a6729c0b8d
Florian Forster <octo@collectd.org>
2010-09-29 17:04:20 +0200

    contextswitch plugin: Add copyright and author information.

--- src/contextswitch.c
+++ src/contextswitch.c
@@ -1,6 +1,7 @@
 /**
  * collectd - src/contextswitch.c
  * Copyright (C) 2009  Patrik Weiskircher
+ * Copyright (C) 2010  Kimo Rosenbaum
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -17,6 +18,7 @@
  *
  * Authors:
  *   Patrik Weiskircher <weiskircher at inqnet.at>
+ *   Kimo Rosenbaum <http://github.com/kimor79>
  **/
 
 #include "collectd.h"
