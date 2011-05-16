36d3cd820b5f977fcd349250a22a932f5af5a062
Sebastian Harl <sh@tokkee.org>
2011-05-16 15:08:41 +0200

    collectd-nagios: Sort LISTVAL output.
    Also, output the data for each host in a separate "section", printing the
    hostname as the heading and leaving it out from any further output.
    
    This way, the strings may directly be used as options for -H and -n.

--- src/collectd-nagios.c
+++ src/collectd-nagios.c
@@ -274,10 +274,20 @@ static int do_listval (lcc_connection_t *connection)
 	lcc_identifier_t *ret_ident = NULL;
 	size_t ret_ident_num = 0;
 
+	char *hostname = NULL;
+
 	int status;
 	size_t i;
 
 	status = lcc_listval (connection, &ret_ident, &ret_ident_num);
+	if (status != 0) {
+		printf ("UNKNOWN: %s\n", lcc_strerror (connection));
+		if (ret_ident != NULL)
+			free (ret_ident);
+		return (RET_UNKNOWN);
+	}
+
+	status = lcc_sort_identifiers (connection, ret_ident, ret_ident_num);
 	if (status != 0) {
 		printf ("UNKNOWN: %s\n", lcc_strerror (connection));
 		if (ret_ident != NULL)
@@ -291,6 +301,17 @@ static int do_listval (lcc_connection_t *connection)
 		if ((hostname_g != NULL) && (strcasecmp (hostname_g, ret_ident[i].host)))
 			continue;
 
+		if ((hostname == NULL) || strcasecmp (hostname, ret_ident[i].host))
+		{
+			if (hostname != NULL)
+				free (hostname);
+			hostname = strdup (ret_ident[i].host);
+			printf ("Host: %s\n", hostname);
+		}
+
+		/* empty hostname; not to be printed again */
+		ret_ident[i].host[0] = '\0';
+
 		status = lcc_identifier_to_string (connection,
 				id, sizeof (id), ret_ident + i);
 		if (status != 0) {
@@ -300,7 +321,8 @@ static int do_listval (lcc_connection_t *connection)
 			continue;
 		}
 
-		printf ("%s\n", id);
+		/* skip over the (empty) hostname and following '/' */
+		printf ("\t%s\n", id + 1);
 	}
 
 	if (ret_ident != NULL)
