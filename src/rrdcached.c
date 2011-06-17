5caecf5ba53ad31fa0626591c1ccc91fb47af955
Sebastian Harl <sh@tokkee.org>
2011-06-17 10:29:13 +0200

    rrdcached plugin: Handle boolean config options as well.
    This was lost previously when changing to the complex config callback.

--- src/rrdcached.c
+++ src/rrdcached.c
@@ -164,13 +164,20 @@ static int value_list_to_filename (char *buffer, int buffer_len,
 static const char *config_get_string (oconfig_item_t *ci)
 {
   if ((ci->children_num != 0) || (ci->values_num != 1)
-      || (ci->values[0].type != OCONFIG_TYPE_STRING))
+      || ((ci->values[0].type != OCONFIG_TYPE_STRING)
+        && (ci->values[0].type != OCONFIG_TYPE_BOOLEAN)))
   {
     ERROR ("rrdcached plugin: %s expects a single string argument.",
         ci->key);
     return (NULL);
   }
 
+  if (ci->values[0].type == OCONFIG_TYPE_BOOLEAN) {
+    if (ci->values[0].value.boolean)
+      return "true";
+    else
+      return "false";
+  }
   return (ci->values[0].value.string);
 } /* const char *config_get_string */
 
