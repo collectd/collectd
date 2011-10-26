45bd61ea2b32000fcfeafebf89527d99a6446be9
Fabien Wernli <cpan@faxm0dem.org>
2011-10-26 11:22:46 +0200

    Add notifications to syslog plugin
    Deduplicated some code from logfile and syslog
    and added NotifyLevel option to plugin.
    
    Change-Id: I364067189d628420333cb625c885a256399e076a
    Signed-off-by: Florian Forster <octo@collectd.org>

--- src/plugin.h
+++ src/plugin.h
@@ -325,6 +325,8 @@ int plugin_dispatch_notification (const notification_t *notif);
 
 void plugin_log (int level, const char *format, ...)
 	__attribute__ ((format(printf,2,3)));
+int parse_log_severity (const char *severity);
+int parse_notif_severity (const char *severity);
 
 #define ERROR(...)   plugin_log (LOG_ERR,     __VA_ARGS__)
 #define WARNING(...) plugin_log (LOG_WARNING, __VA_ARGS__)
