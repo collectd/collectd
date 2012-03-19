aa004cbb1c4edd490ffd8a28b54f43598e9f583f
Teet Talviste <teet.talviste@elion.ee>
2012-03-19 16:50:29 +0200

    Add rrd create options to rrdcached plugin

--- src/rrdcached.c
+++ src/rrdcached.c
@@ -37,16 +37,16 @@ static int config_create_files = 1;
 static int config_collect_stats = 1;
 static rrdcreate_config_t rrdcreate_config =
 {
-	/* stepsize = */ 0,
-	/* heartbeat = */ 0,
-	/* rrarows = */ 1200,
-	/* xff = */ 0.1,
+  /* stepsize = */ 0,
+  /* heartbeat = */ 0,
+  /* rrarows = */ 1200,
+  /* xff = */ 0.1,
 
-	/* timespans = */ NULL,
-	/* timespans_num = */ 0,
+  /* timespans = */ NULL,
+  /* timespans_num = */ 0,
 
-	/* consolidation_functions = */ NULL,
-	/* consolidation_functions_num = */ 0
+  /* consolidation_functions = */ NULL,
+  /* consolidation_functions_num = */ 0
 };
 
 /*
@@ -79,8 +79,8 @@ static int value_list_to_string (char *buffer, int buffer_len,
   {
     if ((ds->ds[i].type != DS_TYPE_COUNTER)
         && (ds->ds[i].type != DS_TYPE_GAUGE)
-	&& (ds->ds[i].type != DS_TYPE_DERIVE)
-	&& (ds->ds[i].type != DS_TYPE_ABSOLUTE))
+        && (ds->ds[i].type != DS_TYPE_DERIVE)
+        && (ds->ds[i].type != DS_TYPE_ABSOLUTE))
       return (-1);
 
     if (ds->ds[i].type == DS_TYPE_COUNTER)
@@ -95,12 +95,12 @@ static int value_list_to_string (char *buffer, int buffer_len,
     }
     else if (ds->ds[i].type == DS_TYPE_DERIVE) {
       status = ssnprintf (buffer + offset, buffer_len - offset,
-	  ":%"PRIi64, vl->values[i].derive);
+          ":%"PRIi64, vl->values[i].derive);
     }
     else /* if (ds->ds[i].type == DS_TYPE_ABSOLUTE) */ {
       status = ssnprintf (buffer + offset, buffer_len - offset,
-	  ":%"PRIu64, vl->values[i].absolute);
- 
+          ":%"PRIu64, vl->values[i].absolute);
+
     }
 
     if ((status < 1) || (status >= (buffer_len - offset)))
@@ -181,6 +181,19 @@ static const char *config_get_string (oconfig_item_t *ci)
   return (ci->values[0].value.string);
 } /* const char *config_get_string */
 
+static int rrd_compare_numeric (const void *a_ptr, const void *b_ptr)
+{
+        int a = *((int *) a_ptr);
+        int b = *((int *) b_ptr);
+
+        if (a < b)
+                return (-1);
+        else if (a > b)
+                return (1);
+        else
+                return (0);
+} /* int rrd_compare_numeric */
+
 static int rc_config (oconfig_item_t *ci)
 {
   int i;
@@ -236,6 +249,83 @@ static int rc_config (oconfig_item_t *ci)
       else
         config_collect_stats = 1;
     }
+    else if (strcasecmp ("StepSize", key) == 0)
+	{
+		unsigned long temp = strtoul (value, NULL, 0);
+		if (temp > 0)
+			rrdcreate_config.stepsize = temp;
+	}
+	else if (strcasecmp ("HeartBeat", key) == 0)
+	{
+		int temp = atoi (value);
+		if (temp > 0)
+			rrdcreate_config.heartbeat = temp;
+	}
+	else if (strcasecmp ("RRARows", key) == 0)
+	{
+		int tmp = atoi (value);
+		if (tmp <= 0)
+		{
+			fprintf (stderr, "rrdtool: `RRARows' must "
+					"be greater than 0.\n");
+			ERROR ("rrdtool: `RRARows' must "
+					"be greater than 0.\n");
+			return (1);
+		}
+		rrdcreate_config.rrarows = tmp;
+	}
+	else if (strcasecmp ("RRATimespan", key) == 0)
+	{
+		char *saveptr = NULL;
+		char *dummy;
+		char *ptr;
+		char *value_copy;
+		int *tmp_alloc;
+
+		value_copy = strdup (value);
+		if (value_copy == NULL)
+			return (1);
+
+		dummy = value_copy;
+		while ((ptr = strtok_r (dummy, ", \t", &saveptr)) != NULL)
+		{
+			dummy = NULL;
+			
+			tmp_alloc = realloc (rrdcreate_config.timespans,
+					sizeof (int) * (rrdcreate_config.timespans_num + 1));
+			if (tmp_alloc == NULL)
+			{
+				fprintf (stderr, "rrdtool: realloc failed.\n");
+				ERROR ("rrdtool: realloc failed.\n");
+				free (value_copy);
+				return (1);
+			}
+			rrdcreate_config.timespans = tmp_alloc;
+			rrdcreate_config.timespans[rrdcreate_config.timespans_num] = atoi (ptr);
+			if (rrdcreate_config.timespans[rrdcreate_config.timespans_num] != 0)
+				rrdcreate_config.timespans_num++;
+		} /* while (strtok_r) */
+
+		qsort (/* base = */ rrdcreate_config.timespans,
+				/* nmemb  = */ rrdcreate_config.timespans_num,
+				/* size   = */ sizeof (rrdcreate_config.timespans[0]),
+				/* compar = */ rrd_compare_numeric);
+
+		free (value_copy);
+	}
+	else if (strcasecmp ("XFF", key) == 0)
+	{
+		double tmp = atof (value);
+		if ((tmp < 0.0) || (tmp >= 1.0))
+		{
+			fprintf (stderr, "rrdtool: `XFF' must "
+					"be in the range 0 to 1 (exclusive).");
+			ERROR ("rrdtool: `XFF' must "
+					"be in the range 0 to 1 (exclusive).");
+			return (1);
+		}
+		rrdcreate_config.xff = tmp;
+	}
     else
     {
       WARNING ("rrdcached plugin: Ignoring invalid option %s.", key);
