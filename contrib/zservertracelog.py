5ef1b4058d867ee63275f452e24f8df9fc49b04c
sirex <sirexas@gmail.com>
2012-06-15 10:34:15 +0300

    Changed zservertracelog.py script licence from GPL to ZPL.
    Licence is change to make included code licence compatible with whole script.

--- contrib/zservertracelog.py
+++ contrib/zservertracelog.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python
-
-# Script for Exec plug, that parses zservertracelog log file.
-# Copyright (C) 2012  POV <info@pov.lt>
+##############################################################################
 #
-# This file is part of collectd.
+# Copyright (c) Zope Corporation and Contributors.
+# All Rights Reserved.
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##############################################################################
 
 
 """
