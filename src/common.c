a46901fbe8d378cde0dc831270eba634c4d2b1c4
Teet Talviste <teet.talviste@gmail.com>
2012-07-23 13:25:51 +0300

    another try

--- src/common.c
+++ src/common.c
@@ -999,7 +999,7 @@ int parse_value (const char *value_orig, value_t *ret_value, int ds_type)
 
   if (value == endptr) {
     ERROR ("parse_value: Failed to parse string as %s: %lld (%llx).",
-        DS_TYPE_TO_STRING (ds_type), (long long int) value, (long long int) value);
+        DS_TYPE_TO_STRING (ds_type), (long long int) &value, (long long int) &value);
     sfree (value);
     return -1;
   }
