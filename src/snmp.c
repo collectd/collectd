77f03c91b3074b5abd3e602002b7fc7ce28ea612
Dan Thomson <dan@astutehosting.com>
2012-09-06 19:00:11 -0700

    snmp plugin: Fix error message printed by the "Shift" option.
    I noticed a small error in snmp.c in collectd-5.1.0 (might be
    elsewhere). The warning message in csnmp_config_add_data_shift says
    "Scale config option" instead of "Shift" config option:
    […]
    
    Signed-off-by: Florian Forster <octo@collectd.org>

--- src/snmp.c
+++ src/snmp.c
@@ -302,7 +302,7 @@ static int csnmp_config_add_data_shift (data_definition_t *dd, oconfig_item_t *c
   if ((ci->values_num != 1)
       || (ci->values[0].type != OCONFIG_TYPE_NUMBER))
   {
-    WARNING ("snmp plugin: The `Scale' config option needs exactly one number argument.");
+    WARNING ("snmp plugin: The `Shift' config option needs exactly one number argument.");
     return (-1);
   }
 
