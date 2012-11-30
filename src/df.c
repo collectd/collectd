0a9f89d27f73ebe89ea41759e0b17d91bed7453e
Daniel Hilst <danielhislt@gmail.com>
2012-11-30 17:20:18 -0200

    df: percentage support for free and avail blocks.

--- src/df.c
+++ src/df.c
@@ -280,6 +280,11 @@ static int df_read (void)
 				(gauge_t) (blk_reserved * blocksize));
 		df_submit_one (disk_name, "df_complex", "used",
 				(gauge_t) (blk_used * blocksize));
+                df_submit_one (disk_name, "df_complex", "free_avail_percent",
+                               (gauge_t) ((statbuf.f_bavail / (double) statbuf.f_blocks) * 100));
+                df_submit_one (disk_name, "df_complex", "free_percent",
+                               (gauge_t) ((statbuf.f_bfree / (double) statbuf.f_blocks) * 100));
+
 
 		/* inode handling */
 		if (report_inodes)
