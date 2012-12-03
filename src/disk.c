af0deea3989cac5408186233588fa330503508bf
Brad Davis <brd@FreeBSD.org>
2012-12-02 23:40:06 -0700

    - Initial attempt to make the disk plugin GEOM aware on FreeBSD

--- src/disk.c
+++ src/disk.c
@@ -73,6 +73,13 @@
 # include <libperfstat.h>
 #endif
 
+#if __FreeBSD__
+# include <sys/devicestat.h>
+# include <sys/resource.h>
+# include <devstat.h>
+# include <libgeom.h>
+#endif
+
 #if HAVE_IOKIT_IOKITLIB_H
 static mach_port_t io_master_port = MACH_PORT_NULL;
 /* This defaults to false for backwards compatibility. Please fix in the next
@@ -114,6 +121,15 @@ extern kstat_ctl_t *kc;
 static kstat_t *ksp[MAX_NUMDISK];
 static int numdisk = 0;
 /* #endif HAVE_LIBKSTAT */
+
+#elif defined(_LIBGEOM_H_)
+static struct devstat *gsp, *gsq;
+static void *sp, *sq;
+static double dt;
+static struct timespec tp, tq;
+static struct gmesh gmp;
+static struct gident *gid;
+/* #endif defined(_LIBGEOM_H_) */
 
 #elif defined(HAVE_LIBSTATGRAB)
 /* #endif HAVE_LIBKSTATGRAB */
@@ -218,7 +234,29 @@ static int disk_init (void)
 			continue;
 		ksp[numdisk++] = ksp_chain;
 	}
-#endif /* HAVE_LIBKSTAT */
+/* #endif HAVE_LIBKSTAT */
+
+#elif defined(_LIBGEOM_H_)
+	int i;
+
+	i = geom_gettree(&gmp);
+	if (i != 0) {
+		ERROR ("geom_gettree = %d", i);
+		return(-1);
+	}
+	i = geom_stats_open();
+	if (i) {
+		ERROR ("geom_stats_open()");
+		return(-1);
+	}
+	sq = NULL;
+	sq = geom_stats_snapshot_get();
+	if (sq == NULL) {
+		ERROR ("geom_stats_snapshot()");
+		return(-1);
+	};
+	geom_stats_snapshot_timestamp(sq, &tq);
+#endif /* defined(_LIBGEOM_H_) */
 
 	return (0);
 } /* int disk_init */
@@ -729,6 +767,80 @@ static int disk_read (void)
 		}
 	}
 /* #endif defined(HAVE_LIBKSTAT) */
+
+#elif defined(_LIBGEOM_H_)
+	long double ld[11];
+	uint64_t u64;
+	struct gprovider *pp;
+	int i;
+	sp = geom_stats_snapshot_get();
+	if (sp == NULL) {
+		ERROR ("geom_stats_snapshot()");
+		return(-1);
+	}
+	geom_stats_snapshot_timestamp(sp, &tp);
+	dt = tp.tv_sec - tq.tv_sec;
+	dt += (tp.tv_nsec - tq.tv_nsec) * 1e-9;
+	tq = tp;
+	
+	geom_stats_snapshot_reset(sp);
+	geom_stats_snapshot_reset(sq);
+	for (;;) {
+		gsp = geom_stats_snapshot_next(sp);
+		gsq = geom_stats_snapshot_next(sq);
+		if (gsp == NULL || gsq == NULL)
+			break;
+		if (gsp->id == NULL)
+			continue;
+		gid = geom_lookupid(&gmp, gsp->id);
+		if (gid == NULL) {
+			geom_deletetree(&gmp);
+			i = geom_gettree(&gmp);
+			if (i != 0) {
+				ERROR ("geom_gettree = %d", i);
+				return(-1);
+			}
+			gid = geom_lookupid(&gmp, gsp->id);
+		}
+		if (gid == NULL)
+			continue;
+		/* Only PROVIDER */
+		if (gid->lg_what == ISCONSUMER)
+			continue;
+		if (gsp->sequence0 != gsp->sequence1) {
+			/* I am dont understand this */
+			continue;
+		}
+		if (gid->lg_what == ISPROVIDER) {
+			pp = gid->lg_ptr;
+			if (ignorelist_match (ignorelist, pp->lg_name) != 0) {
+				continue;
+			}
+			/* Some parametres keep for future*/
+			devstat_compute_statistics(gsp, gsq, dt, 
+			    DSM_QUEUE_LENGTH, &u64,
+			    DSM_TRANSFERS_PER_SECOND, &ld[0],
+			    DSM_TRANSFERS_PER_SECOND_READ, &ld[1],
+			    DSM_MB_PER_SECOND_READ, &ld[2],
+			    DSM_MS_PER_TRANSACTION_READ, &ld[3],
+			    DSM_TRANSFERS_PER_SECOND_WRITE, &ld[4],
+			    DSM_MB_PER_SECOND_WRITE, &ld[5],
+			    DSM_MS_PER_TRANSACTION_WRITE, &ld[6],
+			    DSM_BUSY_PCT, &ld[7],
+			    DSM_TRANSFERS_PER_SECOND_FREE, &ld[8],
+			    DSM_MB_PER_SECOND_FREE, &ld[9],
+			    DSM_MS_PER_TRANSACTION_FREE, &ld[10],
+			    DSM_NONE);
+			disk_submit (pp->lg_name, "disk_ops", ld[1], ld[4]);
+			disk_submit (pp->lg_name, "disk_octets", ld[2], ld[5]);
+			disk_submit (pp->lg_name, "disk_time", ld[3], ld[6]);
+			// Hide this so I don't have to impletment submit()
+			//submit(pp->lg_name, "gdisk_busy", ld[7]);
+		}
+		*gsq = *gsp;
+	}
+	geom_stats_snapshot_free(sp);
+/* #endif defined(_LIBGEOM_H_) */
 
 #elif defined(HAVE_LIBSTATGRAB)
 	sg_disk_io_stats *ds;
