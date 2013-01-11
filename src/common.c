aaabdb7dcc46df1597930f9bf7fbfa464aa64d5d
Florian Forster <octo@collectd.org>
2013-01-11 14:56:35 +0100

    src/common.[ch]: Implement strarray_{add,free}.

--- src/common.c
+++ src/common.c
@@ -1396,3 +1396,35 @@ int strtoderive (const char *string, derive_t *ret_value) /* {{{ */
 	*ret_value = tmp;
 	return (0);
 } /* }}} int strtoderive */
+
+int strarray_add (char ***ret_array, size_t *ret_array_len, char const *str) /* {{{ */
+{
+	char **array;
+	size_t array_len = *ret_array_len;
+
+	if (str == NULL)
+		return (EINVAL);
+
+	array = realloc (*ret_array,
+            (array_len + 1) * sizeof (*array));
+	if (array == NULL)
+		return (ENOMEM);
+	*ret_array = array;
+
+	array[array_len] = strdup (str);
+	if (array[array_len] == NULL)
+		return (ENOMEM);
+
+	array_len++;
+        *ret_array_len = array_len;
+	return (0);
+} /* }}} int strarray_add */
+
+void strarray_free (char **array, size_t array_len) /* {{{ */
+{
+	size_t i;
+
+	for (i = 0; i < array_len; i++)
+		sfree (array[i]);
+	sfree (array);
+} /* }}} void strarray_free */
