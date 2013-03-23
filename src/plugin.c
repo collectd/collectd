044cb79594f94632eedf24a7bbfb8e645691a6d3
Florian Forster <octo@collectd.org>
2013-03-23 09:53:28 +0100

    src/plugin.c: Assert that "data_sets" is non-NULL.
    Otherwise we might run into an assertion in the AVL tree. This can be
    triggered e.g. when calling plugin_get_ds() from within a config
    callback.

--- src/plugin.c
+++ src/plugin.c
@@ -2016,6 +2016,12 @@ const data_set_t *plugin_get_ds (const char *name)
 {
 	data_set_t *ds;
 
+	if (data_sets == NULL)
+	{
+		ERROR ("plugin_get_ds: No data sets are defined yet.");
+		return (NULL);
+	}
+
 	if (c_avl_get (data_sets, name, (void *) &ds) != 0)
 	{
 		DEBUG ("No such dataset registered: %s", name);
