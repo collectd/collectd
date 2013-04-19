f5369db30531d559dff64eb8c901f9c99d8ec0b8
Andrés J. Díaz <ajdiaz@connectical.com>
2013-04-19 15:41:27 +0200

    Change redis collect behaviour.
    According to octo comments in:
     https://github.com/collectd/collectd/pull/296#discussion_r3771870
    
    - Remove info struct and parse info in pipeline
    - Handle DS_TYPE_ABSOLUTE and DS_TYPE_DERIVE
    - Removed dashed in plugin name
    - Some other minor changes

--- src/redis.c
+++ src/redis.c
@@ -41,6 +41,7 @@
 #define MAX_REDIS_QUERY_NAME 64
 #define MAX_REDIS_COMMAND_LEN 256
 #define MAX_REDIS_TYPE_LEN 256
+#define MAX_REDIS_VAL_SIZE 256
 
 /* Redis plugin configuration example:
  *
@@ -81,24 +82,6 @@ struct redis_node_s
   redis_query_t *query;
 
   redis_node_t *next;
-};
-
-struct redis_info_s;
-typedef struct redis_info_s redis_info_t;
-struct redis_info_s
-{
-  long uptime_in_seconds;
-  int connected_clients;
-  int connected_slaves;
-  int blocked_clients;
-  unsigned long used_memory;
-  long long changes_since_last_save;
-  long last_save_time;
-  long long total_connections_received;
-  long long total_commands_processed;
-  long long expired_keys;
-  long pubsub_channels;
-  unsigned int pubsub_patterns;
 };
 
 static redis_node_t *nodes_head = NULL;
@@ -288,39 +271,14 @@ static int redis_config (oconfig_item_t *ci) /* {{{ */
 } /* }}} */
 
   __attribute__ ((nonnull(2)))
-static void redis_submit_g (char *plugin_instance,
+static void redis_submit (char *plugin_instance,
     const char *type, const char *type_instance,
-    gauge_t value) /* {{{ */
+    value_t value) /* {{{ */
 {
   value_t values[1];
   value_list_t vl = VALUE_LIST_INIT;
 
-  values[0].gauge = value;
-
-  vl.values = values;
-  vl.values_len = 1;
-  sstrncpy (vl.host, hostname_g, sizeof (vl.host));
-  sstrncpy (vl.plugin, "redis", sizeof (vl.plugin));
-  if (plugin_instance != NULL)
-    sstrncpy (vl.plugin_instance, plugin_instance,
-        sizeof (vl.plugin_instance));
-  sstrncpy (vl.type, type, sizeof (vl.type));
-  if (type_instance != NULL)
-    sstrncpy (vl.type_instance, type_instance,
-        sizeof (vl.type_instance));
-
-  plugin_dispatch_values (&vl);
-} /* }}} */
-
-  __attribute__ ((nonnull(2)))
-static void redis_submit_d (char *plugin_instance,
-    const char *type, const char *type_instance,
-    derive_t value) /* {{{ */
-{
-  value_t values[1];
-  value_list_t vl = VALUE_LIST_INIT;
-
-  values[0].derive = value;
+  values[0] = value;
 
   vl.values = values;
   vl.values_len = 1;
@@ -353,39 +311,41 @@ static int redis_init (void) /* {{{ */
   return (0);
 } /* }}} int redis_init */
 
-static void redis_info_line (const char *info, const char *field,  const char *format, void *storage) { /* {{{ */
-  char *str = strstr(info, field);
-  if (str) {
-    str += strlen(field) + 1; /* also skip the ':' */
-    sscanf(str, format, storage);
-  }
-} /* }}} void redis_info_item */
-
-static void redis_info (const char *str, redis_info_t *info) /* {{{ */
+int redis_handle_info (char *node, char const *info_line, char const *type, char const *type_instance, char const *field_name, int ds_type) /* {{{ */
 {
-  redis_info_line (str, "uptime_in_seconds", "%ld", &(info->uptime_in_seconds));
-  redis_info_line (str, "connected_clients", "%d", &(info->connected_clients));
-  redis_info_line (str, "connected_slaves", "%d", &(info->connected_slaves));
-  redis_info_line (str, "blocked_clients", "%d", &(info->blocked_clients));
-  redis_info_line (str, "used_memory", "%zu", &(info->used_memory));
-  redis_info_line (str, "changes_since_last_save", "%lld", &(info->changes_since_last_save));
-  redis_info_line (str, "last_save_time", "%ld", &(info->last_save_time));
-  redis_info_line (str, "total_connections_received", "%lld", &(info->total_connections_received));
-  redis_info_line (str, "total_commands_processed", "%lld", &(info->total_commands_processed));
-  redis_info_line (str, "expired_keys", "%lld", &(info->expired_keys));
-  redis_info_line (str, "pubsub_patterns", "%u", &(info->pubsub_patterns));
-  redis_info_line (str, "pubsub_channels", "%ld", &(info->pubsub_channels));
-} /* }}} void redis_info */
+  char *str = strstr (info_line, field_name);
+  static char buf[MAX_REDIS_VAL_SIZE];
+  value_t val;
+  if (str)
+  {
+    int i;
+
+    str += strlen (field_name) + 1; /* also skip the ':' */
+    for(i=0;(*str && (isdigit(*str) || *str == '.'));i++,str++)
+      buf[i] = *str;
+    buf[i] ='\0';
+
+    if(parse_value (buf, &val, ds_type) == -1)
+    {
+      WARNING ("redis plugin: Unable to parse field `%s'.", field_name);
+      return (-1);
+    }
+
+    redis_submit (node, type, type_instance, val);
+    return (0);
+  }
+  return (-1);
+
+} /* }}} int redis_handle_info */
 
 static int redis_read (void) /* {{{ */
 {
-  int i;
   redis_node_t *rn;
-  redis_info_t info;
   redis_query_t *query;
 
   const data_set_t *ds;
   value_list_t vl = VALUE_LIST_INIT;
+  value_t val;
 
 
   for (rn = nodes_head; rn != NULL; rn = rn->next)
@@ -427,19 +387,16 @@ static int redis_read (void) /* {{{ */
       continue;
     }
 
-    redis_info(rr->str, &info);
-
-    redis_submit_d (rn->name, "uptime", NULL, info.uptime_in_seconds);
-    redis_submit_g (rn->name, "current_connections", "clients", info.connected_clients);
-    redis_submit_g (rn->name, "current_connections", "slaves", info.connected_slaves);
-    redis_submit_g (rn->name, "blocked_clients", NULL, info.blocked_clients);
-    redis_submit_g (rn->name, "memory", NULL, info.used_memory);
-    redis_submit_g (rn->name, "changes_since_last_save", NULL, info.changes_since_last_save);
-    redis_submit_d (rn->name, "total_connections", NULL, info.total_connections_received);
-    redis_submit_d (rn->name, "total_operations", NULL, info.total_commands_processed);
-    redis_submit_g (rn->name, "expired_keys", NULL, info.expired_keys);
-    redis_submit_g (rn->name, "pubsub", "patterns", info.pubsub_patterns);
-    redis_submit_g (rn->name, "pubsub", "channels", info.pubsub_channels);
+    redis_handle_info (rn->name, rr->str, "uptime", NULL, "uptime_in_seconds", DS_TYPE_GAUGE);
+    redis_handle_info (rn->name, rr->str, "connections", "clients", "connected_clients", DS_TYPE_GAUGE);
+    redis_handle_info (rn->name, rr->str, "connections", "slaves", "connected_slaves", DS_TYPE_GAUGE);
+    redis_handle_info (rn->name, rr->str, "blocked_clients", NULL, "blocked_clients", DS_TYPE_GAUGE);
+    redis_handle_info (rn->name, rr->str, "memory", NULL, "used_memory", DS_TYPE_GAUGE);
+    redis_handle_info (rn->name, rr->str, "changes_since_last_save", NULL, "changes_since_last_save", DS_TYPE_GAUGE);
+    redis_handle_info (rn->name, rr->str, "operations", NULL, "total_commands_processed", DS_TYPE_DERIVE);
+    redis_handle_info (rn->name, rr->str, "expired_keys", NULL, "expired_keys", DS_TYPE_GAUGE);
+    redis_handle_info (rn->name, rr->str, "pubsub", "patterns", "pubsub_patterns", DS_TYPE_GAUGE);
+    redis_handle_info (rn->name, rr->str, "pubsub", "channels", "pubsub_channels", DS_TYPE_GAUGE);
 
     /* Read custom queries */
     for (query=rn->query; query!=NULL; query=query->next)
@@ -447,14 +404,12 @@ static int redis_read (void) /* {{{ */
       if ((rr = redisCommand (rh, query->command)) == NULL)
       {
         WARNING ("redis plugin: unable to execute query `%s' on node `%s'.", query->name, rn->name);
-        redisFree (rh);
         continue;
       }
 
-      if(rr->type != 3)
+      if(rr->type != REDIS_REPLY_INTEGER)
       {
         WARNING ("redis plugin: unable to get reply for query `%s' on node `%s', integer expected.", query->name, rn->name);
-        redisFree (rh);
         continue;
       }
       DEBUG("Get data from query `%s' executing `%s' on node `%s'.", query->name, query->command, rn->name);
@@ -464,7 +419,6 @@ static int redis_read (void) /* {{{ */
       if (!ds)
       {
         ERROR ("redis plugin: DataSet `%s' not defined.", query->type);
-        redisFree (rh);
         continue;
       }
 
@@ -472,28 +426,30 @@ static int redis_read (void) /* {{{ */
       {
         ERROR ("redis plugin: DataSet `%s' requires %i values, but config talks about %i",
             query->type, ds->ds_num, 1);
-        redisFree (rh);
         continue;
       }
 
       vl.values_len = ds->ds_num;
-      vl.values = (value_t *) malloc (sizeof (value_t) * vl.values_len);
-      if (vl.values == NULL)
+      vl.values = &val;
+
+      if (ds->ds[0].type == DS_TYPE_COUNTER)
+        vl.values[0].counter = rr->integer;
+      else if (ds->ds[0].type == DS_TYPE_DERIVE)
+        vl.values[0].derive = rr->integer;
+      else if (ds->ds[0].type == DS_TYPE_ABSOLUTE)
+        vl.values[0].absolute = rr->integer;
+      else if (ds->ds[0].type == DS_TYPE_GAUGE)
+        vl.values[0].gauge = rr->integer;
+      else
       {
-        redisFree (rh);
+        ERROR ("redis plugin: Unknown type `%i' for datasource.", ds->ds[0].type);
         continue;
-      }
-      for (i = 0; i < vl.values_len; i++)
-      {
-        if (ds->ds[i].type == DS_TYPE_COUNTER)
-          vl.values[i].counter = rr->integer;
-        else
-          vl.values[i].gauge = rr->integer;
       }
 
       sstrncpy (vl.host, hostname_g, sizeof (vl.host));
-      sstrncpy (vl.plugin, "redis-query", sizeof (vl.plugin));
+      sstrncpy (vl.plugin, "redis", sizeof (vl.plugin));
       sstrncpy (vl.plugin_instance, rn->name, sizeof (vl.plugin));
+      strncat (vl.plugin_instance, "-query", sizeof (vl.plugin));
       sstrncpy (vl.type, query->type, sizeof (vl.type));
       sstrncpy (vl.type_instance, query->name, sizeof (vl.type));
 
