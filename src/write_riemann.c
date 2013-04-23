7e711643c470b830f3f8e94512c0a7adf5418202
Florian Forster <octo@collectd.org>
2013-04-23 07:15:44 +0200

    write_riemann plugin: Really fix a memory leak.
    Github: #307

--- src/write_riemann.c
+++ src/write_riemann.c
@@ -73,7 +73,11 @@ static void riemann_event_protobuf_free (Event *event) /* {{{ */
 	event->n_tags = 0;
 
 	for (i = 0; i < event->n_attributes; i++)
+	{
+		sfree (event->attributes[i]->key);
+		sfree (event->attributes[i]->value);
 		sfree (event->attributes[i]);
+	}
 	sfree (event->attributes);
 	event->n_attributes = 0;
 
