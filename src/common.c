855ba3880b50bc4a774a8c8d23612bba3550b3e7
Florian Forster <octo@collectd.org>
2013-05-19 08:59:04 +0200

    src/common_test.c: Add some tests for files in src/common.c.

--- src/common.c
+++ src/common.c
@@ -377,8 +377,10 @@ int strunescape (char *buf, size_t buf_len)
 		if (buf[i] != '\\')
 			continue;
 
-		if ((i >= buf_len) || (buf[i + 1] == '\0')) {
+		if (((i + 1) >= buf_len) || (buf[i + 1] == 0)) {
 			ERROR ("string unescape: backslash found at end of string.");
+			/* Ensure null-byte at the end of the buffer. */
+			buf[i] = 0;
 			return (-1);
 		}
 
@@ -397,7 +399,10 @@ int strunescape (char *buf, size_t buf_len)
 				break;
 		}
 
+		/* Move everything after the position one position to the left.
+		 * Add a null-byte as last character in the buffer. */
 		memmove (buf + i + 1, buf + i + 2, buf_len - i - 2);
+		buf[buf_len - 1] = 0;
 	}
 	return (0);
 } /* int strunescape */
