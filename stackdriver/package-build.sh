36ddd8ff457607c23edbc3bbd5257b1e8c95a621
John (J5) Palmieri <john@stackdriver.com>
2013-07-16 17:51:33 -0400

    error out if make fails

--- stackdriver/package-build.sh
+++ stackdriver/package-build.sh
@@ -3,20 +3,28 @@ VERSION=5.3.0
 
 if [ "x$PKGFORMAT" == "xdeb" ]
 then
-	[ -d agent-deb ] || git clone git@github.com:Stackdriver/agent-deb.git
-	pushd agent-deb
-	git pull
-	make DISTRO="$DISTRO" ARCH="$ARCH" VERSION="$VERSION" BUILD_NUM="$BUILD_NUM" build
+    [ -d agent-deb ] || git clone git@github.com:Stackdriver/agent-deb.git
+    pushd agent-deb
+    git pull
+    make DISTRO="$DISTRO" ARCH="$ARCH" VERSION="$VERSION" BUILD_NUM="$BUILD_NUM" build
+    if [ $? -ne 0 ]
+    then
+        return $?
+    fi
 	popd
 elif [ "x$PKGFORMAT" == "xrpm" ]
 then
-	[ -d agent-rpm ] || git clone git@github.com:Stackdriver/agent-rpm.git
-	pushd agent-rpm
-	git pull
-	make DISTRO="$DISTRO" ARCH="$ARCH" VERSION="$VERSION" BUILD_NUM="$BUILD_NUM" build
-	popd
+    [ -d agent-rpm ] || git clone git@github.com:Stackdriver/agent-rpm.git
+    pushd agent-rpm
+    git pull
+    make DISTRO="$DISTRO" ARCH="$ARCH" VERSION="$VERSION" BUILD_NUM="$BUILD_NUM" build
+    if [ $? -ne 0 ]
+    then
+        return $?
+    fi
+    popd
 else
-	echo "I don't know how to handle label '$PKGFORMAT'. Aborting build"
-	exit 1
+    echo "I don't know how to handle label '$PKGFORMAT'. Aborting build"
+    exit 1
 fi
 
