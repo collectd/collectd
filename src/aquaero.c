d33c4140644f9dfc682adcbdba6643f6e49f582e
Florian Forster <octo@collectd.org>
2013-08-18 10:55:38 +0200

    aquaero plugin: Some coding style fixes.
    * Break long lines.
    * Rename temperatures. The type instances are now "sensor", "virtual",
      "software" and "other".
    * Rename "flow-flow" to "flow-sensor".

--- src/aquaero.c
+++ src/aquaero.c
@@ -65,7 +65,8 @@ static int aquaero_shutdown (void)
 	return (0);
 } /* int aquaero_shutdown */
 
-static void aquaero_submit (const char *type, const char *type_instance, double value)
+static void aquaero_submit (const char *type, const char *type_instance,
+		double value)
 {
 	const char *instance = conf_device?conf_device:"default";
 	value_t values[1];
@@ -90,17 +91,21 @@ static void aquaero_submit (const char *type, const char *type_instance, double 
 } /* int aquaero_submit */
 
 /* aquaero_submit_array submits every value of a given array of values */
-static void aquaero_submit_array (const char *type, const char *type_instance_prefix, double *value_array, int len)
+static void aquaero_submit_array (const char *type,
+		const char *type_instance_prefix, double *value_array, int len)
 {
 	char type_instance[DATA_MAX_NAME_LEN];
 	int i;
 
 	for (i = 0; i < len; i++)
-		if (value_array[i] != AQ5_FLOAT_UNDEF)
-		{
-			snprintf(type_instance, sizeof(type_instance), "%s%d", type_instance_prefix, i+1);
-			aquaero_submit(type, type_instance, value_array[i]);
-		}
+	{
+		if (value_array[i] == AQ5_FLOAT_UNDEF)
+			continue;
+
+		snprintf (type_instance, sizeof (type_instance), "%s%d",
+				type_instance_prefix, i + 1);
+		aquaero_submit (type, type_instance, value_array[i]);
+	}
 }
 
 static int aquaero_read (void)
@@ -109,22 +114,24 @@ static int aquaero_read (void)
 	aq5_settings_t aq_sett;
 	char *err_msg = NULL;
 	char type_instance[DATA_MAX_NAME_LEN];
-	char errbuf[1024];
 	int i;
 
 	if (libaquaero5_poll(conf_device, &aq_data, &err_msg) < 0)
 	{
-		sstrerror(errno, errbuf, sizeof (errbuf));
-		ERROR ("Failed to poll device '%s': %s (%s)",
-				conf_device?conf_device:"default", err_msg, errbuf);
+		char errbuf[1024];
+		ERROR ("aquaero plugin: Failed to poll device \"%s\": %s (%s)",
+				conf_device ? conf_device : "default", err_msg,
+				sstrerror (errno, errbuf, sizeof (errbuf)));
 		return (-1);
 	}
 
 	if (libaquaero5_getsettings(conf_device, &aq_sett, &err_msg) < 0)
 	{
-		sstrerror(errno, errbuf, sizeof (errbuf));
-		ERROR ("Failed to get settings for device '%s': %s (%s)\n",
-				conf_device?conf_device:"default", err_msg, errbuf);
+		char errbuf[1024];
+		ERROR ("aquaero plugin: Failed to get settings "
+				"for device \"%s\": %s (%s)",
+				conf_device ? conf_device : "default", err_msg,
+				sstrerror (errno, errbuf, sizeof (errbuf)));
 		return (-1);
 	}
 
@@ -132,43 +139,54 @@ static int aquaero_read (void)
 	aquaero_submit("temperature", "cpu", aq_data.cpu_temp[0]);
 
 	/* Temperature sensors */
-	aquaero_submit_array("temperature", "temp", aq_data.temp, AQ5_NUM_TEMP);
+	aquaero_submit_array("temperature", "sensor", aq_data.temp,
+			AQ5_NUM_TEMP);
 
 	/* Virtual temperature sensors */
-	aquaero_submit_array("temperature", "virttemp", aq_data.vtemp, AQ5_NUM_VIRT_SENSORS);
+	aquaero_submit_array("temperature", "virtual", aq_data.vtemp,
+			AQ5_NUM_VIRT_SENSORS);
 
 	/* Software temperature sensors */
-	aquaero_submit_array("temperature", "softtemp", aq_data.stemp, AQ5_NUM_SOFT_SENSORS);
+	aquaero_submit_array("temperature", "software", aq_data.stemp,
+			AQ5_NUM_SOFT_SENSORS);
 
 	/* Other temperature sensors */
-	aquaero_submit_array("temperature", "othertemp", aq_data.otemp, AQ5_NUM_OTHER_SENSORS);
+	aquaero_submit_array("temperature", "other", aq_data.otemp,
+			AQ5_NUM_OTHER_SENSORS);
 
 	/* Fans */
 	for (i = 0; i < AQ5_NUM_FAN; i++)
 	{
-		if ((aq_sett.fan_data_source[i] == NONE) || (aq_data.fan_vrm_temp[i] != AQ5_FLOAT_UNDEF))
+		if ((aq_sett.fan_data_source[i] == NONE)
+				|| (aq_data.fan_vrm_temp[i] != AQ5_FLOAT_UNDEF))
 			continue;
 
-		/* Always report fanthe fan VRM (Voltage Regulator) temp if a regulator is installed. */
-		snprintf(type_instance, sizeof(type_instance), "fan%d", i+1);
-		aquaero_submit("fanspeed", type_instance, aq_data.fan_rpm[i]);
-		snprintf(type_instance, sizeof(type_instance), "fan-vrm%d", i+1);
-		aquaero_submit("temperature", type_instance, aq_data.fan_vrm_temp[i]);
+		snprintf (type_instance, sizeof (type_instance),
+				"fan%d", i + 1);
 
-		snprintf(type_instance, sizeof(type_instance), "fan%d", i+1);
-		aquaero_submit("percentage", type_instance, aq_data.fan_duty[i]);
+		aquaero_submit ("fanspeed", type_instance,
+				aq_data.fan_rpm[i]);
+		aquaero_submit ("percentage", type_instance,
+				aq_data.fan_duty[i]);
+		aquaero_submit ("voltage", type_instance,
+				aq_data.fan_voltage[i]);
+		aquaero_submit ("current", type_instance,
+				aq_data.fan_current[i]);
 
-		snprintf(type_instance, sizeof(type_instance), "fan%d", i+1);
-		aquaero_submit("voltage", type_instance, aq_data.fan_voltage[i]);
-		snprintf(type_instance, sizeof(type_instance), "fan%d", i+1);
-		aquaero_submit("current", type_instance, aq_data.fan_current[i]);
+		/* Report the voltage reglator module (VRM) temperature with a
+		 * different type instance. */
+		snprintf (type_instance, sizeof (type_instance),
+				"fan%d-vrm", i + 1);
+		aquaero_submit ("temperature", type_instance,
+				aq_data.fan_vrm_temp[i]);
 	}
 
 	/* Flow sensors */
-	aquaero_submit_array("flow", "flow", aq_data.flow, AQ5_NUM_FLOW);
+	aquaero_submit_array("flow", "sensor", aq_data.flow, AQ5_NUM_FLOW);
 
 	/* Liquid level */
-	aquaero_submit_array("percentage", "waterlevel", aq_data.level, AQ5_NUM_LEVEL);
+	aquaero_submit_array("percentage", "waterlevel",
+			aq_data.level, AQ5_NUM_LEVEL);
 
 	return (0);
 }
@@ -180,3 +198,5 @@ void module_register (void)
 	plugin_register_read ("aquaero", aquaero_read);
 	plugin_register_shutdown ("aquaero", aquaero_shutdown);
 } /* void module_register */
+
+/* vim: set sw=8 sts=8 noet : */
