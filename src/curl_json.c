8327ee64139eae2172f35cfdd7ee41998b806bcf
Rainer Müller <raimue@codingfarm.de>
2013-11-10 03:39:22 +0100

    curl_json, curl_xml: Fix unitialized variable
    The variable url is used unintialized here. The code used to be the same
    in both plugins, but diverged in 19808b44. The solution applied there
    does not work correctly as the effective URL can only be queried after
    performing the request. Instead, just use the original request URL.
    
    Signed-off-by: Florian Forster <octo@collectd.org>

--- src/curl_json.c
+++ src/curl_json.c
@@ -859,17 +859,17 @@ static int cj_curl_perform(cj_t *db) /* {{{ */
   int status;
   long rc;
   char *url;
-  url = NULL;
-  curl_easy_getinfo(db->curl, CURLINFO_EFFECTIVE_URL, &url);
+  url = db->url;
 
   status = curl_easy_perform (db->curl);
   if (status != CURLE_OK)
   {
     ERROR ("curl_json plugin: curl_easy_perform failed with status %i: %s (%s)",
-           status, db->curl_errbuf, (url != NULL) ? url : "<null>");
+           status, db->curl_errbuf, url);
     return (-1);
   }
 
+  curl_easy_getinfo(db->curl, CURLINFO_EFFECTIVE_URL, &url);
   curl_easy_getinfo(db->curl, CURLINFO_RESPONSE_CODE, &rc);
 
   /* The response code is zero if a non-HTTP transport was used. */
