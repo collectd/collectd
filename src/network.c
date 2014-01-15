d21977654dd598186c84b5155f8c906a75e64929
Chris Lundquist <chris.lundquist@github.com>
2014-01-14 18:33:13 -0800

    [network] set_thread_cbs so we initialize the right threading mode in gcry_check_version

--- src/network.c
+++ src/network.c
@@ -501,8 +501,8 @@ static void network_init_gcrypt (void) /* {{{ */
   if (gcry_control (GCRYCTL_ANY_INITIALIZATION_P))
     return;
 
-  gcry_check_version (NULL); /* before calling any other functions */
   gcry_control (GCRYCTL_SET_THREAD_CBS, &gcry_threads_pthread);
+  gcry_check_version (NULL); /* before calling *almost* any other functions */
   gcry_control (GCRYCTL_INIT_SECMEM, 32768);
   gcry_control (GCRYCTL_INITIALIZATION_FINISHED);
 } /* }}} void network_init_gcrypt */
