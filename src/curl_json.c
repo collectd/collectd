401fa6e09c3fc69fabcbc5639b55917c30489c6d
Jeremy Katz <jeremy@katzbox.net>
2014-01-26 20:43:19 -0500

    Call curl_global_init() in _init of plugins using curl
    Need to call curl_global_init() or curl_easy_init() during init
    for plugins when we're still running single threaded.  This
    updates the remaining ones

--- src/curl_json.c
+++ src/curl_json.c
@@ -956,9 +956,18 @@ static int cj_read (user_data_t *ud) /* {{{ */
   return cj_perform (db);
 } /* }}} int cj_read */
 
+static int cj_init (void) /* {{{ */
+{
+  /* Call this while collectd is still single-threaded to avoid
+   * initialization issues in libgcrypt. */
+  curl_global_init (CURL_GLOBAL_SSL);
+  return (0);
+} /* }}} int cj_init */
+
 void module_register (void)
 {
   plugin_register_complex_config ("curl_json", cj_config);
+  plugin_register_init ("curl_json", cj_init);
 } /* void module_register */
 
 /* vim: set sw=2 sts=2 et fdm=marker : */
