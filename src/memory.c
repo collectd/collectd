19c5725693a7271ab7e4d73026da2381b2044ab3
Manuel CISSÉ <manuel_cisse@yahoo.fr>
2014-02-20 15:58:57 +0100

    memory plugin: fix submit multivalue (fill vl.type)

--- src/memory.c
+++ src/memory.c
@@ -487,6 +487,7 @@ static int memory_read (void) /* {{{ */
 	vl.values_len = STATIC_ARRAY_SIZE (v);
 	sstrncpy (vl.host, hostname_g, sizeof (vl.host));
 	sstrncpy (vl.plugin, "memory", sizeof (vl.plugin));
+	sstrncpy (vl.type, "memory", sizeof (vl.type));
 	vl.time = cdtime ();
 
 	return (memory_read_internal (&vl));
