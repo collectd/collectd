0b4c7e550fe641f2b8fc2940c5a303f6dd1aac41
Wilfried Goesgens <dothebart@citadel.org>
2014-04-28 22:13:58 +0200

    Configure the scope of the statsd histogram (fixes #401) - bugfix
      - when resetting the structure preserve our configured scale
      - don't devide zero by scale.

--- src/utils_latency.h
+++ src/utils_latency.h
@@ -34,7 +34,7 @@ latency_counter_t *latency_counter_create (int Resolution);
 void latency_counter_destroy (latency_counter_t *lc);
 
 void latency_counter_add (latency_counter_t *lc, cdtime_t latency);
-void latency_counter_reset (latency_counter_t *lc);
+void latency_counter_reset (latency_counter_t *lc, int Resolution);
 
 cdtime_t latency_counter_get_min (latency_counter_t *lc);
 cdtime_t latency_counter_get_max (latency_counter_t *lc);
