804ef69e8163aa3de6652d52f89935444accbb4f
Wilfried Goesgens <dothebart@citadel.org>
2014-06-21 21:14:28 +0200

    Statsd: avoid doing a division by zero when calculating the average if we didn't get any values.

--- src/utils_latency.c
+++ src/utils_latency.c
@@ -128,7 +128,7 @@ cdtime_t latency_counter_get_average (latency_counter_t *lc) /* {{{ */
 {
   double average;
 
-  if (lc == NULL)
+  if ((lc == NULL) || (lc->num == 0))
     return (0);
 
   average = CDTIME_T_TO_DOUBLE (lc->sum) / ((double) lc->num);
