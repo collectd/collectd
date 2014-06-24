cb4ea7112145fa91500b4d4518b83289acb80c2f
Marc Fournier <marc.fournier@camptocamp.com>
2014-06-24 17:47:08 +0200

    logfile: avoid total silence in case of a misconfiguration
    Basically the same patch that was applied to syslog.c in 84c38056

--- src/logfile.c
+++ src/logfile.c
@@ -54,7 +54,11 @@ static int logfile_config (const char *key, const char *value)
 {
 	if (0 == strcasecmp (key, "LogLevel")) {
 		log_level = parse_log_severity(value);
-		if (log_level == -1) return 1; /* to keep previous behaviour */
+		if (log_level < 0) {
+			log_level = LOG_INFO;
+			ERROR ("logfile: invalid loglevel [%s] defaulting to 'info'", value);
+			return (1);
+		}
 	}
 	else if (0 == strcasecmp (key, "File")) {
 		sfree (log_file);
