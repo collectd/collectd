ad459d91bd8d24dc02737f398943b325cd446d48
Pierre-Yves Ritschard <pyr@spootnik.org>
2014-07-25 16:08:13 +0200

    forgotten printf

--- src/log_logstash.c
+++ src/log_logstash.c
@@ -60,7 +60,6 @@ static int log_logstash_config (const char *key, const char *value)
 
 	if (0 == strcasecmp (key, "LogLevel")) {
 		log_level = parse_log_severity(value);
-		printf("parsed log level: %d\n", log_level);
         if (log_level < 0) {
             log_level = LOG_INFO;
             ERROR("log_logstash: invalid loglevel [%s] defaulting to 'info'",
