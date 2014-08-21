b87c57591e40b3d9cd86c0a382818acc78558d96
Moshe Zada <moshe@forter.com>
2014-08-21 15:23:27 +0300

    Small naming conventions fixes, let the user to choose its delimiter

--- src/write_riemann.c
+++ src/write_riemann.c
@@ -48,7 +48,7 @@ int write_riemann_threshold_check(const data_set_t *, const value_list_t *, int 
 
 struct riemann_host {
 	char			*name;
-	char			*eventServicePrefix;
+	char			*event_service_prefix;
 #define F_CONNECT		 0x01
 	uint8_t			 flags;
 	pthread_mutex_t		 lock;
@@ -570,19 +570,19 @@ static Event *riemann_value_to_protobuf (struct riemann_host const *host, /* {{{
 			/* host = */ "", vl->plugin, vl->plugin_instance,
 			vl->type, vl->type_instance);
 	if (host->always_append_ds || (ds->ds_num > 1))
-		if (host->eventServicePrefix == NULL || host->eventServicePrefix[0] == '\0')
+		if (host->event_service_prefix == NULL || host->event_service_prefix[0] == '\0')
 			ssnprintf (service_buffer, sizeof (service_buffer),
 					"%s/%s", &name_buffer[1], ds->ds[index].name);
 		else
 			ssnprintf (service_buffer, sizeof (service_buffer),
-					"%s/%s/%s", host->eventServicePrefix, &name_buffer[1], ds->ds[index].name);
+					"%s%s/%s", host->event_service_prefix, &name_buffer[1], ds->ds[index].name);
 	else
-		if (host->eventServicePrefix == NULL || host->eventServicePrefix[0] == '\0')
+		if (host->event_service_prefix == NULL || host->event_service_prefix[0] == '\0')
 			sstrncpy (service_buffer, &name_buffer[1],
 					sizeof (service_buffer));
 		else
 			ssnprintf (service_buffer, sizeof (service_buffer),
-					"%s/%s", host->eventServicePrefix, &name_buffer[1]);
+					"%s%s", host->event_service_prefix, &name_buffer[1]);
 
 	event->service = strdup (service_buffer);
 
@@ -765,7 +765,7 @@ static int riemann_config_node(oconfig_item_t *ci) /* {{{ */
             if (status != 0)
                 break;
         } else if (strcasecmp ("EventServicePrefix", child->key) == 0) {
-            status = cf_util_get_string (child, &host->eventServicePrefix);
+            status = cf_util_get_string (child, &host->event_service_prefix);
             if (status != 0)
                 break;
         } else if (strcasecmp ("CheckThresholds", child->key) == 0) {
