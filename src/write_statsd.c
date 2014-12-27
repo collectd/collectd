bf7294fc20622337c0f13bcd39c5ba6a289f83b3
Stefano Pogliani <stefano@spogliani.net>
2014-12-27 17:16:22 +0100

    Updated write_statsd based on code style.

--- src/write_statsd.c
+++ src/write_statsd.c
@@ -1,19 +1,24 @@
 /**
  * collectd - src/write_statsd.c
- * Copyright (C) 2014       Stefano Pogliani
+ * Copyright (c) 2014 Stefano Pogliani
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; only version 2 of the License is applicable.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  *
  * Authors:
  *   Stefano Pogliani <stefano at spogliani.net>
@@ -33,7 +38,7 @@
 #include "plugin.h"
 
 
-// *** Compile-time constants. ***
+/*** Compile-time constants. ***/
 #define WS_DOUBLE_FORMAT  "%f"
 #define VALUE_STR_LEN     20
 #define WRITE_STATSD_NAME "write_statsd"
@@ -45,9 +50,8 @@
   }
 
 
-// *** Type declarations. ***
-static const char *config_keys[] =
-{
+/*** Type declarations. ***/
+static const char *config_keys[] = {
   "Host",
   "Port",
   "Postfix",
@@ -61,9 +65,9 @@ struct write_statsd_config_s {
   int   port;
   char* postfix;
   char* prefix;
-  int   silence_type_warnings;
+  _Bool silence_type_warnings;
 
-  // Internally derved options.
+  /* Internally derved options. */
   char* event_line_format;
   size_t event_line_base_len;
 };
@@ -76,24 +80,25 @@ struct write_statsd_link_s {
 typedef struct write_statsd_link_s write_statsd_link_t;
 
 
-// *** Module variables. ***
+/*** Module variables. ***/
 static char* DS_TYPE_TO_STATSD[] = {
-  "c",  // DS_TYPE_COUNTER
-  "g",  // DS_TYPE_GAUGE
-  "g",  // DS_TYPE_DERIVE
-  NULL  // DS_TYPE_ABSOLUTE
+  "c",  /* DS_TYPE_COUNTER  */
+  "g",  /* DS_TYPE_GAUGE    */
+  "g",  /* DS_TYPE_DERIVE   */
+  NULL  /* DS_TYPE_ABSOLUTE */
 };
 
-// The configuration is stored in a global instance and must not
-// be modified outside of load_config or initialise.
+/*
+ * The configuration is stored in a global instance and must not
+ * be modified outside of load_config or initialise.
+ */
 static write_statsd_config_t configuration = {
   NULL, 8125, NULL, NULL, 0, NULL, 0
 };
 
 
-// *** Module functions. *** 
-void* allocate(size_t size)
-{
+/*** Module functions. ***/
+static void* allocate(size_t size) {
   void* mem = (char*)malloc(size);
   if (mem == NULL) {
     ERROR("write_statsd plugin: not enough memory.");
@@ -102,8 +107,7 @@ void* allocate(size_t size)
   return mem;
 }
 
-char* ds_value_to_string(int type, value_t value)
-{
+static char* ds_value_to_string(int type, value_t value) {
   char* result = allocate(VALUE_STR_LEN);
   if (result == NULL) {
     return NULL;
@@ -111,15 +115,15 @@ char* ds_value_to_string(int type, value_t value)
 
   switch(type) {
     case DS_TYPE_COUNTER:
-      snprintf(result, VALUE_STR_LEN, "%lld", value.counter);
+      ssnprintf(result, VALUE_STR_LEN, "%lld", value.counter);
       break;
 
     case DS_TYPE_GAUGE:
-      snprintf(result, VALUE_STR_LEN, WS_DOUBLE_FORMAT, value.gauge);
+      ssnprintf(result, VALUE_STR_LEN, WS_DOUBLE_FORMAT, value.gauge);
       break;
 
     case DS_TYPE_DERIVE:
-      snprintf(result, VALUE_STR_LEN, "%"PRId64, value.derive);
+      ssnprintf(result, VALUE_STR_LEN, "%"PRId64, value.derive);
       break;
 
     default:
@@ -131,8 +135,7 @@ char* ds_value_to_string(int type, value_t value)
 }
 
 
-write_statsd_link_t open_socket(void)
-{
+static write_statsd_link_t open_socket(void) {
   write_statsd_link_t link      = {{0}, 0};
   struct addrinfo     requested = {0};
   struct addrinfo*    resolved  = NULL;
@@ -167,8 +170,7 @@ write_statsd_link_t open_socket(void)
 }
 
 
-int write_statsd_config(const char *key, const char *value)
-{
+static int write_statsd_config(const char *key, const char *value) {
   if (strcasecmp(key, "Host") == 0) {
     configuration.host = strdup(value);
   }
@@ -190,14 +192,13 @@ int write_statsd_config(const char *key, const char *value)
   }
 
   if (strcasecmp(key, "SilenceTypeWarnings") == 0) {
-    configuration.silence_type_warnings = strcasecmp(value, "yes") == 0;
+    configuration.silence_type_warnings = IS_TRUE(value);
   }
 
   return 0;
 }
 
-int write_statsd_free(void)
-{
+static int write_statsd_free(void) {
   FREE_NOT_NULL(configuration.host);
   FREE_NOT_NULL(configuration.postfix);
   FREE_NOT_NULL(configuration.prefix);
@@ -209,8 +210,10 @@ int write_statsd_free(void)
   return 0;
 }
 
-int write_statsd_init(void)
-{
+static int write_statsd_init(void) {
+  size_t buffer_len = 0;
+  size_t len = 0;
+
   if (configuration.host == NULL) {
     ERROR("write_statsd plugin: missing required 'Host' configuration.");
     FREE_NOT_NULL(configuration.postfix)
@@ -218,53 +221,57 @@ int write_statsd_init(void)
     return -1;
   }
 
-  // Generate format for event lines to pass to snprintf.
-  // A line full event line would be:
-  // <prefix>.<metric-name>.<value-name>.<postfix>:<value>|<type>
-  // The length of the format must exclude the placeholders.
-  size_t buffer_len = 0;
-  size_t len = 0;
+  /*
+   * Generate format for event lines to pass to ssnprintf.
+   * A line full event line would be:
+   * <prefix>.<metric-name>.<value-name>.<postfix>:<value>|<type>
+   * The length of the format must exclude the placeholders.
+   */
   if (configuration.prefix == NULL && configuration.postfix == NULL) {
-    // %s.%s:%s|%s
+    /* %s.%s:%s|%s */
     configuration.event_line_format = strdup("%s.%s:%s|%s");
     len = 3;
 
   } else if (configuration.prefix == NULL) {
-    // %s.%s.<postfix>:%s|%s
+    /* %s.%s.<postfix>:%s|%s */
     len = 4 + strlen(configuration.postfix);
     buffer_len = len + 8 + 1;
     configuration.event_line_format = allocate(buffer_len);
     if (configuration.event_line_format == NULL) {
       return -2;
     }
-    snprintf(configuration.event_line_format, buffer_len,
+    ssnprintf(configuration.event_line_format, buffer_len,
              "%%s.%%s.%s:%%s|%%s", configuration.postfix);
 
   } else if (configuration.postfix == NULL) {
-    // <prefix>.%s.%s:%s|%s
+    /* <prefix>.%s.%s:%s|%s */
     len = strlen(configuration.prefix) + 4;
     buffer_len = len + 8 + 1;
     configuration.event_line_format = allocate(buffer_len);
     if (configuration.event_line_format == NULL) {
       return -2;
     }
-    snprintf(configuration.event_line_format, buffer_len,
+    ssnprintf(configuration.event_line_format, buffer_len,
              "%s.%%s.%%s:%%s|%%s", configuration.prefix);
 
   } else {
-    // <prefix>.%s.%s.<potfix>:%s|%s
+    /* <prefix>.%s.%s.<potfix>:%s|%s */
     len = 5 + strlen(configuration.prefix) + strlen(configuration.postfix);
     buffer_len = len + 8 + 1;
     configuration.event_line_format = allocate(buffer_len);
     if (configuration.event_line_format == NULL) {
       return -2;
     }
-    snprintf(configuration.event_line_format, buffer_len,
+    ssnprintf(configuration.event_line_format, buffer_len,
              "%s.%%s.%%s.%s:%%s|%%s", configuration.prefix,
              configuration.postfix);
 
   }
   configuration.event_line_base_len = len;
+
+  /* Postfix and prefix are now in event_line_format so they can be freed. */
+  FREE_NOT_NULL(configuration.postfix)
+  FREE_NOT_NULL(configuration.prefix)
 
   DEBUG("%s configuration completed.", WRITE_STATSD_NAME);
   DEBUG("%s Host: %s", WRITE_STATSD_NAME, configuration.host);
@@ -277,15 +284,17 @@ int write_statsd_init(void)
 }
 
 
-int write_statsd_send_message(const char* message)
-{
+static int write_statsd_send_message(const char* message) {
+  int slen;
+  int result;
   write_statsd_link_t link = open_socket();
+
   if (link.sock == 0) {
     return 0;
   }
 
-  int slen = sizeof(link.server);
-  int result = sendto(link.sock, message, strlen(message), 0,
+  slen = sizeof(link.server);
+  result = sendto(link.sock, message, strlen(message), 0,
                       (struct sockaddr *)&link.server, slen);
 
   if (result == -1) {
@@ -296,13 +305,17 @@ int write_statsd_send_message(const char* message)
   return 0;
 }
 
-int write_statsd_write(
-    const data_set_t *ds, const value_list_t *vl, user_data_t *ud)
-{
+static int write_statsd_write(
+    const data_set_t *ds, const value_list_t *vl, user_data_t *ud) {
   int idx;
 
   for (idx = 0; idx < ds->ds_num; idx++) {
+    char* message;
+    size_t message_len;
+    int result;
     char* type = DS_TYPE_TO_STATSD[ds->ds[idx].type];
+    char* value = NULL;
+
     if (type == NULL) {
       if (!configuration.silence_type_warnings) {
         WARNING("write_statsd plugin: unsupported StatsD type '%s' "
@@ -312,16 +325,15 @@ int write_statsd_write(
       return 0;
     }
 
-    char* value = ds_value_to_string(ds->ds[idx].type, vl->values[idx]);
+    value = ds_value_to_string(ds->ds[idx].type, vl->values[idx]);
     if (value == NULL) {
       return -5;
     }
 
-    // The full message will have prefix, postfix and separators plus
-    // the type name, the value name, value and type identifier.
-    char* message;
-    size_t message_len;
-
+    /*
+     * The full message will have prefix, postfix and separators plus
+     * the type name, the value name, value and type identifier.
+     */
     message_len = configuration.event_line_base_len;
     message_len += strlen(ds->type);
     message_len += strlen(ds->ds[idx].name);
@@ -334,14 +346,14 @@ int write_statsd_write(
       return -6;
     }
 
-    snprintf(message, message_len + 1, configuration.event_line_format,
+    ssnprintf(message, message_len + 1, configuration.event_line_format,
              ds->type, ds->ds[idx].name, value, type);
     free(value);
 
-    int ret = write_statsd_send_message(message);
+    result = write_statsd_send_message(message);
     free(message);
-    if (ret != 0) {
-      return ret;
+    if (result != 0) {
+      return result;
     }
   }
 
@@ -349,8 +361,7 @@ int write_statsd_write(
 }
 
 
-void module_register(void)
-{
+void module_register(void) {
   plugin_register_config(
       WRITE_STATSD_NAME, write_statsd_config, config_keys, config_keys_num);
   plugin_register_init(WRITE_STATSD_NAME, write_statsd_init);
