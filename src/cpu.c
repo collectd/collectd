7e3f82ada8e7412c984ca5ddd31817cf6a846368
Sam Kottler <shk@linux.com>
2015-01-04 12:10:56 -0500

    cpu_temp_retry_max is a time_t, not an int

--- src/cpu.c
+++ src/cpu.c
@@ -218,7 +218,7 @@ static int init (void)
 {
 #if PROCESSOR_CPU_LOAD_INFO
 	kern_return_t status;
-	static int cpu_temp_retry_max;
+	static time_t cpu_temp_retry_max;
 
 	port_host = mach_host_self ();
 
