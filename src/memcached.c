7ab5b9622a30f3703f20955f7755c6db5c7f0df8
Matthias Endler <matthias-endler@gmx.net>
2015-01-19 13:46:58 +0100

    Add support for memcached server alias.
    This is useful if you need to specify an IP in the collectd config to access the
    memcached instance but also want a pretty series name for your metrics.

--- src/memcached.c
+++ src/memcached.c
@@ -48,6 +48,7 @@ struct memcached_s
   char *socket;
   char *host;
   char *port;
+  char *alias;
 };
 typedef struct memcached_s memcached_t;
 
@@ -254,10 +255,19 @@ static void memcached_init_vl (value_list_t *vl, memcached_t const *st)
   {
     if (st->socket != NULL)
       sstrncpy (vl->host, hostname_g, sizeof (vl->host));
+	}
     else
-      sstrncpy (vl->host,
-          (st->host != NULL) ? st->host : MEMCACHED_DEF_HOST,
-          sizeof (vl->host));
+	{
+	    if (st->alias != NULL)
+	    {
+			sstrncpy(vl->host, st->alias, sizeof(st->alias));
+        }
+        else
+        {
+            sstrncpy (vl->host,
+                (st->host != NULL) ? st->host : MEMCACHED_DEF_HOST,
+                sizeof (vl->host));
+		}
     sstrncpy (vl->plugin_instance, st->name, sizeof (vl->plugin_instance));
   }
 }
@@ -621,6 +631,8 @@ static int config_add_instance(oconfig_item_t *ci)
       status = cf_util_get_string (child, &st->host);
     else if (strcasecmp ("Port", child->key) == 0)
       status = cf_util_get_service (child, &st->port);
+    else if (strcasecmp("Alias", child->key) == 0)
+      status = cf_util_get_service(child, &st->alias);
     else
     {
       WARNING ("memcached plugin: Option `%s' not allowed here.",
@@ -641,7 +653,7 @@ static int config_add_instance(oconfig_item_t *ci)
     return (-1);
   }
 
-  return (0);
+	return (0);
 }
 
 static int memcached_config (oconfig_item_t *ci)
@@ -693,6 +705,7 @@ static int memcached_init (void)
   st->socket = NULL;
   st->host = NULL;
   st->port = NULL;
+  st->alias = NULL;
 
   status = memcached_add_read_callback (st);
   if (status == 0)
