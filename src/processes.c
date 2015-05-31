cce1f2703e23abc87d1bdfbfe8ddd047cfac723c
Manuel Luis Sanmartín Rozada <manuel.luis@gmail.com>
2015-05-31 20:25:31 +0200

    processes: remove function ps_read_tasks

--- src/processes.c
+++ src/processes.c
@@ -778,33 +778,6 @@ static void ps_submit_fork_rate (derive_t value)
 
 /* ------- additional functions for KERNEL_LINUX/HAVE_THREAD_INFO ------- */
 #if KERNEL_LINUX
-static int ps_read_tasks (int pid)
-{
-	char           dirname[64];
-	DIR           *dh;
-	struct dirent *ent;
-	int count = 0;
-
-	ssnprintf (dirname, sizeof (dirname), "/proc/%i/task", pid);
-
-	if ((dh = opendir (dirname)) == NULL)
-	{
-		DEBUG ("Failed to open directory `%s'", dirname);
-		return (-1);
-	}
-
-	while ((ent = readdir (dh)) != NULL)
-	{
-		if (!isdigit ((int) ent->d_name[0]))
-			continue;
-		else
-			count++;
-	}
-	closedir (dh);
-
-	return ((count >= 1) ? count : 1);
-} /* int *ps_read_tasks */
-
 /* Read data from /proc/pid/status */
 static procstat_t *ps_read_status (int pid, procstat_t *ps)
 {
@@ -1023,11 +996,7 @@ int ps_read_process (int pid, procstat_t *ps, char *state)
 		}
 		if ( ps->num_lwp <= 0)
 		{
-			if ( (ps->num_lwp = ps_read_tasks (pid)) == -1 )
-			{
-				/* returns -1 => kernel 2.4 */
-				ps->num_lwp = 1;
-			}
+			ps->num_lwp = 1;
 		}
 		ps->num_proc = 1;
 	}
