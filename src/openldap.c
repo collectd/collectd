b842268ca7212869d8b984e921c986309d427161
Marek Becka <marek.becka@superhosting.cz>
2015-06-16 10:59:20 -0400

    fix openldap segfatult when ldapi is used  v2

--- src/openldap.c
+++ src/openldap.c
@@ -604,7 +604,8 @@ static int cldap_config_add (oconfig_item_t *ci) /* {{{ */
 				st->name, st->url);
 			status = -1;
 		}
-		else if (ludpp->lud_host != NULL)
+
+		if ((status == 0) && (ludpp->lud_host != NULL))
 		{
 			st->host = strdup (ludpp->lud_host);
 		}
