07f90477f6bba2d71c4c294226005f2d6443ac71
Corey Kosak <kosak@google.com>
2015-06-25 16:20:44 -0400

    Change package-build.sh so that it checks out a branch specified by an environment variable, defaulting to stackdriver-agent-$BRANCH.

--- stackdriver/package-build.sh
+++ stackdriver/package-build.sh
@@ -1,14 +1,17 @@
 #!/bin/bash -xe
 VERSION=5.5.0
 
+if [ "x$BRANCH" == "x" ]
+then
+    BRANCH=stackdriver-agent-$VERSION
+fi
+
 if [ "x$PKGFORMAT" == "xdeb" ]
 then
     # debian denotes 64 arches with amd64
     [ "x$ARCH" == "xx86_64" ] && ARCH="amd64" || true
-    git clone git@github.com:Stackdriver/agent-deb.git
+    git clone git@github.com:Stackdriver/agent-deb.git --branch $BRANCH
     pushd agent-deb
-    git fetch origin stackdriver-agent-$VERSION
-    git checkout stackdriver-agent-$VERSION
     make clean
     make DISTRO="$DISTRO" ARCH="$ARCH" VERSION="$VERSION" BUILD_NUM="$BUILD_NUM" build
     if [ $? -ne 0 ]
@@ -20,10 +23,8 @@ then
 	cp -r agent-deb/result .
 elif [ "x$PKGFORMAT" == "xrpm" ]
 then
-    git clone git@github.com:Stackdriver/agent-rpm.git
+    git clone git@github.com:Stackdriver/agent-rpm.git --branch $BRANCH
     pushd agent-rpm
-    git fetch origin stackdriver-agent-$VERSION
-    git checkout stackdriver-agent-$VERSION
     make clean
     make DISTRO="$DISTRO" ARCH="$ARCH" VERSION="$VERSION" BUILD_NUM="$BUILD_NUM" build
     if [ $? -ne 0 ]
