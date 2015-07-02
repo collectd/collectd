9a97e6fa25e6703cbe1144cfb7f1c96a3056f8a2
Yves Mettier <ymettier@free.fr>
2015-07-02 16:32:50 +0100

    write_tsdb uses meta data to reformat metrics and set tags

--- src/write_tsdb.c
+++ src/write_tsdb.c
@@ -28,9 +28,11 @@
  * write_tsdb Authors:
  *   Brett Hawn <bhawn at llnw.com>
  *   Kevin Bowling <kbowling@llnw.com>
+ *   Yves Mettier <ymettier@free.fr>
  **/
 
 /* write_tsdb plugin configuation example
+ * --------------------------------------
  *
  * <Plugin write_tsdb>
  *   <Node>
@@ -39,6 +41,85 @@
  *     HostTags "status=production deviceclass=www"
  *   </Node>
  * </Plugin>
+ *
+ * write_tsdb meta_data
+ * --------------------
+ *  - tsdb_prefix : Will prefix the OpenTSDB <metric> (also prefix tsdb_id if defined)
+ *  - tsdb_id     : Replace the metric with this tag
+ *
+ *  - tsdb_tag_plugin         : When defined, tsdb_tag_* removes the related
+ *  - tsdb_tag_pluginInstance : item from metric id.
+ *  - tsdb_tag_type           : If it is not empty, it will be the key of an
+ *  - tsdb_tag_typeInstance   : opentsdb tag (the value is the item itself)
+ *  - tsdb_tag_dsname         : If it is empty, no tag is defined.
+ *
+ * write_tsdb plugin filter rules example
+ * --------------------------------------
+ *
+ * <Chain "PreCache">
+ *   <Rule "opentsdb_cpu">
+ *     <Match "regex">
+ *       Plugin "^cpu$"
+ *     </Match>
+ *     <Target "set">
+ *       MetaDataSet "tsdb_tag_pluginInstance" "cpu"
+ *       MetaDataSet "tsdb_tag_type" ""
+ *       MetaDataSet "tsdb_prefix" "sys."
+ *     </Target>
+ *   </Rule>
+ *   <Rule "opentsdb_df">
+ *     <Match "regex">
+ *       Plugin "^df$"
+ *     </Match>
+ *     <Target "set">
+ *       MetaDataSet "tsdb_tag_pluginInstance" "mount"
+ *       MetaDataSet "tsdb_tag_type" ""
+ *       MetaDataSet "tsdb_prefix" "sys."
+ *     </Target>
+ *   </Rule>
+ *   <Rule "opentsdb_disk">
+ *     <Match "regex">
+ *       Plugin "^disk$"
+ *     </Match>
+ *     <Target "set">
+ *       MetaDataSet "tsdb_tag_pluginInstance" "disk"
+ *       MetaDataSet "tsdb_prefix" "sys."
+ *     </Target>
+ *   </Rule>
+ *   <Rule "opentsdb_interface">
+ *     <Match "regex">
+ *       Plugin "^interface$"
+ *     </Match>
+ *     <Target "set">
+ *       MetaDataSet "tsdb_tag_pluginInstance" "iface"
+ *       MetaDataSet "tsdb_prefix" "sys."
+ *     </Target>
+ *   </Rule>
+ *   <Rule "opentsdb_load">
+ *     <Match "regex">
+ *       Plugin "^loac$"
+ *     </Match>
+ *     <Target "set">
+ *       MetaDataSet "tsdb_tag_type" ""
+ *       MetaDataSet "tsdb_prefix" "sys."
+ *     </Target>
+ *   </Rule>
+ *   <Rule "opentsdb_swap">
+ *     <Match "regex">
+ *       Plugin "^swap$"
+ *     </Match>
+ *     <Target "set">
+ *       MetaDataSet "tsdb_prefix" "sys."
+ *     </Target>
+ *   </Rule>
+ * </Chain>
+ *
+ * IMPORTANT WARNING
+ * -----------------
+ * OpenTSDB allows no more than 8 tags.
+ * Collectd admins should be aware of this when defining filter rules and host
+ * tags.
+ *
  */
 
 #include "collectd.h"
@@ -68,6 +149,20 @@
 #ifndef WT_SEND_BUF_SIZE
 # define WT_SEND_BUF_SIZE 1428
 #endif
+
+/* Meta data definitions about tsdb tags */
+#define TSDB_TAG_PLUGIN 0
+#define TSDB_TAG_PLUGININSTANCE 1
+#define TSDB_TAG_TYPE 2
+#define TSDB_TAG_TYPEINSTANCE 3
+#define TSDB_TAG_DSNAME 4
+static const char *meta_tag[] = {
+    "tsdb_tag_plugin",
+    "tsdb_tag_pluginInstance",
+    "tsdb_tag_type",
+    "tsdb_tag_typeInstance",
+    "tsdb_tag_dsname"
+};
 
 /*
  * Private variables
@@ -341,15 +436,106 @@ static int wt_format_values(char *ret, size_t ret_len,
     return 0;
 }
 
-static int wt_format_name(char *ret, int ret_len,
+static int wt_format_tags(char *ret, int ret_len,
                           const value_list_t *vl,
                           const struct wt_callback *cb,
                           const char *ds_name)
 {
     int status;
     char *temp = NULL;
-    char *prefix = "";
+    char *ptr = ret;
+    size_t remaining_len = ret_len;
+
+#define TSDB_META_DATA_GET_STRING(tag) do { \
+        temp = NULL; \
+        status = meta_data_get_string(vl->meta, tag, &temp); \
+        if (status == -ENOENT) { \
+            temp = NULL; \
+            /* defaults to empty string */ \
+        } else if (status < 0) { \
+            sfree(temp); \
+            return status; \
+        } \
+    } while(0)
+
+#define TSDB_STRING_APPEND_SPRINTF(key, value) do { \
+        int n; \
+        const char *k = (key); \
+        if(k[0] != '\0') { \
+            n = ssnprintf(ptr, remaining_len, " %s=%s", k, value); \
+            if(n >= remaining_len) { \
+                ptr[0] = '\0'; \
+            } else { \
+                char *ptr2 = ptr+1; \
+                while(NULL != (ptr2 = strchr(ptr2, ' '))) ptr2[0] = '_';  \
+                ptr += n; \
+                remaining_len -= n; \
+            } \
+        } \
+    } while(0)
+
+    if (vl->meta) {
+        TSDB_META_DATA_GET_STRING(meta_tag[TSDB_TAG_PLUGIN]);
+        if(temp) {
+            TSDB_STRING_APPEND_SPRINTF(temp, vl->plugin);
+            sfree(temp);
+        }
+
+        TSDB_META_DATA_GET_STRING(meta_tag[TSDB_TAG_PLUGININSTANCE]);
+        if(temp) {
+            TSDB_STRING_APPEND_SPRINTF(temp, vl->plugin_instance);
+            sfree(temp);
+        }
+
+        TSDB_META_DATA_GET_STRING(meta_tag[TSDB_TAG_TYPE]);
+        if(temp) {
+            TSDB_STRING_APPEND_SPRINTF(temp, vl->type);
+            sfree(temp);
+        }
+
+        TSDB_META_DATA_GET_STRING(meta_tag[TSDB_TAG_TYPEINSTANCE]);
+        if(temp) {
+            TSDB_STRING_APPEND_SPRINTF(temp, vl->type_instance);
+            sfree(temp);
+        }
+
+        if(ds_name) {
+            TSDB_META_DATA_GET_STRING(meta_tag[TSDB_TAG_DSNAME]);
+            if(temp) {
+                TSDB_STRING_APPEND_SPRINTF(temp, ds_name);
+                sfree(temp);
+            }
+        }
+    } else {
+        ret[0] = '\0';
+    }
+
+#undef TSDB_META_DATA_GET_STRING
+#undef TSDB_STRING_APPEND_SPRINTF
+
+    return 0;
+}
+
+static int wt_format_name(char *ret, int ret_len,
+                          const value_list_t *vl,
+                          const struct wt_callback *cb,
+                          const char *ds_name)
+{
+    int status;
+    int i;
+    char *temp = NULL;
+    char *prefix = NULL;
     const char *meta_prefix = "tsdb_prefix";
+    char *tsdb_id = NULL;
+    const char *meta_id = "tsdb_id";
+
+    _Bool include_in_id[] = {
+        /* plugin =          */ 1,
+        /* plugin instance = */ (vl->plugin_instance[0] == '\0')?0:1,
+        /* type =            */ 1,
+        /* type instance =   */ (vl->type_instance[0] == '\0')?0:1,
+        /* ds_name =         */ (ds_name == NULL)?0:1
+    };
 
     if (vl->meta) {
         status = meta_data_get_string(vl->meta, meta_prefix, &temp);
@@ -361,54 +547,92 @@ static int wt_format_name(char *ret, int ret_len,
         } else {
             prefix = temp;
         }
-    }
 
-    if (ds_name != NULL) {
-        if (vl->plugin_instance[0] == '\0') {
-            if (vl->type_instance[0] == '\0') {
-                ssnprintf(ret, ret_len, "%s%s.%s.%s", prefix, vl->plugin,
-                        vl->type, ds_name);
-            } else {
-                ssnprintf(ret, ret_len, "%s%s.%s.%s.%s", prefix, vl->plugin,
-                        vl->type, vl->type_instance, ds_name);
-            }
-        } else { /* vl->plugin_instance != "" */
-            if (vl->type_instance[0] == '\0') {
-                ssnprintf(ret, ret_len, "%s%s.%s.%s.%s", prefix, vl->plugin,
-                        vl->plugin_instance, vl->type, ds_name);
-            } else {
-                ssnprintf(ret, ret_len, "%s%s.%s.%s.%s.%s", prefix,
-                        vl->plugin, vl->plugin_instance, vl->type,
-                        vl->type_instance, ds_name);
-            }
+        status = meta_data_get_string(vl->meta, meta_id, &temp);
+        if (status == -ENOENT) {
+            /* defaults to empty string */
+        } else if (status < 0) {
+            sfree(temp);
+            return status;
+        } else {
+            tsdb_id = temp;
         }
-    } else { /* ds_name == NULL */
-        if (vl->plugin_instance[0] == '\0') {
-            if (vl->type_instance[0] == '\0') {
-                ssnprintf(ret, ret_len, "%s%s.%s", prefix, vl->plugin,
-                        vl->type);
+
+        for(i=0; i < (sizeof(meta_tag)/sizeof(*meta_tag)); i++) {
+            if(0 == meta_data_exists(vl->meta, meta_tag[i])) {
+                /* defaults to already initialized format */
             } else {
-                ssnprintf(ret, ret_len, "%s%s.%s.%s", prefix, vl->plugin,
-                        vl->type_instance, vl->type);
-            }
-        } else { /* vl->plugin_instance != "" */
-            if (vl->type_instance[0] == '\0') {
-                ssnprintf(ret, ret_len, "%s%s.%s.%s", prefix, vl->plugin,
-                        vl->plugin_instance, vl->type);
-            } else {
-                ssnprintf(ret, ret_len, "%s%s.%s.%s.%s", prefix, vl->plugin,
-                        vl->plugin_instance, vl->type, vl->type_instance);
+                include_in_id[i] = 0;
             }
         }
     }
+    if(tsdb_id) {
+        ssnprintf(ret, ret_len, "%s%s", prefix?prefix:"", tsdb_id);
+    } else {
+#define TSDB_STRING_APPEND_STRING(string) do { \
+    const char *str = (string); \
+    size_t len = strlen(str); \
+    if(len > (remaining_len - 1)) { \
+        ptr[0] = '\0'; \
+        return(-ENOSPC); \
+    } \
+    if(len > 0) {  \
+        memcpy(ptr, str, len); \
+        ptr += len; \
+        remaining_len -= len; \
+    } \
+} while(0)
 
-    sfree(temp);
+#define TSDB_STRING_APPEND_DOT do { \
+    if(remaining_len > 2) {  \
+        ptr[0] = '.'; \
+        ptr ++; \
+        remaining_len --; \
+    } else {\
+        ptr[0] = '\0'; \
+        return(-ENOSPC); \
+    } \
+} while(0)
+
+        char *ptr = ret;
+        size_t remaining_len = ret_len;
+        if(prefix) {
+            TSDB_STRING_APPEND_STRING(prefix);
+        }
+        if(include_in_id[TSDB_TAG_PLUGIN]) {
+            TSDB_STRING_APPEND_STRING(vl->plugin);
+        }
+
+        if(include_in_id[TSDB_TAG_PLUGININSTANCE]) {
+            TSDB_STRING_APPEND_DOT;
+            TSDB_STRING_APPEND_STRING(vl->plugin_instance);
+        }
+        if(include_in_id[TSDB_TAG_TYPE]) {
+            TSDB_STRING_APPEND_DOT;
+            TSDB_STRING_APPEND_STRING(vl->type);
+        }
+        if(include_in_id[TSDB_TAG_TYPEINSTANCE]) {
+            TSDB_STRING_APPEND_DOT;
+            TSDB_STRING_APPEND_STRING(vl->type_instance);
+        }
+        if(include_in_id[TSDB_TAG_DSNAME]) {
+            TSDB_STRING_APPEND_DOT;
+            TSDB_STRING_APPEND_STRING(ds_name);
+        }
+        ptr[0] = '\0';
+#undef TSDB_STRING_APPEND_STRING
+#undef TSDB_STRING_APPEND_DOT
+    }
+
+    sfree(tsdb_id);
+    sfree(prefix);
     return 0;
 }
 
 static int wt_send_message (const char* key, const char* value,
+                            const char* value_tags,
                             cdtime_t time, struct wt_callback *cb,
-                            const char* host, meta_data_t *md)
+                            const value_list_t *vl)
 {
     int status;
     size_t message_len;
@@ -417,6 +641,8 @@ static int wt_send_message (const char* key, const char* value,
     char message[1024];
     char *host_tags = cb->host_tags ? cb->host_tags : "";
     const char *meta_tsdb = "tsdb_tags";
+    const char* host = vl->host;
+    meta_data_t *md = vl->meta;
 
     /* skip if value is NaN */
     if (value[0] == 'n')
@@ -438,11 +664,12 @@ static int wt_send_message (const char* key, const char* value,
 
     status = ssnprintf (message,
                              sizeof(message),
-                             "put %s %.0f %s fqdn=%s %s %s\r\n",
+                             "put %s %.0f %s fqdn=%s %s %s %s\r\n",
                              key,
                              CDTIME_T_TO_DOUBLE(time),
                              value,
                              host,
+                             value_tags,
                              tags,
                              host_tags);
     sfree(temp);
@@ -507,6 +734,7 @@ static int wt_write_messages(const data_set_t *ds, const value_list_t *vl,
 {
     char key[10*DATA_MAX_NAME_LEN];
     char values[512];
+    char tags[10*DATA_MAX_NAME_LEN];
 
     int status;
     size_t i;
@@ -545,8 +773,18 @@ static int wt_write_messages(const data_set_t *ds, const value_list_t *vl,
             return status;
         }
 
+        /* Copy tags from p-pi/t-ti ds notation into tags */
+        tags[0] = '\0';
+        status = wt_format_tags(tags, sizeof(tags), vl, cb, ds_name);
+        if (status != 0)
+        {
+            ERROR("write_tsdb plugin: error with format_tags");
+            return status;
+        }
+        
+
         /* Send the message to tsdb */
-        status = wt_send_message(key, values, vl->time, cb, vl->host, vl->meta);
+        status = wt_send_message(key, values, tags, vl->time, cb, vl);
         if (status != 0)
         {
             ERROR("write_tsdb plugin: error with "
