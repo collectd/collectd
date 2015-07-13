3714567a52015f3604585be2bc2f4e64a0a6818a
Michael Salmon <michael.salmon@ericsson.com>
2015-07-13 18:40:34 +0200

    Don't send warning if the old and new callback is the same

--- src/daemon/plugin.c
+++ src/daemon/plugin.c
@@ -296,12 +296,14 @@ static int register_callback (llist_t **list, /* {{{ */
 		old_cf = le->value;
 		le->value = cf;
 
-		WARNING ("plugin: register_callback: "
+		if (old_cf->cf_callback != cf->cf_callback) {
+			WARNING ("plugin: register_callback: "
 				"a callback named `%s' already exists - "
 				"overwriting the old entry!", name);
+		}
 
 		destroy_callback (old_cf);
-		sfree (key);
+		free (key);
 	}
 
 	return (0);
