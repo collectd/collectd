95e71d0a6659875380fdb5d2c8c75d18d8e1692c
Florian Forster <octo@collectd.org>
2015-07-15 09:57:41 +0200

    src/utils_latency_test.c: Log result of  DOUBLE_TO_CDTIME_T() conversion.
    The test fails on some i386 architectures. Time for some printf debugging.

--- src/utils_latency_test.c
+++ src/utils_latency_test.c
@@ -54,6 +54,8 @@ DEF_TEST(simple)
   CHECK_NOT_NULL (l = latency_counter_create ());
 
   for (i = 0; i < STATIC_ARRAY_SIZE (cases); i++) {
+    printf ("# case %zu: DOUBLE_TO_CDTIME_T(%g) = %"PRIu64"\n",
+        i, cases[i].val, DOUBLE_TO_CDTIME_T (cases[i].val));
     latency_counter_add (l, DOUBLE_TO_CDTIME_T (cases[i].val));
 
     DBLEQ (cases[i].min, CDTIME_T_TO_DOUBLE (latency_counter_get_min (l)));
