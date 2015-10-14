f15948ab1c8801d428b72ade95e6d38c9945d50d
Yves Mettier <ymettier@free.fr>
2015-10-14 17:25:13 +0100

    Bugfix on write_tsdb configuration parser

--- src/write_tsdb.c
+++ src/write_tsdb.c
@@ -980,7 +980,7 @@ static int wt_config(oconfig_item_t *ci)
 
         if (strcasecmp("Node", child->key) == 0)
             wt_config_tsd(child);
-        if (strcasecmp("DNS_Cache_TTL", child->key) == 0)
+        else if (strcasecmp("DNS_Cache_TTL", child->key) == 0)
         {
             int ttl;
             cf_util_get_int(child, &ttl);
