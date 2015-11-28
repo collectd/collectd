63f8df381c3c7d5c349fdfb1f4ea1cf6ae40d521
Ruben Kerkhof <ruben@rubenkerkhof.com>
2015-11-28 18:37:15 +0100

    write_redis: don't return with lock held

--- src/write_redis.c
+++ src/write_redis.c
@@ -70,9 +70,10 @@ static int wr_write (const data_set_t *ds, /* {{{ */
   value_ptr = &value[0];
 
   status = format_values (value_ptr, value_size, ds, vl, /* store rates = */ 0);
-  pthread_mutex_lock (&node->lock);
   if (status != 0)
     return (status);
+
+  pthread_mutex_lock (&node->lock);
 
   if (node->conn == NULL)
   {
