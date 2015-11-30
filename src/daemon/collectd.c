f89012e915b0aa0a3c65655ea156904799174299
Florian Forster <octo@collectd.org>
2015-11-30 20:27:24 +0100

    Set locale environment variables at startup.
    Fixes: #1237

--- src/daemon/collectd.c
+++ src/daemon/collectd.c
@@ -302,6 +302,11 @@ static int do_init (void)
 #if HAVE_SETLOCALE
 	if (setlocale (LC_NUMERIC, COLLECTD_LOCALE) == NULL)
 		WARNING ("setlocale (\"%s\") failed.", COLLECTD_LOCALE);
+
+	/* Update the environment, so that libraries that are calling
+	 * setlocale(LC_NUMERIC, "") don't accidentally revert these changes. */
+	unsetenv ("LC_ALL");
+	setenv ("LC_NUMERIC", COLLECTD_LOCALE, /* overwrite = */ 1);
 #endif
 
 #if HAVE_LIBKSTAT
