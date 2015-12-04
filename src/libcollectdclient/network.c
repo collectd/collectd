84bf5e22635704f34a0d456ac3d2481ebe0e3672
Ruben Kerkhof <ruben@rubenkerkhof.com>
2015-12-04 19:02:32 +0100

    libcollectdclient: don't close socket twice
    CID #38039

--- src/libcollectdclient/network.c
+++ src/libcollectdclient/network.c
@@ -83,6 +83,7 @@ static int server_close_socket (lcc_server_t *srv) /* {{{ */
     return (0);
 
   close (srv->fd);
+  srv->fd = -1;
   free (srv->sa);
   srv->sa = NULL;
   srv->sa_len = 0;
@@ -100,12 +101,6 @@ static void int_server_destroy (lcc_server_t *srv) /* {{{ */
   server_close_socket (srv);
 
   next = srv->next;
-
-  if (srv->fd >= 0)
-  {
-    close (srv->fd);
-    srv->fd = -1;
-  }
 
   free (srv->node);
   free (srv->service);
