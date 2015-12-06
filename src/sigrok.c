6be294f013b8999690c9211bdea50b2be1b351c1
Ruben Kerkhof <ruben@rubenkerkhof.com>
2015-12-06 12:09:20 +0100

    sigrok plugin: don't pass status to strerror
    CID #38120

--- src/sigrok.c
+++ src/sigrok.c
@@ -360,10 +360,13 @@ static int sigrok_init(void)
 		return -1;
 	}
 
-	if ((status = plugin_thread_create(&sr_thread, NULL, sigrok_read_thread,
-			NULL)) != 0) {
+	status = plugin_thread_create(&sr_thread, NULL, sigrok_read_thread,
+			NULL);
+	if (status != 0)
+	{
+		char errbuf[1024];
 		ERROR("sigrok plugin: Failed to create thread: %s.",
-				strerror(status));
+				sstrerror (errno, errbuf, sizeof (errbuf)));
 		return -1;
 	}
 	sr_thread_running = TRUE;
