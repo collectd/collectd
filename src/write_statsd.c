a13816dd4d8dcd665582ea73f371578ad4adec92
Stefano Pogliani <stefano@spogliani.net>
2015-12-23 00:02:08 +0100

    Pass write_statsd configuration as user data.

--- src/write_statsd.c
+++ src/write_statsd.c
@@ -26,30 +26,18 @@
 
 #define _BSD_SOURCE
 
-#include <limits.h>
-#include <stdlib.h>
+#include "collectd.h"
 
 #include <netdb.h>
-#include <sys/socket.h>
-#include <sys/types.h>
 
-#include "collectd.h"
 #include "common.h"
-#include "plugin.h"
 #include "utils_cache.h"
 
 
 /*** Compile-time constants. ***/
 #define MAX_KEY_LENGTH    1024
-#define WS_DOUBLE_FORMAT  "%f"
 #define VALUE_STR_LEN     20
 #define WRITE_STATSD_NAME "write_statsd"
-
-#define FREE_NOT_NULL(var) \
-  if (var != NULL) {       \
-    free(var);             \
-    var = NULL;            \
-  }
 
 
 /*** Type declarations. ***/
@@ -79,34 +67,15 @@ static char* DS_TYPE_TO_STATSD[] = {
   "c"   /* DS_TYPE_ABSOLUTE */
 };
 
-/*
- * The configuration is stored in a global instance and must not
- * be modified outside of load_config or initialise.
- */
-static write_statsd_config_t configuration = {
-  NULL,  // host
-  8125,  // port
-  NULL,  // postfix
-  NULL,  // prefix
-  0,     // always_append_ds
-  0,     // silence_type_warnings
-  1      // store_rates
-};
-
-
 /*** Module functions. ***/
-static void* allocate(size_t size) {
-  void* mem = (char*)malloc(size);
-  if (mem == NULL) {
-    ERROR("write_statsd plugin: not enough memory.");
-    return NULL;
-  }
-  return mem;
-}
+static int write_statsd_write(
+    const data_set_t *ds, const value_list_t *vl, user_data_t *ud);
+
 
 static char* ds_value_to_string(int type, value_t value) {
-  char* result = allocate(VALUE_STR_LEN);
+  char* result = malloc(VALUE_STR_LEN);
   if (result == NULL) {
+    ERROR("write_statsd plugin: not enough memory for value buffer.");
     return NULL;
   }
 
@@ -116,7 +85,7 @@ static char* ds_value_to_string(int type, value_t value) {
       break;
 
     case DS_TYPE_GAUGE:
-      ssnprintf(result, VALUE_STR_LEN, WS_DOUBLE_FORMAT, value.gauge);
+      ssnprintf(result, VALUE_STR_LEN, GAUGE_FORMAT, value.gauge);
       break;
 
     case DS_TYPE_DERIVE:
@@ -138,17 +107,18 @@ static char* ds_value_to_string(int type, value_t value) {
 
 
 static char* ds_rate_to_string(int type, double rate) {
-  char* result = allocate(VALUE_STR_LEN);
+  char* result = malloc(VALUE_STR_LEN);
   if (result == NULL) {
+    ERROR("write_statsd plugin: not enough memory for rates buffer.");
     return NULL;
   }
 
-  ssnprintf(result, VALUE_STR_LEN, WS_DOUBLE_FORMAT, rate);
+  ssnprintf(result, VALUE_STR_LEN, GAUGE_FORMAT, rate);
   return result;
 }
 
 
-static write_statsd_link_t open_socket(void) {
+static write_statsd_link_t open_socket(write_statsd_config_t* configuration) {
   write_statsd_link_t link  = {{0}, 0};
   struct addrinfo* resolved = NULL;
   int error = 0;
@@ -161,8 +131,8 @@ static write_statsd_link_t open_socket(void) {
   }
 
   link.server.sin_family = AF_INET;
-  link.server.sin_port   = htons(configuration.port);
-  error = getaddrinfo(configuration.host, NULL, NULL, &resolved);
+  link.server.sin_port   = htons(configuration->port);
+  error = getaddrinfo(configuration->host, NULL, NULL, &resolved);
 
   if (error) {
     ERROR("write_statsd plugin: unable to resolve address - %s.",
@@ -184,32 +154,47 @@ static int write_statsd_config(oconfig_item_t *conf) {
   int idx;
   int status;
 
+  // Initialise configuration holder.
+  write_statsd_config_t* configuration = malloc(sizeof(write_statsd_config_t));
+  if (configuration == NULL) {
+    ERROR("write_statsd plugin: not enough memory for configuration.");
+    return -1;
+  }
+  configuration->host = NULL;
+  configuration->port = 8125;
+  configuration->postfix = NULL;
+  configuration->prefix  = NULL;
+  configuration->always_append_ds = 0;
+  configuration->silence_type_warnings = 0;
+  configuration->store_rates = 1;
+
+  // Process configuration.
   for (idx = 0; idx < conf->children_num; idx++) {
     oconfig_item_t *child = conf->children + idx;
     status = 0;
 
     if (strcasecmp("Host", child->key) == 0) {
-      status = cf_util_get_string(child, &configuration.host);
+      status = cf_util_get_string(child, &configuration->host);
 
     } else if (strcasecmp("Port", child->key) == 0) {
-      status = cf_util_get_int(child, & configuration.port);
+      status = cf_util_get_int(child, &configuration->port);
       
     } else if (strcasecmp("Postfix", child->key) == 0) {
-      status = cf_util_get_string(child, &configuration.postfix);
+      status = cf_util_get_string(child, &configuration->postfix);
 
     } else if (strcasecmp("Prefix", child->key) == 0) {
-      status = cf_util_get_string(child, &configuration.prefix);
+      status = cf_util_get_string(child, &configuration->prefix);
     
     } else if (strcasecmp("SilenceTypeWarnings", child->key) == 0) {
       status = cf_util_get_boolean(
-          child, &configuration.silence_type_warnings);
+          child, &configuration->silence_type_warnings);
 
     } else if (strcasecmp("AlwaysAppendDS", child->key) == 0) {
       status = cf_util_get_boolean(
-          child, &configuration.always_append_ds);
+          child, &configuration->always_append_ds);
 
     } else if (strcasecmp("StoreRates", child->key) == 0) {
-      status = cf_util_get_boolean(child, &configuration.store_rates);
+      status = cf_util_get_boolean(child, &configuration->store_rates);
 
     } else {
       WARNING("write_statsd plugin: Ignoring unknown config option '%s'.",
@@ -219,8 +204,7 @@ static int write_statsd_config(oconfig_item_t *conf) {
     /*
      * If any of the options cannot be parsed print an error message
      * but keep going on with the defaults.
-     * If the host was not parsed correctily the plugin init step
-     * will fail for us.
+     * If the host was not parsed correctily the configuration will fail.
      */
     if (status != 0) {
       ERROR("write_statsd plugin: Ignoring config option '%s' due to an error.",
@@ -228,45 +212,76 @@ static int write_statsd_config(oconfig_item_t *conf) {
       return status;
     }
   }
-  return 0;
-}
 
-static int write_statsd_free(void) {
-  FREE_NOT_NULL(configuration.host);
-  FREE_NOT_NULL(configuration.postfix);
-  FREE_NOT_NULL(configuration.prefix);
-  DEBUG("Freed %s module.", WRITE_STATSD_NAME);
-  return 0;
-}
-
-static int write_statsd_init(void) {
-  if (configuration.host == NULL) {
+  // Check required options.
+  if (configuration->host == NULL) {
     ERROR("write_statsd plugin: missing required 'Host' configuration.");
-    FREE_NOT_NULL(configuration.postfix)
-    FREE_NOT_NULL(configuration.prefix)
-    return -1;
+    sfree(configuration->postfix);
+    sfree(configuration->prefix);
+    return -2;
   }
 
+  // Print loaded configuration (for debug).
   DEBUG("%s configuration completed.", WRITE_STATSD_NAME);
-  DEBUG("%s Host: %s", WRITE_STATSD_NAME, configuration.host);
-  DEBUG("%s Port: %i", WRITE_STATSD_NAME, configuration.port);
-  DEBUG("%s Prefix: %s", WRITE_STATSD_NAME, configuration.prefix);
-  DEBUG("%s Postfix: %s", WRITE_STATSD_NAME, configuration.postfix);
-
+  DEBUG("%s Host: %s", WRITE_STATSD_NAME, configuration->host);
+  DEBUG("%s Port: %i", WRITE_STATSD_NAME, configuration->port);
+  DEBUG("%s Prefix: %s", WRITE_STATSD_NAME, configuration->prefix);
+  DEBUG("%s Postfix: %s", WRITE_STATSD_NAME, configuration->postfix);
   DEBUG("%s AlwaysAppendDS: %i", WRITE_STATSD_NAME,
-        configuration.always_append_ds);
+        configuration->always_append_ds);
   DEBUG("%s SilenceTypeWarnings: %i", WRITE_STATSD_NAME,
-        configuration.silence_type_warnings);
-  DEBUG("%s StoreRates: %i", WRITE_STATSD_NAME, configuration.store_rates);
+        configuration->silence_type_warnings);
+  DEBUG("%s StoreRates: %i", WRITE_STATSD_NAME, configuration->store_rates);
+
+  // Register write callback with configuration as the user data.
+  user_data_t ud;
+  ud.data = (void*)configuration;
+  ud.free_func = free;
+  plugin_register_write(WRITE_STATSD_NAME, write_statsd_write, &ud);
 
   return 0;
 }
 
 
-static int write_statsd_send_message(const char* message) {
+static size_t write_statsd_format_key(
+    const value_list_t *vl, _Bool include_ds_name, char* ds_name, char* key,
+    write_statsd_config_t* configuration) {
+  size_t plugin_instance_len = strlen(vl->plugin_instance);
+  size_t type_instance_len = strlen(vl->type_instance);
+
+  // Check which parts of the name should be included.
+  _Bool has_prefix  = configuration->prefix != NULL;
+  _Bool has_postfix = configuration->postfix != NULL;
+  _Bool has_plugin_instance = plugin_instance_len != 0;
+  _Bool has_type_instance   = type_instance_len != 0;
+
+  // Format key to be :
+  //   [prefix.]host.plugin.[plugin_instance.]type[.type_instance][.ds_name][.postfix]
+  size_t length = snprintf(
+      //                    PR. H. P. PI. T . TI. DS. PO
+      key, MAX_KEY_LENGTH, "%s%s%s.%s.%s%s%s%s%s%s%s%s%s",
+      has_prefix ? configuration->prefix : "", has_prefix ? "." : "",
+      vl->host, vl->plugin,
+      vl->plugin_instance, has_plugin_instance ? "." : "",
+      vl->type, has_type_instance ? "." : "", vl->type_instance,
+      include_ds_name ? "." : "", include_ds_name ? ds_name : "",
+      has_postfix ? "." : "", has_postfix ? configuration->postfix : ""
+  );
+
+  // Check that the string would fit in MAX_KEY_LENGTH.
+  if (length >= MAX_KEY_LENGTH) {
+    ERROR("write_statsd plugin: value name too long, truncated.");
+    return MAX_KEY_LENGTH;
+  }
+  return length;
+}
+
+
+static int write_statsd_send_message(
+    const char* message, write_statsd_config_t* configuration) {
   int slen;
   int result;
-  write_statsd_link_t link = open_socket();
+  write_statsd_link_t link = open_socket(configuration);
 
   if (link.sock == 0) {
     return 0;
@@ -285,62 +300,11 @@ static int write_statsd_send_message(const char* message) {
 }
 
 
-static size_t write_statsd_format_key(
-    const value_list_t *vl, _Bool include_ds_name, char* ds_name, char* key) {
-  size_t plugin_instance_len = strlen(vl->plugin_instance);
-  size_t type_instance_len = strlen(vl->type_instance);
-
-  // Check which parts of the name should be included.
-  _Bool has_prefix  = configuration.prefix != NULL;
-  _Bool has_postfix = configuration.postfix != NULL;
-  _Bool has_plugin_instance = plugin_instance_len != 0;
-  _Bool has_type_instance   = type_instance_len != 0;
-
-  // Check that the string would fit in MAX_KEY_LENGTH.
-  size_t key_length = strlen(vl->host) + strlen(vl->plugin);
-  key_length += strlen(vl->type) + 2;
-
-  //  optionals:
-  if (has_prefix) {
-    key_length += strlen(configuration.prefix) + 1;
-  }
-  if (has_postfix) {
-    key_length += strlen(configuration.postfix) + 1;
-  }
-  if (has_plugin_instance) {
-    key_length += plugin_instance_len + 1;
-  }
-  if (has_type_instance) {
-    key_length += type_instance_len + 1;
-  }
-  if (include_ds_name) {
-    key_length += strlen(ds_name) + 1;
-  }
-
-  if (key_length >= MAX_KEY_LENGTH) {
-    ERROR("write_statsd plugin: cannot process value, name too long");
-    return 0;
-  }
-
-  // Format key to be :
-  //   [prefix.]host.plugin.[plugin_instance.]type[.type_instance][.ds_name][.postfix]
-  return snprintf(
-      //                    PR. H. P. PI. T . TI. DS. PO
-      key, MAX_KEY_LENGTH, "%s%s%s.%s.%s%s%s%s%s%s%s%s%s",
-      has_prefix ? configuration.prefix : "", has_prefix ? "." : "",
-      vl->host, vl->plugin,
-      vl->plugin_instance, has_plugin_instance ? "." : "",
-      vl->type, has_type_instance ? "." : "", vl->type_instance,
-      include_ds_name ? "." : "", include_ds_name ? ds_name : "",
-      has_postfix ? "." : "", has_postfix ? configuration.postfix : ""
-  );
-}
-
-
 static int write_statsd_write(
     const data_set_t *ds, const value_list_t *vl, user_data_t *ud) {
+  write_statsd_config_t* configuration = (write_statsd_config_t*)ud->data;
   int   idx;
-  _Bool include_ds_name = configuration.always_append_ds || ds->ds_num > 1;
+  _Bool include_ds_name = configuration->always_append_ds || ds->ds_num > 1;
 
   /*
    * Build the type name using all available fields in vl:
@@ -349,12 +313,13 @@ static int write_statsd_write(
   gauge_t* rates = NULL;
   char*    key = NULL;
 
-  key = allocate(MAX_KEY_LENGTH);
+  key = malloc(MAX_KEY_LENGTH);
   if (key == NULL) {
+    ERROR("write_statsd plugin: not enough memory for key buffer.");
     return -5;
   }
 
-  if (configuration.store_rates) {
+  if (configuration->store_rates) {
     rates = uc_get_rate(ds, vl);
     if (rates == NULL) {
       ERROR("write_statsd plugin: uc_get_rate failed.");
@@ -373,7 +338,7 @@ static int write_statsd_write(
     char*  value = NULL;
 
     if (ds_type == NULL) {
-      if (!configuration.silence_type_warnings) {
+      if (!configuration->silence_type_warnings) {
         WARNING("write_statsd plugin: unsupported StatsD type '%s' "
                 "for value with name '%s'.",
                 DS_TYPE_TO_STRING(ds->ds[idx].type), ds->ds[idx].name);
@@ -398,7 +363,7 @@ static int write_statsd_write(
      * the type name, the value name, value and type identifier.
      */
     actual_key_length = write_statsd_format_key(
-        vl, include_ds_name, ds->ds[idx].name, key);
+        vl, include_ds_name, ds->ds[idx].name, key, configuration);
     if (actual_key_length <= 0) {
       ERROR("write_statsd plugin: unable to get key from data set.");
       free(value);
@@ -410,8 +375,9 @@ static int write_statsd_write(
     message_len += strlen(ds_type);
     message_len += 3;  // For the ": " and "|" parts.
 
-    message = allocate(message_len + 1);
+    message = malloc(message_len + 1);
     if (message == NULL) {
+      ERROR("write_statsd plugin: not enough memory for message buffer.");
       free(key);
       free(value);
       return -7;
@@ -420,7 +386,7 @@ static int write_statsd_write(
     snprintf(message, message_len + 1, "%s: %s|%s", key, value, ds_type);
     free(value);
 
-    result = write_statsd_send_message(message);
+    result = write_statsd_send_message(message, configuration);
     free(message);
     if (result != 0) {
       free(key);
@@ -436,9 +402,5 @@ static int write_statsd_write(
 
 void module_register(void) {
   plugin_register_complex_config(WRITE_STATSD_NAME, write_statsd_config);
-  plugin_register_init(WRITE_STATSD_NAME, write_statsd_init);
-  plugin_register_shutdown(WRITE_STATSD_NAME, write_statsd_free);
-  plugin_register_write(WRITE_STATSD_NAME, write_statsd_write, NULL);
   DEBUG("Registered %s module.", WRITE_STATSD_NAME);
 }
-
