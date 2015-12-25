92811dfcb772a4613b0938b010869cab536acc3e
Ruben Kerkhof <ruben@rubenkerkhof.com>
2015-12-25 11:19:29 +0100

    uuid plugin: make it work on NetBSD

--- src/uuid.c
+++ src/uuid.c
@@ -114,7 +114,7 @@ uuid_get_from_dmidecode(void)
     return (uuid);
 }
 
-#if defined(__APPLE__) || defined(__FreeBSD__)
+#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__NetBSD__)
 static char *
 uuid_get_from_sysctlbyname(const char *name)
 {
@@ -214,6 +214,9 @@ uuid_get_local(void)
         return (uuid);
 #elif defined(__FreeBSD__)
     if ((uuid = uuid_get_from_sysctlbyname("kern.hostuuid")) != NULL)
+        return (uuid);
+#elif defined(__NetBSD__)
+    if ((uuid = uuid_get_from_sysctlbyname("machdep.dmi.system-uuid")) != NULL)
         return (uuid);
 #elif defined(__linux__)
     if ((uuid = uuid_get_from_file("/sys/class/dmi/id/product_uuid")) != NULL)
