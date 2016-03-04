0b37c599e9299091655b1c1f0e6ed4dd1b7ddf4f
Ruben Kerkhof <ruben@rubenkerkhof.com>
2016-03-04 21:08:30 +0100

    zookeeper plugin: constify

--- src/zookeeper.c
+++ src/zookeeper.c
@@ -108,8 +108,8 @@ static int zookeeper_connect (void)
 	struct addrinfo ai_hints;
 	struct addrinfo *ai;
 	struct addrinfo *ai_list;
-	char *host;
-	char *port;
+	const char *host;
+	const char *port;
 
 	memset ((void *) &ai_hints, '\0', sizeof (ai_hints));
 	ai_hints.ai_family   = AF_UNSPEC;
