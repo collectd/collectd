dd81ee8b3c4d50c27e49995db5618c429d073e2c
Ruben Kerkhof <ruben@rubenkerkhof.com>
2016-04-16 13:49:01 +0200

    tail_csv plugin: set pointer to NULL not 0
    Found with coccinelle

--- src/tail_csv.c
+++ src/tail_csv.c
@@ -85,7 +85,7 @@ static int tcsv_submit (instance_definition_t *id,
 static cdtime_t parse_time (char const *tbuf)
 {
     double t;
-    char *endptr = 0;
+    char *endptr = NULL;
 
     errno = 0;
     t = strtod (tbuf, &endptr);
