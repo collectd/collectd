8101a5c5d1487bccf7922961ccb4dc54d92a761d
Ruben Kerkhof <ruben@rubenkerkhof.com>
2016-04-23 10:25:40 +0200

    daemon/common.c: remove unused function
    Found with cppcheck

--- src/daemon/common.c
+++ src/daemon/common.c
@@ -373,27 +373,6 @@ int strjoin (char *buffer, size_t buffer_size,
 	return ((int) strlen (buffer));
 }
 
-int strsubstitute (char *str, char c_from, char c_to)
-{
-	int ret;
-
-	if (str == NULL)
-		return (-1);
-
-	ret = 0;
-	while (*str != '\0')
-	{
-		if (*str == c_from)
-		{
-			*str = c_to;
-			ret++;
-		}
-		str++;
-	}
-
-	return (ret);
-} /* int strsubstitute */
-
 int escape_string (char *buffer, size_t buffer_size)
 {
   char *temp;
