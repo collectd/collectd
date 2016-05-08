f1538c3b02b7c3caa080f8a5d4d572cd3f662dd0
Pavel Rochnyack <pavel2000@ngs.ru>
2016-05-08 14:10:08 +0600

    apcups plugin: Change "PersistentConnection" option default value depending on poll Interval.

--- src/apcups.c
+++ src/apcups.c
@@ -378,6 +378,7 @@ static int apc_query_server (char const *node, char const *service,
 static int apcups_config (oconfig_item_t *ci)
 {
 	int i;
+	_Bool persistent_conn_set = 0;
 
 	for (i = 0; i < ci->children_num; i++)
 	{
@@ -389,10 +390,23 @@ static int apcups_config (oconfig_item_t *ci)
 			cf_util_get_service (child, &conf_service);
 		else if (strcasecmp (child->key, "ReportSeconds") == 0)
 			cf_util_get_boolean (child, &conf_report_seconds);
-		else if (strcasecmp (child->key, "PersistentConnection") == 0)
+		else if (strcasecmp (child->key, "PersistentConnection") == 0) {
 			cf_util_get_boolean (child, &conf_persistent_conn);
+			persistent_conn_set = 1;
+		}
 		else
 			ERROR ("apcups plugin: Unknown config option \"%s\".", child->key);
+	}
+
+	if (!persistent_conn_set) {
+		double interval = CDTIME_T_TO_DOUBLE(plugin_get_interval());
+		if (interval > 15.0) {
+			NOTICE ("apcups plugin: Plugin poll interval set to %.3f seconds. "
+				"Apcupsd NIS socket timeout is 15 seconds, "
+				"PersistentConnection disabled by default.",
+				interval);
+			conf_persistent_conn = 0;
+		}
 	}
 
 	return (0);
