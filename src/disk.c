collectd/collectd

commit bdbc066e4646876a774e940995859a4760b8946c

disk plugin: Call disk_udev_attr_name() only when UdevNameAttr configured

src/disk.c
@@ -867,9 +867,13 @@ static int disk_read (void)
 		output_name = disk_name;
 
 #if HAVE_LIBUDEV
-		char *alt_name = disk_udev_attr_name (handle_udev, disk_name, conf_udev_name_attr);
-		if (alt_name != NULL)
-			output_name = alt_name;
+		char *alt_name = NULL;
+		if (conf_udev_name_attr != NULL)
+		{
+			alt_name = disk_udev_attr_name (handle_udev, disk_name, conf_udev_name_attr);
+			if (alt_name != NULL)
+				output_name = alt_name;
+		}
 #endif
 
 		if (ignorelist_match (ignorelist, output_name) != 0)

commit 6f9ba64739210907df09e862983a0b5255dae233

Plug a memory leak in disk plugin
Introduced by bdbc066 so no need to backport.
CID 116311

src/disk.c
@@ -893,7 +893,13 @@ static int disk_read (void)
 #endif
 
 		if (ignorelist_match (ignorelist, output_name) != 0)
+		{
+#if HAVE_LIBUDEV
+			/* release udev-based alternate name, if allocated */
+			sfree (output_name);
+#endif
 			continue;
+		}
 
 		if ((ds->read_bytes != 0) || (ds->write_bytes != 0))
 			disk_submit (output_name, "disk_octets",