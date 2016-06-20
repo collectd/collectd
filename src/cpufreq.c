0e9775872e061d001f53757c7a8a3811127d072d
Igor Solovyov <Igor.Solovyov@gmail.com>
2016-06-21 01:57:10 +0300

    Tested version.

--- src/cpufreq.c
+++ src/cpufreq.c
@@ -31,7 +31,7 @@
 static int num_cpu = 0;
 
 static char const * freq_fname_def = "/sys/devices/system/cpu/cpu%d/cpufreq/"
-                                     "scaling_cur_freq";
+									 "scaling_cur_freq";
 /**
  * Linux kernel exposes current CPU frequency via
  * "/sys/devices/system/cpu/cpuX/cpufreq/scaling_cur_freq" file.
@@ -45,10 +45,10 @@ static char const * freq_fname_def = "/sys/devices/system/cpu/cpu%d/cpufreq/"
  * This file exposes not exactly the same thing as "scaling_cur_freq" does, but close by nature.
  *
  * Thus it's better to have a workaround to monitor some CPU frequency on even "broken" kernels.
- * So, the optional paramer "Path" was introduced to specify alternative CPU frequency path.
+ * So, the optional parameter "Path" was introduced to specify alternative CPU frequency path.
  * Example:
- * <Plugin cpufreq>/sys/devices/system/cpu/cpu%d/cpufreq/cpuinfo_cur_freq
- *     Path ""
+ * <Plugin cpufreq>
+ *     Path "/sys/devices/system/cpu/cpu%d/cpufreq/cpuinfo_cur_freq"
  * </Plugin>
  *
  * Note, "%d" has to be used only once as CPU index placeholder.
@@ -58,173 +58,173 @@ static char freq_fname[MAX_STR_L] = {0};
 
 static const char *config_keys[] =
 {
-    "Path"
+	"Path"
 };
 static int config_keys_num = STATIC_ARRAY_SIZE (config_keys);
 
 int check_format (char const * src)
 {
-    int pc = -2;
-    int d = -2;
+	int pc = -2;
+	int d = -2;
 
-    for (char const * p=src; *p; ++p) {
-        switch (*p)
-        {
-            case '%':
-                if (-2 == pc) pc=p-src;
-                else return 0;
-                break;
+	for (char const * p=src; *p; ++p) {
+		switch (*p)
+		{
+			case '%':
+				if (-2 == pc) pc=p-src;
+				else return 0;
+				break;
 
-            case 'd':
-                if (pc+1 == p-src) d=p-src;
-                break;
-        }
-    }
+			case 'd':
+				if (pc+1 == p-src) d=p-src;
+				break;
+		}
+	}
 
-    return pc+1==d;
+	return pc+1==d;
 } /* check_format */
 
 static int cpufreq_config (char const * key, char const * value)
 {
-    if (strcasecmp ("Path", key) == 0) {
-        if (check_format (value)) {
-            sstrncpy(freq_fname, value, sizeof(freq_fname));
-        }
-        else {
-            WARNING ("cpufreq: Path parameter is wrong: %s", value);
-            return -1;
-        }
-    }
+	if (strcasecmp ("Path", key) == 0) {
+		if (check_format (value)) {
+			sstrncpy(freq_fname, value, sizeof(freq_fname));
+		}
+		else {
+			WARNING ("cpufreq: Path parameter is wrong: %s", value);
+			return -1;
+		}
+	}
 
-    return 0;
+	return 0;
 } /* cpufreq_config */
 
 static int setup_freq_fname ()
 {
-    int status;
-    char filename[MAX_STR_L];
+	int status;
+	char filename[MAX_STR_L];
 
-    if (!freq_fname[0]) {
-        sstrncpy (freq_fname, freq_fname_def, sizeof (freq_fname));
-    }
+	if (!freq_fname[0]) {
+		sstrncpy (freq_fname, freq_fname_def, sizeof (freq_fname));
+	}
 
-    status = ssnprintf (filename, sizeof (filename), freq_fname, 0);
-    if ((status < 1) || ((unsigned int)status >= sizeof (filename)))
-        return 0;
+	status = ssnprintf (filename, sizeof (filename), freq_fname, 0);
+	if ((status < 1) || ((unsigned int)status >= sizeof (filename)))
+		return 0;
 
-    if (!access (filename, R_OK)) {
-        return 1;
-    }
+	if (!access (filename, R_OK)) {
+		return 1;
+	}
 
-    return 0;
+	return 0;
 } /* setup_freq_fname */
 
 static int cpufreq_init (void)
 {
-    int status;
-    char filename[MAX_STR_L];
+	int status;
+	char filename[MAX_STR_L];
 
-    if (!setup_freq_fname ()) {
-        return -1;
-    }
+	if (!setup_freq_fname ()) {
+		return -1;
+	}
 
-    num_cpu = 0;
+	num_cpu = 0;
 
-    while (1)
-    {
-        status = ssnprintf (filename, sizeof (filename), freq_fname, num_cpu);
-        if ((status < 1) || ((unsigned int)status >= sizeof (filename)))
-            break;
+	while (1)
+	{
+		status = ssnprintf (filename, sizeof (filename), freq_fname, num_cpu);
+		if ((status < 1) || ((unsigned int)status >= sizeof (filename)))
+			break;
 
-        if (access (filename, R_OK))
-            break;
+		if (access (filename, R_OK))
+			break;
 
-        num_cpu++;
-    }
+		num_cpu++;
+	}
 
-    INFO ("cpufreq plugin: Found %d CPU%s", num_cpu,
-            (num_cpu == 1) ? "" : "s");
+	INFO ("cpufreq plugin: Found %d CPU%s", num_cpu,
+			(num_cpu == 1) ? "" : "s");
 
-    if (num_cpu == 0)
-        return -1;
+	if (num_cpu == 0)
+		return -1;
 
-    return 0;
+	return 0;
 } /* int cpufreq_init */
 
 static void cpufreq_submit (int cpu_num, double value)
 {
-    value_t values[1];
-    value_list_t vl = VALUE_LIST_INIT;
+	value_t values[1];
+	value_list_t vl = VALUE_LIST_INIT;
 
-    values[0].gauge = value;
+	values[0].gauge = value;
 
-    vl.values = values;
-    vl.values_len = 1;
-    sstrncpy (vl.host, hostname_g, sizeof (vl.host));
-    sstrncpy (vl.plugin, "cpufreq", sizeof (vl.plugin));
-    sstrncpy (vl.type, "cpufreq", sizeof (vl.type));
-    ssnprintf (vl.type_instance, sizeof (vl.type_instance),
-            "%i", cpu_num);
+	vl.values = values;
+	vl.values_len = 1;
+	sstrncpy (vl.host, hostname_g, sizeof (vl.host));
+	sstrncpy (vl.plugin, "cpufreq", sizeof (vl.plugin));
+	sstrncpy (vl.type, "cpufreq", sizeof (vl.type));
+	ssnprintf (vl.type_instance, sizeof (vl.type_instance),
+			"%i", cpu_num);
 
-    plugin_dispatch_values (&vl);
+	plugin_dispatch_values (&vl);
 }
 
 static int cpufreq_read (void)
 {
-    int status;
-    unsigned long long val;
-    int i = 0;
-    FILE *fp;
-    char filename[MAX_STR_L];
-    char buffer[16];
+	int status;
+	unsigned long long val;
+	int i = 0;
+	FILE *fp;
+	char filename[MAX_STR_L];
+	char buffer[16];
 
-    for (i = 0; i < num_cpu; i++)
-    {
-        status = ssnprintf (filename, sizeof (filename), freq_fname, i);
-        if ((status < 1) || ((unsigned int)status >= sizeof (filename)))
-            return (-1);
+	for (i = 0; i < num_cpu; i++)
+	{
+		status = ssnprintf (filename, sizeof (filename), freq_fname, i);
+		if ((status < 1) || ((unsigned int)status >= sizeof (filename)))
+			return (-1);
 
-        if ((fp = fopen (filename, "r")) == NULL)
-        {
-            char errbuf[1024];
-            WARNING ("cpufreq: fopen (%s): %s", filename,
-                    sstrerror (errno, errbuf,
-                        sizeof (errbuf)));
-            return (-1);
-        }
+		if ((fp = fopen (filename, "r")) == NULL)
+		{
+			char errbuf[1024];
+			WARNING ("cpufreq: fopen (%s): %s", filename,
+					sstrerror (errno, errbuf,
+						sizeof (errbuf)));
+			return (-1);
+		}
 
-        if (fgets (buffer, 16, fp) == NULL)
-        {
-            char errbuf[1024];
-            WARNING ("cpufreq: fgets: %s",
-                    sstrerror (errno, errbuf,
-                        sizeof (errbuf)));
-            fclose (fp);
-            return (-1);
-        }
+		if (fgets (buffer, 16, fp) == NULL)
+		{
+			char errbuf[1024];
+			WARNING ("cpufreq: fgets: %s",
+					sstrerror (errno, errbuf,
+						sizeof (errbuf)));
+			fclose (fp);
+			return (-1);
+		}
 
-        if (fclose (fp))
-        {
-            char errbuf[1024];
-            WARNING ("cpufreq: fclose: %s",
-                    sstrerror (errno, errbuf,
-                        sizeof (errbuf)));
-        }
+		if (fclose (fp))
+		{
+			char errbuf[1024];
+			WARNING ("cpufreq: fclose: %s",
+					sstrerror (errno, errbuf,
+						sizeof (errbuf)));
+		}
 
 
-        /* You're seeing correctly: The file is reporting kHz values.. */
-        val = atoll (buffer) * 1000;
+		/* You're seeing correctly: The file is reporting kHz values.. */
+		val = atoll (buffer) * 1000;
 
-        cpufreq_submit (i, val);
-    }
+		cpufreq_submit (i, val);
+	}
 
-    return (0);
+	return (0);
 } /* int cpufreq_read */
 
 void module_register (void)
 {
-    plugin_register_config ("cpufreq", cpufreq_config,
-                            config_keys, config_keys_num);
-    plugin_register_init ("cpufreq", cpufreq_init);
-    plugin_register_read ("cpufreq", cpufreq_read);
+	plugin_register_config ("cpufreq", cpufreq_config,
+							config_keys, config_keys_num);
+	plugin_register_init ("cpufreq", cpufreq_init);
+	plugin_register_read ("cpufreq", cpufreq_read);
 }
