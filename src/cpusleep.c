c8a4558dd36e594c7453982a18724e02b26ba328
Rinigus <rinigus.git@gmail.com>
2016-07-05 22:56:31 +0300

    typo in error message fixed

--- src/cpusleep.c
+++ src/cpusleep.c
@@ -59,13 +59,13 @@ static int cpusleep_read (void)
   struct timespec b, m;
   if ( clock_gettime(CLOCK_BOOTTIME, &b ) < 0 )
     {
-      ERROR("cpusleep plugin: clock_gettime CLOCK_MONOTONIC failed");
+      ERROR("cpusleep plugin: clock_boottime failed");
       return (-1);
     }
 
   if ( clock_gettime(CLOCK_MONOTONIC, &m ) < 0 )
     {
-      ERROR("cpusleep plugin: clock_gettime CLOCK_MONOTONIC failed");
+      ERROR("cpusleep plugin: clock_monotonic failed");
       return (-1);
     }
 
