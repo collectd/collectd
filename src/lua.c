c42be8ddd05812768837d63313650e7b66b4ab8e
Ruben Kerkhof <ruben@rubenkerkhof.com>
2016-08-12 20:51:24 +0200

    lua_cb_register_read: return errors to Lua
    So Lua can catch them with a pcall

--- src/lua.c
+++ src/lua.c
@@ -312,43 +312,27 @@ static int lua_cb_register_read(lua_State *L) /* {{{ */
 {
   int nargs = lua_gettop(L);
 
-  if (nargs != 1) {
-    WARNING("Lua plugin: collectd.register_read() called with an invalid "
-            "number of arguments (%i).",
-            nargs);
-    RETURN_LUA(L, -1);
-  }
+  if (nargs != 1)
+    return luaL_error(L, "Invalid number of arguments (%d != 1)", nargs);
 
-  char function_name[DATA_MAX_NAME_LEN] = "";
+  luaL_checktype(L, 1, LUA_TFUNCTION);
 
-  if (lua_isstring(L, 1)) {
-    const char *tmp = lua_tostring(L, 1);
-    ssnprintf(function_name, sizeof(function_name), "lua/%s", tmp);
-  }
+  char function_name[DATA_MAX_NAME_LEN];
+  ssnprintf(function_name, sizeof(function_name), "lua/%s", lua_tostring(L, 1));
 
   int callback_id = clua_store_callback(L, 1);
-  if (callback_id < 0) {
-    ERROR("Lua plugin: Storing callback function failed.");
-    RETURN_LUA(L, -1);
-  }
+  if (callback_id < 0)
+    return luaL_error(L, "%s", "Storing callback function failed");
 
   lua_State *thread = lua_newthread(L);
-  if (thread == NULL) {
-    ERROR("Lua plugin: lua_newthread failed.");
-    RETURN_LUA(L, -1);
-  }
+  if (thread == NULL)
+    return luaL_error(L, "%s", "lua_newthread failed");
   clua_store_thread(L, -1);
   lua_pop(L, 1);
 
-  if (function_name[0] == '\0')
-    ssnprintf(function_name, sizeof(function_name), "lua/callback_%i",
-              callback_id);
-
   clua_callback_data_t *cb = calloc(1, sizeof(*cb));
-  if (cb == NULL) {
-    ERROR("Lua plugin: calloc failed.");
-    RETURN_LUA(L, -1);
-  }
+  if (cb == NULL)
+    return luaL_error(L, "%s", "calloc failed");
 
   cb->lua_state = thread;
   cb->callback_id = callback_id;
@@ -359,15 +343,15 @@ static int lua_cb_register_read(lua_State *L) /* {{{ */
     .data = cb
   };
 
-  plugin_register_complex_read(/* group = */ "lua",
-                               /* name      = */ function_name,
-                               /* callback  = */ clua_read,
-                               /* interval  = */ 0,
-                               /* user_data = */ &ud);
+  int status = plugin_register_complex_read(/* group = */ "lua",
+                                            /* name      = */ function_name,
+                                            /* callback  = */ clua_read,
+                                            /* interval  = */ 0,
+                                            /* user_data = */ &ud);
 
-  DEBUG("Lua plugin: Successful call to lua_cb_register_read().");
-
-  RETURN_LUA(L, 0);
+  if (status != 0)
+    return luaL_error(L, "%s", "plugin_register_complex_read failed");
+  return 0;
 } /* }}} int lua_cb_register_read */
 
 static int lua_cb_register_write(lua_State *L) /* {{{ */
