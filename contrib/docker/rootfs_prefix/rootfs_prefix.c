3d0f82ce316e9712e08493d21e7e792d62dff98e
Ruben Kerkhof <ruben@rubenkerkhof.com>
2016-11-29 17:26:37 +0100

    contrib/docker: cleanup wrapper a bit
    - No need to declare errno
    - Remove one level of indentation

--- contrib/docker/rootfs_prefix/rootfs_prefix.c
+++ contrib/docker/rootfs_prefix/rootfs_prefix.c
@@ -11,28 +11,22 @@
 #define BUFSIZE 256
 
 const char *add_prefix(const char *orig, char *prefixed) {
-  int status;
-  int errno;
-
-  if ((strncmp(orig, "/proc", 5) == 0) || (strncmp(orig, "/sys", 4) == 0)) {
-
-    status = snprintf(prefixed, BUFSIZE, "%s%s", PREFIX, orig);
-    if ((unsigned int)status >= BUFSIZE) {
-      error(status, ENAMETOOLONG,
-            "'%s' got truncated when adding '%s' prefix: '%s'", orig, PREFIX,
-            prefixed);
-      return orig;
-    } else if (status < 1) {
-      error(status, errno,
-            "adding '%s' prefix to file path failed: '%s' -> '%s'", PREFIX,
-            orig, prefixed);
-      return orig;
-    } else {
-      return (const char *)prefixed;
-    }
-
-  } else {
+  if ((strncmp(orig, "/proc", 5) != 0) && (strncmp(orig, "/sys", 4) != 0))
     return orig;
+
+  int status = snprintf(prefixed, BUFSIZE, "%s%s", PREFIX, orig);
+  if ((unsigned int)status >= BUFSIZE) {
+    error(status, ENAMETOOLONG,
+          "'%s' got truncated when adding '%s' prefix: '%s'", orig, PREFIX,
+          prefixed);
+    return orig;
+  } else if (status < 1) {
+    error(status, errno,
+          "adding '%s' prefix to file path failed: '%s' -> '%s'", PREFIX,
+          orig, prefixed);
+    return orig;
+  } else {
+    return (const char *)prefixed;
   }
 }
 
