e40075040c17ab9826fab713bcf8ee4518b0778a
Florian Forster <octo@collectd.org>
2016-12-16 18:42:13 +0100

    src/utils_cmd_getthreshold.c: Fix formatting.

--- src/utils_cmd_getthreshold.c
+++ src/utils_cmd_getthreshold.c
@@ -155,48 +155,31 @@ int handle_getthreshold(FILE *fh, char *buffer) {
   print_to_socket(fh, "%zu Threshold found\n", i);
 
   if (threshold.host[0] != 0)
-    print_to_socket(fh, "Host: %s\n", threshold.host) if (
-        threshold.plugin[0] !=
-        0) print_to_socket(fh, "Plugin: %s\n",
-                           threshold.plugin) if (threshold.plugin_instance[0] !=
-                                                 0)
-        print_to_socket(fh, "Plugin Instance: %s\n",
-                        threshold.plugin_instance) if (threshold.type[0] != 0)
-            print_to_socket(fh, "Type: %s\n", threshold.type) if (
-                threshold.type_instance[0] !=
-                0) print_to_socket(fh, "Type Instance: %s\n",
-                                   threshold
-                                       .type_instance) if (threshold.data_source
-                                                               [0] != 0)
-                print_to_socket(
-                    fh, "Data Source: %s\n",
-                    threshold.data_source) if (!isnan(threshold.warning_min))
-                    print_to_socket(
-                        fh, "Warning Min: %g\n",
-                        threshold
-                            .warning_min) if (!isnan(threshold.warning_max))
-                        print_to_socket(
-                            fh, "Warning Max: %g\n",
-                            threshold
-                                .warning_max) if (!isnan(threshold.failure_min))
-                            print_to_socket(
-                                fh, "Failure Min: %g\n",
-                                threshold
-                                    .failure_min) if (!isnan(threshold
-                                                                 .failure_max))
-                                print_to_socket(
-                                    fh, "Failure Max: %g\n",
-                                    threshold.failure_max) if (threshold
-                                                                   .hysteresis >
-                                                               0.0)
-                                    print_to_socket(
-                                        fh, "Hysteresis: %g\n",
-                                        threshold.hysteresis) if (threshold
-                                                                      .hits > 1)
-                                        print_to_socket(fh, "Hits: %i\n",
-                                                        threshold.hits)
+    print_to_socket(fh, "Host: %s\n", threshold.host);
+  if (threshold.plugin[0] != 0)
+    print_to_socket(fh, "Plugin: %s\n", threshold.plugin);
+  if (threshold.plugin_instance[0] != 0)
+    print_to_socket(fh, "Plugin Instance: %s\n", threshold.plugin_instance);
+  if (threshold.type[0] != 0)
+    print_to_socket(fh, "Type: %s\n", threshold.type);
+  if (threshold.type_instance[0] != 0)
+    print_to_socket(fh, "Type Instance: %s\n", threshold.type_instance);
+  if (threshold.data_source[0] != 0)
+    print_to_socket(fh, "Data Source: %s\n", threshold.data_source);
+  if (!isnan(threshold.warning_min))
+    print_to_socket(fh, "Warning Min: %g\n", threshold.warning_min);
+  if (!isnan(threshold.warning_max))
+    print_to_socket(fh, "Warning Max: %g\n", threshold.warning_max);
+  if (!isnan(threshold.failure_min))
+    print_to_socket(fh, "Failure Min: %g\n", threshold.failure_min);
+  if (!isnan(threshold.failure_max))
+    print_to_socket(fh, "Failure Max: %g\n", threshold.failure_max);
+  if (threshold.hysteresis > 0.0)
+    print_to_socket(fh, "Hysteresis: %g\n", threshold.hysteresis);
+  if (threshold.hits > 1)
+    print_to_socket(fh, "Hits: %i\n", threshold.hits);
 
-                                            return (0);
+  return (0);
 } /* int handle_getthreshold */
 
 /* vim: set sw=2 sts=2 ts=8 et : */
