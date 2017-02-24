d6a8ea79d494d8cda8d625f45619e8fd23f83c41
Kamil Wiatrowski <kamilx.wiatrowski@intel.com>
2017-02-24 07:44:07 +0000

    pcie: plugin to read PCIe errors - log parsing
    Use utils_message_parser to parse syslog for AER errors.
    Errors are reported as notifications.
    
    Change-Id: Ie26f51b57d2ad743bf1c31feccd45c68a72f8c2e
    Signed-off-by: Kamil Wiatrowski <kamilx.wiatrowski@intel.com>

--- src/pcie.c
+++ src/pcie.c
@@ -26,9 +26,12 @@
  **/
 
 #include "collectd.h"
+
 #include "common.h"
 #include "utils_llist.h"
+#include "utils_message_parser.h"
 
+#include <fnmatch.h>
 #include <linux/pci_regs.h>
 
 #define PCIE_PLUGIN "pcie"
@@ -47,11 +50,23 @@
 
 #define PCIE_ECAP_OFFSET 0x100 /* ECAP always begin at offset 0x100 */
 
+#define PCIE_LOG_PORT "root port"
+#define PCIE_LOG_SEVERITY "severity"
+#define PCIE_LOG_DEV "device"
+#define PCIE_LOG_TYPE "error type"
+#define PCIE_LOG_ID "id"
+
 typedef struct pcie_config_s {
   _Bool use_sysfs;
   _Bool notif_masked;
   _Bool persistent;
-  char access_dir[PCIE_NAME_LEN];
+  _Bool first_read;
+  _Bool read_devices;
+  _Bool read_log;
+  _Bool default_patterns;
+  char access_dir[PATH_MAX];
+  char logfile[PATH_MAX];
+  _Bool config_error;
 } pcie_config_t;
 
 typedef struct pcie_device_s {
@@ -81,8 +96,16 @@ typedef struct pcie_error_s {
   const char *desc;
 } pcie_error_t;
 
+typedef struct pcie_msg_parser_s {
+  char name[PCIE_NAME_LEN];
+  parser_job_data *job;
+  message_pattern *patterns;
+  size_t patterns_len;
+} pcie_msg_parser_t;
+
 static llist_t *pcie_dev_list;
-static pcie_config_t pcie_config = {.access_dir = ""};
+static pcie_config_t pcie_config = {.access_dir = "",
+                                    .logfile = "/var/log/syslog"};
 static pcie_fops_t pcie_fops;
 
 /* Device Error Status */
@@ -125,24 +148,53 @@ static pcie_error_t pcie_aer_ces[] = {
     {PCI_ERR_COR_LOG_OVER, "Header Log Overflow"}};
 static const int pcie_aer_ces_num = STATIC_ARRAY_SIZE(pcie_aer_ces);
 
-static void pcie_add_device(llist_t *list, int domain, uint8_t bus,
-                            uint8_t device, uint8_t fn) {
+static pcie_msg_parser_t *pcie_parsers = NULL;
+static size_t pcie_parsers_len = 0;
+
+/* Default patterns for AER errors in syslog */
+static message_pattern pcie_default_patterns[] = {
+    {.name = PCIE_LOG_PORT,
+     .regex = "pcieport (.*): AER:",
+     .submatch_idx = 1,
+     .is_mandatory = 1},
+    {.name = PCIE_LOG_DEV,
+     .regex = " ([0-9a-fA-F:\\.]*): PCIe Bus Error",
+     .submatch_idx = 1,
+     .is_mandatory = 1},
+    {.name = PCIE_LOG_SEVERITY,
+     .regex = "severity=([^,]*)",
+     .submatch_idx = 1,
+     .is_mandatory = 1},
+    {.name = PCIE_LOG_TYPE,
+     .regex = "type=(.*),",
+     .submatch_idx = 1,
+     .is_mandatory = 0},
+    {.name = PCIE_LOG_ID,
+     .regex = ", id=(.*)",
+     .submatch_idx = 1,
+     .is_mandatory = 1}};
+
+static int pcie_add_device(llist_t *list, int domain, uint8_t bus,
+                           uint8_t device, uint8_t fn) {
   llentry_t *entry;
-  pcie_device_t *dev = smalloc(sizeof(*dev));
+  pcie_device_t *dev = calloc(1, sizeof(*dev));
+  if (dev == NULL) {
+    ERROR(PCIE_PLUGIN ": Failed to allocate device");
+    return -ENOMEM;
+  }
 
   dev->domain = domain;
   dev->bus = bus;
   dev->device = device;
   dev->function = fn;
-  dev->excluded = 0;
   dev->cap_exp = -1;
   dev->ecap_aer = -1;
-  dev->aer_not_found = 0;
   entry = llentry_create(NULL, dev);
   llist_append(list, entry);
 
   DEBUG(PCIE_PLUGIN ": pci device added to list: %04x:%02x:%02x.%d", domain,
         bus, device, fn);
+  return 0;
 }
 
 static void pcie_clear_list(llist_t *list) {
@@ -161,6 +213,7 @@ static int pcie_list_devices_proc(llist_t *dev_list) {
   char file_name[PCIE_NAME_LEN];
   char buf[PCIE_BUFF_SIZE];
   unsigned int i = 0;
+  int ret = 0;
 
   if (dev_list == NULL)
     return -EINVAL;
@@ -186,19 +239,22 @@ static int pcie_list_devices_proc(llist_t *dev_list) {
     bus = slot >> 8U;
     dev = PCIE_DEV(slot);
     fn = PCIE_FN(slot);
-    pcie_add_device(dev_list, 0, bus, dev, fn);
+    ret = pcie_add_device(dev_list, 0, bus, dev, fn);
+    if (ret)
+      break;
 
     ++i;
   }
 
   fclose(fd);
-  return 0;
+  return ret;
 }
 
 static int pcie_list_devices_sysfs(llist_t *dev_list) {
   DIR *dir;
   struct dirent *item;
   char dir_name[PCIE_NAME_LEN];
+  int ret = 0;
 
   if (dev_list == NULL)
     return -EINVAL;
@@ -224,11 +280,13 @@ static int pcie_list_devices_sysfs(llist_t *dev_list) {
       continue;
     }
 
-    pcie_add_device(dev_list, dom, bus, dev, fn);
+    ret = pcie_add_device(dev_list, dom, bus, dev, fn);
+    if (ret)
+      break;
   }
 
   closedir(dir);
-  return 0;
+  return ret;
 }
 
 static void pcie_close(pcie_device_t *dev) {
@@ -311,16 +369,23 @@ static uint32_t pcie_read32(pcie_device_t *dev, int pos) {
   return value;
 }
 
+static void pcie_do_dispatch_notification(notification_t *n, const char *type) {
+  sstrncpy(n->host, hostname_g, sizeof(n->host));
+  sstrncpy(n->type, type, sizeof(n->type));
+
+  plugin_dispatch_notification(n);
+  if (n->meta != NULL)
+    plugin_notification_meta_free(n->meta);
+}
+
 static void pcie_dispatch_notification(pcie_device_t *dev, notification_t *n,
                                        const char *type,
                                        const char *type_instance) {
-  sstrncpy(n->host, hostname_g, sizeof(n->host));
   ssnprintf(n->plugin_instance, sizeof(n->plugin_instance), "%04x:%02x:%02x.%d",
             dev->domain, dev->bus, dev->device, dev->function);
-  sstrncpy(n->type, type, sizeof(n->type));
   sstrncpy(n->type_instance, type_instance, sizeof(n->type_instance));
 
-  plugin_dispatch_notification(n);
+  pcie_do_dispatch_notification(n, type);
 }
 
 /* Report errors found in AER Correctable Error Status register */
@@ -597,9 +662,96 @@ static int pcie_process_devices(llist_t *devs) {
   return ret;
 }
 
-static int pcie_plugin_read(__attribute__((unused)) user_data_t *ud) {
+static void pcie_parse_msg(message *msg, unsigned int max_items) {
+  notification_t n = {.severity = NOTIF_WARNING,
+                      .time = cdtime(),
+                      .plugin = PCIE_PLUGIN,
+                      .meta = NULL};
 
-  return pcie_process_devices(pcie_dev_list);
+  for (int i = 0; i < max_items; i++) {
+    message_item *item = msg->message_items + i;
+    if (!item->value[0])
+      break;
+
+    DEBUG(PCIE_PLUGIN "[%02d] %s:%s", i, item->name, item->value);
+
+    if (strncmp(item->name, PCIE_LOG_SEVERITY, strlen(PCIE_LOG_SEVERITY)) ==
+        0) {
+      if (fnmatch("*[nN]on-[fF]atal*", item->value, 0) == 0) {
+        sstrncpy(n.type_instance, PCIE_SEV_NOFATAL, sizeof(n.type_instance));
+      } else if (fnmatch("*[fF]atal*", item->value, 0) == 0) {
+        n.severity = NOTIF_FAILURE;
+        sstrncpy(n.type_instance, PCIE_SEV_FATAL, sizeof(n.type_instance));
+      } else {
+        sstrncpy(n.type_instance, PCIE_SEV_CE, sizeof(n.type_instance));
+      }
+    } else if (strncmp(item->name, PCIE_LOG_DEV, strlen(PCIE_LOG_DEV)) == 0) {
+      sstrncpy(n.plugin_instance, item->value, sizeof(n.plugin_instance));
+    } else {
+      if (plugin_notification_meta_add_string(&n, item->name, item->value))
+        ERROR(PCIE_PLUGIN ": Failed to add notification meta data %s:%s",
+              item->name, item->value);
+    }
+  }
+  ssnprintf(n.message, sizeof(n.message), "AER %s error reported in log",
+            n.type_instance);
+
+  pcie_do_dispatch_notification(&n, PCIE_ERROR);
+}
+
+static int pcie_logfile_read(parser_job_data *job, const char *name) {
+  message *messages_storage;
+  unsigned int max_item_num;
+  int msg_num =
+      message_parser_read(job, &messages_storage, pcie_config.first_read);
+  if (msg_num < 0) {
+    notification_t n = {.severity = NOTIF_FAILURE,
+                        .time = cdtime(),
+                        .message = "Failed to read from log file",
+                        .plugin = PCIE_PLUGIN,
+                        .meta = NULL};
+    pcie_do_dispatch_notification(&n, "");
+    return -1;
+  }
+
+  max_item_num = STATIC_ARRAY_SIZE(messages_storage[0].message_items);
+
+  DEBUG(PCIE_PLUGIN ": read %d messages, %s", msg_num, name);
+
+  for (int i = 0; i < msg_num; i++) {
+    message *msg = messages_storage + i;
+    pcie_parse_msg(msg, max_item_num);
+  }
+  return 0;
+}
+
+static int pcie_plugin_read(__attribute__((unused)) user_data_t *ud) {
+  int ret = 0;
+
+  if (pcie_config.read_devices) {
+    ret = pcie_process_devices(pcie_dev_list);
+    if (ret < 0) {
+      ERROR(PCIE_PLUGIN ": Failed to read devices state");
+      return -1;
+    }
+  }
+
+  if (!pcie_config.read_log)
+    return 0;
+
+  for (int i = 0; i < pcie_parsers_len; i++) {
+    ret = pcie_logfile_read(pcie_parsers[i].job, pcie_parsers[i].name);
+    if (ret < 0) {
+      ERROR(PCIE_PLUGIN ": Failed to parse %s messages from %s",
+            pcie_parsers[i].name, pcie_config.logfile);
+      break;
+    }
+  }
+
+  if (pcie_config.first_read)
+    pcie_config.first_read = 0;
+
+  return ret;
 }
 
 static void pcie_access_config(void) {
@@ -624,56 +776,127 @@ static void pcie_access_config(void) {
   pcie_fops.read = pcie_read;
 }
 
-static int pcie_plugin_config(oconfig_item_t *ci) {
-  for (int i = 0; i < ci->children_num; i++) {
-    oconfig_item_t *child = ci->children + i;
+static int pcie_patterns_config(message_pattern *patterns,
+                                oconfig_item_t *match_opt, int num) {
+  for (int i = 0; i < num; i++) {
+    if (strcasecmp("Match", match_opt[i].key) == 0) {
+      /* set default submatch index to 1 since single submatch is the most
+       * common use case */
+      patterns[i].submatch_idx = 1;
+      for (int j = 0; j < match_opt[i].children_num; j++) {
+        int status = 0;
+        oconfig_item_t *regex_opt = match_opt[i].children + j;
+        if (strcasecmp("Name", regex_opt->key) == 0)
+          status = cf_util_get_string(regex_opt, &(patterns[i].name));
+        else if (strcasecmp("Regex", regex_opt->key) == 0)
+          status = cf_util_get_string(regex_opt, &(patterns[i].regex));
+        else if (strcasecmp("SubmatchIdx", regex_opt->key) == 0)
+          status = cf_util_get_int(regex_opt, &(patterns[i].submatch_idx));
+        else if (strcasecmp("Excluderegex", regex_opt->key) == 0)
+          status = cf_util_get_string(regex_opt, &(patterns[i].excluderegex));
+        else if (strcasecmp("IsMandatory", regex_opt->key) == 0)
+          status = cf_util_get_boolean(regex_opt, &(patterns[i].is_mandatory));
+        else {
+          ERROR(PCIE_PLUGIN ": Invalid configuration option \"%s\".",
+                regex_opt->key);
+          return -1;
+        }
 
-    if (strcasecmp("Source", child->key) == 0) {
-      if ((child->values_num != 1) ||
-          (child->values[0].type != OCONFIG_TYPE_STRING)) {
-        ERROR(PCIE_PLUGIN ": Invalid configuration parameter \"%s\".",
-              child->key);
-        return -1;
-      }
-      if (strcasecmp("sysfs", child->values[0].value.string) == 0)
-        pcie_config.use_sysfs = 1;
-    } else if (strcasecmp("AccessDir", child->key) == 0) {
-      if (cf_util_get_string_buffer(child, pcie_config.access_dir,
-                                    sizeof(pcie_config.access_dir))) {
-        ERROR(PCIE_PLUGIN ": Invalid configuration parameter \"%s\".",
-              child->key);
-        return -1;
-      }
-    } else if (strcasecmp("ReportMasked", child->key) == 0) {
-      if (cf_util_get_boolean(child, &pcie_config.notif_masked)) {
-        ERROR(PCIE_PLUGIN ": Invalid configuration parameter \"%s\".",
-              child->key);
-        return -1;
-      }
-    } else if (strcasecmp("Persistent", child->key) == 0) {
-      if (cf_util_get_boolean(child, &pcie_config.persistent)) {
-        ERROR(PCIE_PLUGIN ": Invalid configuration parameter \"%s\".",
-              child->key);
-        return -1;
+        if (status) {
+          ERROR(PCIE_PLUGIN ": Error setting regex option %s", regex_opt->key);
+        }
       }
     } else {
-      ERROR(PCIE_PLUGIN ": Invalid configuration option \"%s\".", child->key);
+      ERROR(PCIE_PLUGIN ": option \"%s\" is not allowed here.",
+            match_opt[i].key);
       return -1;
     }
   }
 
-  pcie_access_config();
-
   return 0;
 }
 
-static int pcie_init(void) {
-  pcie_dev_list = llist_create();
-  if (pcie_fops.list_devices(pcie_dev_list) != 0) {
-    ERROR(PCIE_PLUGIN ": Failed to find devices.");
-    pcie_clear_list(pcie_dev_list);
-    pcie_dev_list = NULL;
-    return -1;
+static int pcie_plugin_config(oconfig_item_t *ci) {
+  int parser_idx = 0;
+  /* Get number of configured parsers in advance */
+  for (int i = 0; i < ci->children_num; i++) {
+    oconfig_item_t *child = ci->children + i;
+    if (strcasecmp("MsgPattern", child->key) == 0)
+      pcie_parsers_len++;
+  }
+
+  for (int i = 0; i < ci->children_num; i++) {
+    oconfig_item_t *child = ci->children + i;
+    int status = 0;
+
+    if (strcasecmp("Source", child->key) == 0) {
+      if ((child->values_num != 1) ||
+          (child->values[0].type != OCONFIG_TYPE_STRING)) {
+        status = -1;
+      } else if (strcasecmp("sysfs", child->values[0].value.string) == 0) {
+        pcie_config.use_sysfs = 1;
+        pcie_config.read_devices = 1;
+      } else if (strcasecmp("proc", child->values[0].value.string) == 0) {
+        pcie_config.read_devices = 1;
+      }
+    } else if (strcasecmp("AccessDir", child->key) == 0) {
+      status = cf_util_get_string_buffer(child, pcie_config.access_dir,
+                                         sizeof(pcie_config.access_dir));
+    } else if (strcasecmp("ReportMasked", child->key) == 0) {
+      status = cf_util_get_boolean(child, &pcie_config.notif_masked);
+    } else if (strcasecmp("PersistentNotifications", child->key) == 0) {
+      status = cf_util_get_boolean(child, &pcie_config.persistent);
+    } else if (strcasecmp("LogFile", child->key) == 0) {
+      status = cf_util_get_string_buffer(child, pcie_config.logfile,
+                                         sizeof(pcie_config.logfile));
+    } else if (strcasecmp("ReadLog", child->key) == 0) {
+      status = cf_util_get_boolean(child, &pcie_config.read_log);
+    } else if (strcasecmp("FirstFullRead", child->key) == 0) {
+      status = cf_util_get_boolean(child, &pcie_config.first_read);
+    } else if (strcasecmp("MsgPattern", child->key) == 0) {
+      if (!pcie_parsers) {
+        pcie_parsers = calloc(pcie_parsers_len, sizeof(*pcie_parsers));
+        if (pcie_parsers == NULL) {
+          ERROR(PCIE_PLUGIN ": Error allocating message parsers");
+          pcie_config.config_error = 1;
+          break;
+        }
+      }
+      if (cf_util_get_string_buffer(child, pcie_parsers[parser_idx].name,
+                                    sizeof(pcie_parsers[parser_idx].name))) {
+        ERROR(PCIE_PLUGIN ": Invalid configuration parameter \"%s\".",
+              child->key);
+        pcie_config.config_error = 1;
+        break;
+      }
+      pcie_parsers[parser_idx].patterns_len = child->children_num;
+      pcie_parsers[parser_idx].patterns =
+          calloc(pcie_parsers[parser_idx].patterns_len,
+                 sizeof(*(pcie_parsers[parser_idx].patterns)));
+      if (pcie_parsers[parser_idx].patterns == NULL) {
+        ERROR(PCIE_PLUGIN ": Error allocating message_patterns");
+        pcie_config.config_error = 1;
+        break;
+      }
+      if (pcie_patterns_config(pcie_parsers[parser_idx].patterns,
+                               child->children, child->children_num)) {
+        ERROR(PCIE_PLUGIN ": Failed to parse patterns for \"%s\".", child->key);
+        pcie_config.config_error = 1;
+        break;
+      }
+      parser_idx++;
+    } else {
+      ERROR(PCIE_PLUGIN ": Invalid configuration option \"%s\".", child->key);
+      pcie_config.config_error = 1;
+      break;
+    }
+
+    if (status) {
+      ERROR(PCIE_PLUGIN ": Invalid configuration parameter \"%s\".",
+            child->key);
+      pcie_config.config_error = 1;
+      break;
+    }
   }
 
   return 0;
@@ -682,6 +905,72 @@ static int pcie_init(void) {
 static int pcie_shutdown(void) {
   pcie_clear_list(pcie_dev_list);
   pcie_dev_list = NULL;
+
+  for (int i = 0; i < pcie_parsers_len; i++) {
+    if (pcie_parsers[i].job != NULL)
+      message_parser_cleanup(pcie_parsers[i].job);
+    if (!pcie_config.default_patterns)
+      sfree(pcie_parsers[i].patterns);
+  }
+
+  if (pcie_parsers)
+    sfree(pcie_parsers);
+
+  return 0;
+}
+
+static int pcie_init(void) {
+  if (pcie_config.config_error) {
+    ERROR(PCIE_PLUGIN ": Error in configuration, failed to init plugin.");
+    return -1;
+  }
+
+  if (!pcie_config.read_devices && !pcie_config.read_log) {
+    ERROR(PCIE_PLUGIN ": Plugin is not configured for any source of data.");
+    return -1;
+  }
+
+  if (pcie_config.read_devices) {
+    pcie_access_config();
+    pcie_dev_list = llist_create();
+    if (pcie_fops.list_devices(pcie_dev_list) != 0) {
+      ERROR(PCIE_PLUGIN ": Failed to find devices.");
+      pcie_shutdown();
+      return -1;
+    }
+  }
+
+  if (!pcie_config.read_log)
+    return 0;
+
+  if (pcie_parsers == NULL) {
+    /* Set default patterns if no config is provided */
+    INFO(PCIE_PLUGIN ": Using default message parser");
+    pcie_config.default_patterns = 1;
+    pcie_parsers = calloc(1, sizeof(*pcie_parsers));
+    if (pcie_parsers == NULL) {
+      ERROR(PCIE_PLUGIN ": Error allocating default message parser");
+      pcie_shutdown();
+      return -1;
+    }
+    pcie_parsers_len = 1;
+    sstrncpy(pcie_parsers[0].name, "default", sizeof(pcie_parsers[0].name));
+    pcie_parsers[0].patterns = pcie_default_patterns;
+    pcie_parsers[0].patterns_len = STATIC_ARRAY_SIZE(pcie_default_patterns);
+  }
+
+  for (int i = 0; i < pcie_parsers_len; i++) {
+    pcie_parsers[i].job = message_parser_init(
+        pcie_config.logfile, 0, pcie_parsers[i].patterns_len - 1,
+        pcie_parsers[i].patterns, pcie_parsers[i].patterns_len);
+    if (pcie_parsers[i].job == NULL) {
+      ERROR(PCIE_PLUGIN ": Failed to initialize %s parser.",
+            pcie_parsers[i].name);
+      pcie_shutdown();
+      return -1;
+    }
+  }
+
   return 0;
 }
 
