65ca7b983d12caf81abb19d23bff069bbcca33c7
Ruben Kerkhof <ruben@rubenkerkhof.com>
2017-06-30 16:53:12 +0200

    Fix mismerge

--- src/openldap.c
+++ src/openldap.c
@@ -120,14 +120,9 @@ static int cldap_init_host(cldap_t *st) /* {{{ */
       ERROR("openldap plugin: Failed to start tls on %s: %s", st->url,
             ldap_err2string(rc));
       st->state = 0;
-<<<<<<< HEAD
-      ldap_unbind_ext_s(st->ld, NULL, NULL);
-      return -1;
-=======
       if (st->ld != NULL)
         ldap_unbind_ext_s(st->ld, NULL, NULL);
       return (-1);
->>>>>>> collectd-5.7
     }
   }
 
@@ -146,14 +141,9 @@ static int cldap_init_host(cldap_t *st) /* {{{ */
     ERROR("openldap plugin: Failed to bind to %s: %s", st->url,
           ldap_err2string(rc));
     st->state = 0;
-<<<<<<< HEAD
-    ldap_unbind_ext_s(st->ld, NULL, NULL);
-    return -1;
-=======
     if (st->ld != NULL)
       ldap_unbind_ext_s(st->ld, NULL, NULL);
     return (-1);
->>>>>>> collectd-5.7
   } else {
     DEBUG("openldap plugin: Successfully connected to %s", st->url);
     st->state = 1;
@@ -227,14 +217,9 @@ static int cldap_read_host(user_data_t *ud) /* {{{ */
     ERROR("openldap plugin: Failed to execute search: %s", ldap_err2string(rc));
     ldap_msgfree(result);
     st->state = 0;
-<<<<<<< HEAD
-    ldap_unbind_ext_s(st->ld, NULL, NULL);
-    return -1;
-=======
     if (st->ld != NULL)
       ldap_unbind_ext_s(st->ld, NULL, NULL);
     return (-1);
->>>>>>> collectd-5.7
   }
 
   for (LDAPMessage *e = ldap_first_entry(st->ld, result); e != NULL;
