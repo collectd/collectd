5203a7f54b80593fc2f78c7fe407ca1e7b969984
Ruben Kerkhof <ruben@rubenkerkhof.com>
2017-07-02 21:50:48 +0200

    libcollectclient: snprintf already NUL-terminates

--- src/libcollectdclient/client.c
+++ src/libcollectdclient/client.c
@@ -92,7 +92,6 @@
 #define LCC_SET_ERRSTR(c, ...)                                                 \
   do {                                                                         \
     snprintf((c)->errbuf, sizeof((c)->errbuf), __VA_ARGS__);                   \
-    (c)->errbuf[sizeof((c)->errbuf) - 1] = 0;                                  \
   } while (0)
 
 /*
