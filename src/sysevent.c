ae11633f49273a3a39c032490ccdda146e8406d1
Andrew Bays <abays@redhat.com>
2017-07-13 10:18:03 -0400

    Fix sysevent file name in header comment

--- src/sysevent.c
+++ src/sysevent.c
@@ -1,5 +1,5 @@
 /**
- * collectd - src/netlink2.c
+ * collectd - src/sysevent.c
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
