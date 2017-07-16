72b766cc2c0413eae5fd2cce468b349a33b8b557
Ruben Kerkhof <ruben@rubenkerkhof.com>
2017-07-16 22:37:31 +0200

    nut plugin: remove trailing whitespace

--- src/nut.c
+++ src/nut.c
@@ -160,7 +160,7 @@ static int nut_set_connect_timeout(const char *value) {
   ret = strtol(value, /* endptr = */ NULL, /* base = */ 10);
   if (errno == 0)
     connect_timeout = ret;
-  else 
+  else
     WARNING("nut plugin: The ConnectTimeout option requires numeric argument. "
             "Setting ignored.");
 #else /* #if HAVE_UPSCLI_TRYCONNECT */
