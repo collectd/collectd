20115ea505208ac2f1e8b453a35284b871db1321
Pavel Rochnyack <pavel2000@ngs.ru>
2017-07-17 11:13:53 +0700

    rrdtool plugin: Raise interval of random_variation to match documentation
    Documentation states what the actual timeout for each value is chosen randomly
    between <CacheTimeout>-<RandomTimeout> and <CacheTimeout>+<RandomTimeout>.
    
    The implementation did not match this.

--- src/rrdtool.c
+++ src/rrdtool.c
@@ -605,9 +605,6 @@ static int rrd_cache_flush_identifier(cdtime_t timeout,
 } /* int rrd_cache_flush_identifier */
 
 static int64_t rrd_get_random_variation(void) {
-  long min;
-  long max;
-
   if (random_timeout == 0)
     return (0);
 
@@ -618,10 +615,7 @@ static int64_t rrd_get_random_variation(void) {
     random_timeout = cache_timeout;
   }
 
-  max = (long)(random_timeout / 2);
-  min = max - ((long)random_timeout);
-
-  return ((int64_t)cdrand_range(min, max));
+  return (int64_t)cdrand_range(-random_timeout, random_timeout);
 } /* int64_t rrd_get_random_variation */
 
 static int rrd_cache_insert(const char *filename, const char *value,
