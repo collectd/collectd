ebf73b656a05c937ac8ab9f36753ac637c36df25
Christian Bartolomäus <use_v6@aglaz.de>
2017-09-19 09:09:39 +0200

    nfs plugin: Fix typo

--- src/nfs.c
+++ src/nfs.c
@@ -592,7 +592,7 @@ static int nfs_read(void) {
     nfs_read_kstat(nfs3_ksp_server, /* version = */ 3, "server",
                    nfs3_procedures_names, nfs3_procedures_names_num);
   }
-  if (report_v2) {
+  if (report_v4) {
     nfs_read_kstat(nfs4_ksp_client, /* version = */ 4, "client",
                    nfs4_procedures_names, nfs4_procedures_names_num);
     nfs_read_kstat(nfs4_ksp_server, /* version = */ 4, "server",
