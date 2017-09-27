11a94dba68c7797fc3c4bc45f249447b4243e082
Florian Forster <octo@collectd.org>
2017-09-27 11:08:50 +0200

    src/utils_match.c: Reset UTILS_MATCH_CF_GAUGE_INC matches to zero (was: NaN)
    Fixes: #2448

--- src/utils_match.c
+++ src/utils_match.c
@@ -294,7 +294,7 @@ void match_value_reset(cu_match_value_t *mv) {
   /* Reset GAUGE metrics only and except GAUGE_PERSIST. */
   if ((mv->ds_type & UTILS_MATCH_DS_TYPE_GAUGE) &&
       !(mv->ds_type & UTILS_MATCH_CF_GAUGE_PERSIST)) {
-    mv->value.gauge = NAN;
+    mv->value.gauge = (mv->ds_type & UTILS_MATCH_CF_GAUGE_INC) ? 0 : NAN;
     mv->values_num = 0;
   }
 } /* }}} void match_value_reset */
