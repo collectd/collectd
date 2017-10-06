6f8e9c1d7e8e1fef00cbafc7eeeb750ae153d89e
Serhiy Pshyk <serhiyx.pshyk@intel.com>
2017-10-06 02:24:11 +0100

    intel_pmu: address PR comments
    Signed-off-by: Serhiy Pshyk <serhiyx.pshyk@intel.com>

--- src/intel_pmu.c
+++ src/intel_pmu.c
@@ -289,17 +289,19 @@ meta_data_t *pmu_meta_data_create(const struct efd *efd) {
   meta_data_t *meta = NULL;
 
   /* create meta data only if value was scaled */
-  if (efd->val[1] != efd->val[2] && efd->val[2]) {
-    meta = meta_data_create();
-    if (meta == NULL) {
-      ERROR(PMU_PLUGIN ": meta_data_create failed.");
-      return NULL;
-    }
-
-    meta_data_add_unsigned_int(meta, "intel_pmu:raw_count", efd->val[0]);
-    meta_data_add_unsigned_int(meta, "intel_pmu:time_enabled", efd->val[1]);
-    meta_data_add_unsigned_int(meta, "intel_pmu:time_running", efd->val[2]);
+  if (efd->val[1] == efd->val[2] || !efd->val[2]) {
+    return NULL;
   }
+
+  meta = meta_data_create();
+  if (meta == NULL) {
+    ERROR(PMU_PLUGIN ": meta_data_create failed.");
+    return NULL;
+  }
+
+  meta_data_add_unsigned_int(meta, "intel_pmu:raw_count", efd->val[0]);
+  meta_data_add_unsigned_int(meta, "intel_pmu:time_enabled", efd->val[1]);
+  meta_data_add_unsigned_int(meta, "intel_pmu:time_running", efd->val[2]);
 
   return meta;
 }
@@ -332,10 +334,7 @@ static void pmu_dispatch_data(void) {
       /* dispatch per CPU value */
       pmu_submit_counter(i, e->event, value, meta);
 
-      if (meta) {
-        meta_data_destroy(meta);
-        meta = NULL;
-      }
+      meta_data_destroy(meta);
     }
 
     if (event_enabled > 0) {
