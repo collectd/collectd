7230f84a63e7616799479c9925ca73a3b6c45f41
Sergey tnt4brain Pechenko <10977752+tnt4brain@users.noreply.github.com>
2017-12-12 19:12:11 -0500

    Minor formatting changes according to clang-format.

--- src/notify_icinga.c
+++ src/notify_icinga.c
@@ -122,7 +122,8 @@ struct ni_callback {
  "check_source": "example.localdomain" }'
 
 static const char *config_keys[] = {"IcingaHost", "IcingaPort", "IcingaUser",
-                                    "IcingaPassword", "IcingaCert", "IcingaKey"};
+                                    "IcingaPassword", "IcingaCert",
+"IcingaKey"};
 
 static const int config_keys_num = STATIC_ARRAY_SIZE(config_keys);
 */
