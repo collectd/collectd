caf8a67c62e6b12ac21561f036703e06741cac04
Ruben Kerkhof <ruben@rubenkerkhof.com>
2018-02-24 11:03:10 +0100

    build.sh: use consistent indentation

--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
-#! /bin/sh
+#!/bin/sh
 
 GLOBAL_ERROR_INDICATOR=0
 
 check_for_application ()
 {
-	for PROG in "$@"
-	do
-		which "$PROG" >/dev/null 2>&1
-		if test $? -ne 0; then
-			cat >&2 <<EOF
+    for PROG in "$@"
+    do
+        which "$PROG" >/dev/null 2>&1
+        if test $? -ne 0; then
+            cat >&2 <<EOF
 WARNING: \`$PROG' not found!
     Please make sure that \`$PROG' is installed and is in one of the
     directories listed in the PATH environment variable.
 EOF
-			GLOBAL_ERROR_INDICATOR=1
-		fi
-	done
+            GLOBAL_ERROR_INDICATOR=1
+        fi
+    done
 }
 
 check_for_application lex bison autoheader aclocal automake autoconf pkg-config
@@ -24,25 +24,25 @@ libtoolize=""
 libtoolize --version >/dev/null 2>/dev/null
 if test $? -eq 0
 then
-	libtoolize=libtoolize
+    libtoolize=libtoolize
 else
-	glibtoolize --version >/dev/null 2>/dev/null
-	if test $? -eq 0
-	then
-		libtoolize=glibtoolize
-	else
-		cat >&2 <<EOF
+    glibtoolize --version >/dev/null 2>/dev/null
+    if test $? -eq 0
+    then
+        libtoolize=glibtoolize
+    else
+        cat >&2 <<EOF
 WARNING: Neither \`libtoolize' nor \`glibtoolize' have been found!
     Please make sure that one of them is installed and is in one of the
     directories listed in the PATH environment variable.
 EOF
-		GLOBAL_ERROR_INDICATOR=1
-	fi
+        GLOBAL_ERROR_INDICATOR=1
+    fi
  fi
 
 if test "$GLOBAL_ERROR_INDICATOR" != "0"
 then
-	exit 1
+    exit 1
 fi
 
 set -x
