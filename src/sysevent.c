4613cd79224e68ecd6dc76aaa67f675115a7176d
Andrew Bays <andrew.bays@gmail.com>
2018-02-28 14:48:23 -0500

    Move misplaced yajl_gen_map_close

--- src/sysevent.c
+++ src/sysevent.c
@@ -344,9 +344,6 @@ static int gen_message_payload(const char *msg, char *sev, int sev_num,
       yajl_gen_status_ok)
     goto err;
 
-  if (yajl_gen_map_close(g) != yajl_gen_status_ok)
-    goto err;
-
   // syslogMsg
   if (msg != NULL) {
     if (yajl_gen_string(g, (u_char *)SYSEVENT_SYSLOG_MSG_FIELD,
@@ -388,6 +385,9 @@ static int gen_message_payload(const char *msg, char *sev, int sev_num,
 
   if (yajl_gen_string(g, (u_char *)SYSEVENT_SYSLOG_TAG_VALUE,
                       strlen(SYSEVENT_SYSLOG_TAG_VALUE)) != yajl_gen_status_ok)
+    goto err;
+  
+  if (yajl_gen_map_close(g) != yajl_gen_status_ok)
     goto err;
 
   // *** END syslog fields ***
