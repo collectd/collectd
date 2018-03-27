collectd/collectd

commit 12f649741333f3b36d0f859800041674ad9e5011

ovs_events: Add external ids as metadata
- Interface meta data are added to dispatched
  values & notification.
  interface metadata:
    - UUID (always);
    - iface-id (if exists)
    - vm-uuid (if exists)
- Fix read callback to poll actual data instead of
  reading cache;
- Code clean up;

Change-Id: I291cb190d31ae091c1d47ce0f5e9d439f6958d61
Signed-off-by: Mytnyk, VolodymyrX <volodymyrx.mytnyk@intel.com>

src/utils_ovs.c
@@ -80,7 +80,7 @@
         ## __VA_ARGS__); } while (0)
 
 #define OVS_DB_POLL_TIMEOUT          1  /* poll receive timeout (sec) */
-#define OVS_DB_POLL_READ_BLOCK_SIZE  5  /* read block size (bytes) */
+#define OVS_DB_POLL_READ_BLOCK_SIZE  512        /* read block size (bytes) */
 #define OVS_DB_DEFAULT_DB_NAME       "Open_vSwitch"
 #define OVS_DB_RECONNECT_TIMEOUT     1  /* reconnect timeout (sec) */
 
@@ -632,15 +632,17 @@ ovs_db_json_data_process(ovs_db_t *pdb, const char *data, size_t len)
 
   /* duplicate the data to make null-terminated string
    * required for yajl_tree_parse() */
-  if ((sjson = strndup(data, len)) == NULL)
+  if ((sjson = malloc(len + 1)) == NULL)
     return (-1);
 
-  OVS_DEBUG("%s", sjson);
+  sstrncpy(sjson, data, len + 1);
+  OVS_DEBUG("[len=%d] %s", len, sjson);
 
   /* parse json data */
   jnode = yajl_tree_parse(sjson, yajl_errbuf, sizeof(yajl_errbuf));
   if (jnode == NULL) {
     OVS_ERROR("yajl_tree_parse() %s", yajl_errbuf);
+    sfree(sjson);
     return (-1);
   }
 
@@ -656,11 +658,12 @@ ovs_db_json_data_process(ovs_db_t *pdb, const char *data, size_t len)
       if (ovs_db_table_update_cb(pdb, jnode) < 0)
         OVS_ERROR("handle update notification failed");
     }
-  } else if (jval = yajl_tree_get(jnode, result_path, yajl_t_object)) {
+  } else if (jval = yajl_tree_get(jnode, result_path, yajl_t_any)) {
     /* result notification */
     if (ovs_db_result_cb(pdb, jnode) < 0)
       OVS_ERROR("handle result reply failed");
-  }
+  } else
+    OVS_ERROR("connot find method or result failed");
 
   /* release memory */
   yajl_tree_free(jnode);
@@ -1285,7 +1288,7 @@ ovs_utils_get_value_by_key(yajl_val jval, const char *key)
   const char *obj_key = NULL;
 
   /* check params */
-  if (!YAJL_IS_OBJECT(jval) || !key)
+  if (!YAJL_IS_OBJECT(jval) || (key == NULL))
     return NULL;
 
   /* find a value by key */
@@ -1295,5 +1298,51 @@ ovs_utils_get_value_by_key(yajl_val jval, const char *key)
       return YAJL_GET_OBJECT(jval)->values[i];
   }
 
+  return NULL;
+}
+
+/* Get OVS DB map value by given map key */
+yajl_val
+ovs_utils_get_map_value(yajl_val jval, const char *key)
+{
+  size_t map_len = 0;
+  size_t array_len = 0;
+  yajl_val *map_values = NULL;
+  yajl_val *array_values = NULL;
+
+  /* check YAJL array */
+  if (!YAJL_IS_ARRAY(jval) || (key == NULL))
+    return NULL;
+
+  /* check a database map value (2-element, first one should be a string */
+  array_len = YAJL_GET_ARRAY(jval)->len;
+  array_values = YAJL_GET_ARRAY(jval)->values;
+  if ((array_len != 2) || (!YAJL_IS_STRING(array_values[0])) ||
+      (!YAJL_IS_ARRAY(array_values[1])))
+    return NULL;
+
+  /* check first element of the array */
+  if (strcmp("map", YAJL_GET_STRING(array_values[0])) != 0)
+    return NULL;
+
+  /* try to find map value by map key */
+  map_len = YAJL_GET_ARRAY(array_values[1])->len;
+  map_values = YAJL_GET_ARRAY(array_values[1])->values;
+  for (int i = 0; i < map_len; i++) {
+    /* check YAJL array */
+    if (!YAJL_IS_ARRAY(map_values[i]))
+      break;
+
+    /* check a database pair value (2-element, first one represents a key
+     * and it should be a string in our case */
+    array_len = YAJL_GET_ARRAY(map_values[i])->len;
+    array_values = YAJL_GET_ARRAY(map_values[i])->values;
+    if ((array_len != 2) || (!YAJL_IS_STRING(array_values[0])))
+      break;
+
+    /* return map value if given key equals map key */
+    if (strcmp(key, YAJL_GET_STRING(array_values[0])) == 0)
+      return array_values[1];
+  }
   return NULL;
 }

src/utils_ovs.h
@@ -210,4 +210,20 @@ int ovs_db_table_cb_register(ovs_db_t *pdb, const char *tb_name,
  */
 yajl_val ovs_utils_get_value_by_key(yajl_val jval, const char *key);
 
+/*
+ * NAME
+ *   ovs_utils_get_map_value
+ *
+ * DESCRIPTION
+ *   Get OVS DB map value by given map key (rfc7047, "Notation" section).
+ *
+ * PARAMETERS
+ *   `jval'        A 2-element YAJL array that represents a OVS DB map value.
+ *   `key'         OVS DB map key name.
+ *
+ * RETURN VALUE
+ *   YAJL value upon success or NULL if key not found.
+ */
+yajl_val ovs_utils_get_map_value(yajl_val jval, const char *key);
+
 #endif

commit 7858bf59eeed7b8769dd7258af7ea223e37287e4

utils_ovs: fix potential NULL
In ovs_utils_get_map_value(), YAJL_GET_ARRAY may return a NULL
pointer, which is subsequently dereferenced. Ensure that the
pointer is non-NULL before proceeding.

Fixes: 12f6497 ("ovs_events: Add external ids as metadata")
Signed-off-by: Mark Kavanagh <mark.b.kavanagh@intel.com>

src/utils_ovs.c
@@ -1375,7 +1375,7 @@ yajl_val ovs_utils_get_map_value(yajl_val jval, const char *key) {
   map_values = YAJL_GET_ARRAY(array_values[1])->values;
   for (size_t i = 0; i < map_len; i++) {
     /* check YAJL array */
-    if (!YAJL_IS_ARRAY(map_values[i]))
+    if (!YAJL_IS_ARRAY(map_values[i]) || YAJL_GET_ARRAY(map_values[i]) == NULL)
       break;
 
     /* check a database pair value (2-element, first one represents a key