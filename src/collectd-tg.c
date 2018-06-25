ffba5b8b514103329bd82f8d773194fb7744f55a
Andrew Bays <abays@redhat.com>
2018-06-25 10:12:07 -0400

    Use CLOCK_REALTIME for collectd-tg times (fixes issue 2219)

--- src/collectd-tg.c
+++ src/collectd-tg.c
@@ -104,7 +104,7 @@ static double dtime(void) /* {{{ */
 {
   struct timespec ts = {0};
 
-  if (clock_gettime(CLOCK_MONOTONIC, &ts) != 0)
+  if (clock_gettime(CLOCK_REALTIME, &ts) != 0)
     perror("clock_gettime");
 
   return (double)ts.tv_sec + (double)ts.tv_nsec / 1e9;
