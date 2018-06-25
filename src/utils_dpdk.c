6f7193eb7af4a76239332988ac2803eb2ce361d9
Kevin Laatz <kevin.laatz@intel.com>
2018-06-25 17:14:34 +0100

    dpdk: fix deprecation warning
    This fixes a warning produced by a change introduced in the latest DPDK
    relase caused by a deprecation notice.
    
    Signed-off-by: Kevin Laatz <kevin.laatz@intel.com>

--- src/utils_dpdk.c
+++ src/utils_dpdk.c
@@ -853,7 +853,11 @@ uint128_t str_to_uint128(const char *str, int len) {
 }
 
 uint8_t dpdk_helper_eth_dev_count() {
+#if RTE_VERSION < RTE_VERSION_NUM(18, 05, 0, 0)
   uint8_t ports = rte_eth_dev_count();
+#else
+  uint8_t ports = rte_eth_dev_count_avail();
+#endif
   if (ports == 0) {
     ERROR(
         "%s:%d: No DPDK ports available. Check bound devices to DPDK driver.\n",
