84d1e960b05c1ca267369d4a5d9d25ac34c9369f
Andrew Bays <andrew.bays@gmail.com>
2019-06-17 07:23:14 -0400

    Attempt to align with new util library locations

--- src/connectivity.c
+++ src/connectivity.c
@@ -27,10 +27,10 @@
 
 #include "collectd.h"
 
-#include "common.h"
+#include "utils/common/common.h"
 #include "plugin.h"
 #include "utils_complain.h"
-#include "utils_ignorelist.h"
+#include "utils/ignorelist/ignorelist.h"
 
 #include <asm/types.h>
 #include <errno.h>
