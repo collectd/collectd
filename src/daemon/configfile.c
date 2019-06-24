collectd/collectd

commit 3b9c7b21b4ddfcf59a0147bc9e91e8889ca78d56

Handle failure of simple config callbacks
When they return an error, collectd should not ignore this.
Fixes #3076

src/daemon/configfile.c
@@ -411,10 +411,12 @@ static int dispatch_block_plugin(oconfig_item_t *ci) {
   }
 
   /* Hm, no complex plugin found. Dispatch the values one by one */
-  for (int i = 0; i < ci->children_num; i++) {
-    if (ci->children[i].children == NULL)
-      dispatch_value_plugin(name, ci->children + i);
-    else {
+  for (int i = 0, ret = 0; i < ci->children_num; i++) {
+    if (ci->children[i].children == NULL) {
+      ret = dispatch_value_plugin(name, ci->children + i);
+      if (ret != 0)
+        return ret;
+    } else {
       WARNING("There is a `%s' block within the "
               "configuration for the %s plugin. "
               "The plugin either only expects "

commit 42a30efe33c4d16276589e0dd1f9e135b38e054d

be more verbose when plugin config cb is failing
3b9c7b2 introduced stricter RC check.
This commit provides user feedback when config callback is failing.

Change-Id: Ia9c13048e95559b5be84477fc1602ff418a1df37

src/daemon/configfile.c
@@ -414,8 +414,10 @@ static int dispatch_block_plugin(oconfig_item_t *ci) {
   for (int i = 0, ret = 0; i < ci->children_num; i++) {
     if (ci->children[i].children == NULL) {
       ret = dispatch_value_plugin(name, ci->children + i);
-      if (ret != 0)
+      if (ret != 0) {
+        ERROR("dispatch for plugin %s returned non-zero code : %i", name, ret);
         return ret;
+      }
     } else {
       WARNING("There is a `%s' block within the "
               "configuration for the %s plugin. "