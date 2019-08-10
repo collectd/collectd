8d974a544bac5f982c47b8f31db2957dd0c305ad
Michal Zubac <zubac@avast.com>
2019-08-10 15:39:58 +0200

    ran contrib/format.sh on new files
    * src/compat/closefrom.c
    * src/compat/missing.h

--- src/compat/closefrom.c
+++ src/compat/closefrom.c
@@ -17,68 +17,66 @@
 
 #include <config.h>
 
-#include <sys/types.h>
-#include <sys/param.h>
-#include <unistd.h>
 #include <stdio.h>
+#include <sys/param.h>
+#include <sys/types.h>
+#include <unistd.h>
 #ifdef STDC_HEADERS
-# include <stdlib.h>
-# include <stddef.h>
+#include <stddef.h>
+#include <stdlib.h>
 #else
-# ifdef HAVE_STDLIB_H
-#  include <stdlib.h>
-# endif
+#ifdef HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
 #endif /* STDC_HEADERS */
 #include <fcntl.h>
 #ifdef HAVE_DIRENT_H
-# include <dirent.h>
-# define NAMLEN(dirent) strlen((dirent)->d_name)
+#include <dirent.h>
+#define NAMLEN(dirent) strlen((dirent)->d_name)
 #else
-# define dirent direct
-# define NAMLEN(dirent) (dirent)->d_namlen
-# ifdef HAVE_SYS_NDIR_H
-#  include <sys/ndir.h>
-# endif
-# ifdef HAVE_SYS_DIR_H
-#  include <sys/dir.h>
-# endif
-# ifdef HAVE_NDIR_H
-#  include <ndir.h>
-# endif
+#define dirent direct
+#define NAMLEN(dirent) (dirent)->d_namlen
+#ifdef HAVE_SYS_NDIR_H
+#include <sys/ndir.h>
+#endif
+#ifdef HAVE_SYS_DIR_H
+#include <sys/dir.h>
+#endif
+#ifdef HAVE_NDIR_H
+#include <ndir.h>
+#endif
 #endif
 
 #include "missing.h"
 
 #ifndef HAVE_FCNTL_CLOSEM
-# ifndef HAVE_DIRFD
-#   define closefrom_fallback	closefrom
-# endif
+#ifndef HAVE_DIRFD
+#define closefrom_fallback closefrom
+#endif
 #endif
 
 /*
  * Close all file descriptors greater than or equal to lowfd.
  * This is the expensive (ballback) method.
  */
-void
-closefrom_fallback(int lowfd)
-{
-    long fd, maxfd;
+void closefrom_fallback(int lowfd) {
+  long fd, maxfd;
 
-    /*
-     * Fall back on sysconf() or getdtablesize().  We avoid checking
-     * resource limits since it is possible to open a file descriptor
-     * and then drop the rlimit such that it is below the open fd.
-     */
+  /*
+   * Fall back on sysconf() or getdtablesize().  We avoid checking
+   * resource limits since it is possible to open a file descriptor
+   * and then drop the rlimit such that it is below the open fd.
+   */
 #ifdef HAVE_SYSCONF
-    maxfd = sysconf(_SC_OPEN_MAX);
+  maxfd = sysconf(_SC_OPEN_MAX);
 #else
-    maxfd = getdtablesize();
+  maxfd = getdtablesize();
 #endif /* HAVE_SYSCONF */
-    if (maxfd < 0)
-	maxfd = OPEN_MAX;
+  if (maxfd < 0)
+    maxfd = OPEN_MAX;
 
-    for (fd = lowfd; fd < maxfd; fd++)
-	(void) close((int) fd);
+  for (fd = lowfd; fd < maxfd; fd++)
+    (void)close((int)fd);
 }
 
 /*
@@ -86,33 +84,29 @@ closefrom_fallback(int lowfd)
  * We try the fast way first, falling back on the slow method.
  */
 #ifdef HAVE_FCNTL_CLOSEM
-void
-closefrom(int lowfd)
-{
-    if (fcntl(lowfd, F_CLOSEM, 0) == -1)
-	closefrom_fallback(lowfd);
+void closefrom(int lowfd) {
+  if (fcntl(lowfd, F_CLOSEM, 0) == -1)
+    closefrom_fallback(lowfd);
 }
 #else
-# ifdef HAVE_DIRFD
-void
-closefrom(int lowfd)
-{
-    struct dirent *dent;
-    DIR *dirp;
-    char *endp;
-    long fd;
+#ifdef HAVE_DIRFD
+void closefrom(int lowfd) {
+  struct dirent *dent;
+  DIR *dirp;
+  char *endp;
+  long fd;
 
-    /* Use /proc/self/fd directory if it exists. */
-    if ((dirp = opendir("/proc/self/fd")) != NULL) {
-	while ((dent = readdir(dirp)) != NULL) {
-	    fd = strtol(dent->d_name, &endp, 10);
-	    if (dent->d_name != endp && *endp == '\0' &&
-		fd >= 0 && fd < INT_MAX && fd >= lowfd && fd != dirfd(dirp))
-		(void) close((int) fd);
-	}
-	(void) closedir(dirp);
-    } else
-	closefrom_fallback(lowfd);
+  /* Use /proc/self/fd directory if it exists. */
+  if ((dirp = opendir("/proc/self/fd")) != NULL) {
+    while ((dent = readdir(dirp)) != NULL) {
+      fd = strtol(dent->d_name, &endp, 10);
+      if (dent->d_name != endp && *endp == '\0' && fd >= 0 && fd < INT_MAX &&
+          fd >= lowfd && fd != dirfd(dirp))
+        (void)close((int)fd);
+    }
+    (void)closedir(dirp);
+  } else
+    closefrom_fallback(lowfd);
 }
 #endif /* HAVE_DIRFD */
 #endif /* HAVE_FCNTL_CLOSEM */
--- src/compat/missing.h
+++ src/compat/missing.h
@@ -2,7 +2,7 @@
  * Some systems lack full limit definitions.
  */
 #ifndef OPEN_MAX
-# define OPEN_MAX 256
+#define OPEN_MAX 256
 #endif
 
 #ifndef HAVE_CLOSEFROM
