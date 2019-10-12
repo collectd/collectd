3afa68b6ab684153e8d82683c3e9055fc32cbef8
Rémi Ferrand <remi.ferrand@cc.in2p3.fr>
2019-10-12 20:19:19 +0200

    ./contrib/format.sh src/livestatus.c src/livestatus_test.c was run

--- src/livestatus_test.c
+++ src/livestatus_test.c
@@ -12,26 +12,37 @@
 
 #include "testing.h"
 
-// ls_lstatus_cmp_fn is a type of function used to compare two livestatus_status_t structs
+// ls_lstatus_cmp_fn is a type of function used to compare two
+// livestatus_status_t structs
 typedef int (*ls_lstatus_cmp_fn)(livestatus_status_t *, livestatus_status_t *);
 
 /* raw livestatus responses */
-static char lresponse_ok[] = "0;2161;7.8094609445e-02;0;0.0000000000e+00;34289;5.1425594053e-01;0;0.0000000000e+00;0;0.0000000000e+00;213267;2.7369494031e+00;741957;9.4749721737e+00;68688;8.5057596878e-01;140849;1.6611919493e+00\n";
-static char lresponse_too_many_fields[] = "0;2161;7.8094609445e-02;0;0.0000000000e+00;34289;5.1425594053e-01;0;0.0000000000e+00;0;0.0000000000e+00;213267;2.7369494031e+00;741957;9.4749721737e+00;68688;8.5057596878e-01;140849;1.6611919493e+00;1;45;56\n";
-static char lresponse_not_enough_fields[] = "0;2161;7.8094609445e-02;0;0.0000000000e+00;34289;5.1425594053e-01;0;0.0000000000e+00;0;0.0000000000e+00;213267;2.7369494031e+00;741957;9.4749721737e+00;68688;8.5057596878e-01\n";
+static char lresponse_ok[] =
+    "0;2161;7.8094609445e-02;0;0.0000000000e+00;34289;5.1425594053e-01;0;0."
+    "0000000000e+00;0;0.0000000000e+00;213267;2.7369494031e+00;741957;9."
+    "4749721737e+00;68688;8.5057596878e-01;140849;1.6611919493e+00\n";
+static char lresponse_too_many_fields[] =
+    "0;2161;7.8094609445e-02;0;0.0000000000e+00;34289;5.1425594053e-01;0;0."
+    "0000000000e+00;0;0.0000000000e+00;213267;2.7369494031e+00;741957;9."
+    "4749721737e+00;68688;8.5057596878e-01;140849;1.6611919493e+00;1;45;56\n";
+static char lresponse_not_enough_fields[] =
+    "0;2161;7.8094609445e-02;0;0.0000000000e+00;34289;5.1425594053e-01;0;0."
+    "0000000000e+00;0;0.0000000000e+00;213267;2.7369494031e+00;741957;9."
+    "4749721737e+00;68688;8.5057596878e-01\n";
 static char lresponse_empty[] = "";
 
 /* livestatus_status_t structs */
 static livestatus_status_t lresponse_ok_status_t = {
-  .cached_log_messages = 0,
-  .connections_rate = 0.078094609445,
-  .neb_callbacks_rate = 9.4749721737,
-  .service_checks_rate = 1.6611919493,
+    .cached_log_messages = 0,
+    .connections_rate = 0.078094609445,
+    .neb_callbacks_rate = 9.4749721737,
+    .service_checks_rate = 1.6611919493,
 };
 
-/* test_cmp_livestatus_status_t compares two livestatus_status_t structs by comparing only a subset of the fields */
-static int test_cmp_livestatus_status_t(livestatus_status_t *a, livestatus_status_t *b)
-{
+/* test_cmp_livestatus_status_t compares two livestatus_status_t structs by
+ * comparing only a subset of the fields */
+static int test_cmp_livestatus_status_t(livestatus_status_t *a,
+                                        livestatus_status_t *b) {
   EXPECT_EQ_INT(a->cached_log_messages, b->cached_log_messages);
   EXPECT_EQ_DOUBLE(a->connections_rate, b->connections_rate);
   EXPECT_EQ_DOUBLE(a->neb_callbacks_rate, b->neb_callbacks_rate);
@@ -47,14 +58,14 @@ DEF_TEST(parse) {
     ls_lstatus_cmp_fn lstatus_cmp;
     livestatus_status_t *want_lstatus;
   } cases[] = {
-    /* all ok */
-    {lresponse_ok, 0, test_cmp_livestatus_status_t, &lresponse_ok_status_t},
-    /* too many fields */
-    {lresponse_too_many_fields, -2, NULL, NULL},
-    /* not enough fields */
-    {lresponse_not_enough_fields, -3, NULL, NULL},
-    /* empty response */
-    {lresponse_empty, -3, NULL, NULL},
+      /* all ok */
+      {lresponse_ok, 0, test_cmp_livestatus_status_t, &lresponse_ok_status_t},
+      /* too many fields */
+      {lresponse_too_many_fields, -2, NULL, NULL},
+      /* not enough fields */
+      {lresponse_not_enough_fields, -3, NULL, NULL},
+      /* empty response */
+      {lresponse_empty, -3, NULL, NULL},
   };
 
   for (size_t i = 0; i < STATIC_ARRAY_SIZE(cases); i++) {
@@ -63,7 +74,7 @@ DEF_TEST(parse) {
 
     memset(&lstatus, 0x0, sizeof(lstatus));
 
-    rc = ls_parse((const char *) cases[i].lresponse, &lstatus);
+    rc = ls_parse((const char *)cases[i].lresponse, &lstatus);
     EXPECT_EQ_INT(cases[i].want_rc, rc);
 
     if (cases[i].lstatus_cmp == NULL)
