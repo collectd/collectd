cce773a3a38e6b227fb32dca2b359818b6b635fa
Florian Forster <octo@google.com>
2020-06-18 11:22:09 +0200

    src/daemon/utils_cache_mock.c: Fix function definitions to match the header file.

--- src/daemon/utils_cache_mock.c
+++ src/daemon/utils_cache_mock.c
@@ -29,14 +29,19 @@
 
 #include <errno.h>
 
-gauge_t *uc_get_rate(__attribute__((unused)) data_set_t const *ds,
-                     __attribute__((unused)) value_list_t const *vl) {
+gauge_t *uc_get_rate_vl(__attribute__((unused)) data_set_t const *ds,
+                        __attribute__((unused)) value_list_t const *vl) {
   errno = ENOTSUP;
   return NULL;
 }
 
-int uc_get_rate_by_name(const char *name, gauge_t **ret_values,
-                        size_t *ret_values_num) {
+int uc_get_rate(__attribute__((unused)) metric_t const *m,
+                __attribute__((unused)) gauge_t *ret_value) {
+  return ENOTSUP;
+}
+
+int uc_get_rate_by_name_vl(const char *name, gauge_t **ret_values,
+                           size_t *ret_values_num) {
   return ENOTSUP;
 }
 
@@ -44,8 +49,8 @@ int uc_get_names(char ***ret_names, cdtime_t **ret_times, size_t *ret_number) {
   return ENOTSUP;
 }
 
-int uc_get_value_by_name(const char *name, value_t **ret_values,
-                         size_t *ret_values_num) {
+int uc_get_value_by_name_vl(const char *name, value_t **ret_values,
+                            size_t *ret_values_num) {
   return ENOTSUP;
 }
 
