196ebbb0057ac63160757af681612da8db30a2af
Edgar Fuß <ef@math.uni-bonn.de>
2020-07-08 18:30:39 +0200

    Remove use of undefined suppress_warning variable

--- src/nfs.c
+++ src/nfs.c
@@ -622,19 +622,14 @@ static int nfs_read(void) {
 
   /* NetBSD reports v2 statistics mapped to v3 and doen't yet support v4 */
   if (report_v2) {
-    if (!suppress_warning) {
-      WARNING(
-          "nfs plugin: NFSv2 statistics have been requested "
-          "but they are mapped to NFSv3 statistics in the kernel on NetBSD.");
-    }
+    WARNING("nfs plugin: NFSv2 statistics have been requested "
+            "but they are mapped to NFSv3 statistics in the kernel on NetBSD.");
     return 0;
   }
 
   if (report_v4) {
-    if (!suppress_warning) {
-      WARNING("nfs plugin: NFSv4 statistics have been requested "
-              "but they are not yet supported on NetBSD.");
-    }
+    WARNING("nfs plugin: NFSv4 statistics have been requested "
+            "but they are not yet supported on NetBSD.");
     return 0;
   }
 
