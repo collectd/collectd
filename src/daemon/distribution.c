8e04c194c9dbbb1d5b934078988c81b11f800768
Svetlana Shmidt <sshmidt@google.com>
2020-07-27 15:11:00 +0500

    add checks + add infinity bucket

--- src/daemon/distribution.c
+++ src/daemon/distribution.c
@@ -3,6 +3,9 @@
 //
 
 #include "distribution.h"
+#include <stdio.h>
+#include <errno.h>
+#include <math.h>
 
 typedef struct bucket_s {
   uint64_t bucket_counter;
@@ -47,20 +50,49 @@ void build_tree(distribution_t *d, bucket_t *buckets, size_t node_index, size_t 
 }
 
 distribution_t* distribution_new_linear(size_t num_buckets, double size) {
+  if (num_buckets == 0 || size <= 0) {
+    errno = EINVAL;
+    return NULL;
+  }
+
   bucket_t *bucket_array = calloc(num_buckets, sizeof(bucket_t));
+  if (bucket_array == NULL)
+    return NULL;
+
   for (size_t i = 0; i < num_buckets; i++) {
     bucket_array[i].minimum = i * size;
-    bucket_array[i].maximum = (i + 1) * size;
+    if (i == num_buckets - 1)
+      bucket_array[i].maximum = INFINITY;
+    else
+      bucket_array[i].maximum = (i + 1) * size;
   }
 
   distribution_t *new_distribution = calloc(1, sizeof(distribution_t));
+  if (new_distribution == NULL)
+    return NULL;
   new_distribution->tree = calloc(2 * num_buckets - 1, sizeof(bucket_t));
+  if (new_distribution->tree == NULL) {
+    free (new_distribution);
+    return NULL;
+  }
+
   new_distribution->num_buckets = num_buckets;
   build_tree(new_distribution, bucket_array, 0, 0, num_buckets - 1);
   free(bucket_array);
   return new_distribution;
 }
 
+void distribution_destroy(distribution_t *d) {
+  if (d == NULL)
+    return;
+  free(d->tree);
+  free(d);
+}
+
 int main() {
-  distribution_t *p = distribution_new_linear(5, 2);
+  distribution_t *p = distribution_new_linear(3, 2);
+  for (size_t i = 0; i < 5; i++) {
+    printf("%f %f\n", p->tree[i].minimum, p->tree[i].maximum);
+  }
+  distribution_destroy(p);
 }
