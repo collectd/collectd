f2ed78d655f7ba10d101f9dc177556f10f674388
Svetlana Shmidt <sshmidt@google.com>
2020-08-27 11:52:06 +0000

    change name of get_buckets to distribution_get_buckets

--- src/daemon/distribution.h
+++ src/daemon/distribution.h
@@ -108,11 +108,11 @@ void distribution_destroy(distribution_t *d);
 size_t distribution_num_buckets(distribution_t *dist);
 
 /** @return - array of buckets in the distribution **/
-buckets_array_t get_buckets(distribution_t *dist);
+buckets_array_t distribution_get_buckets(distribution_t *dist);
 
 double distribution_total_sum(distribution_t *dist);
 
-double distribution_total_counter(distribution_t *dist);
+uint64_t distribution_total_counter(distribution_t *dist);
 
 double distribution_squared_deviation_sum(distribution_t *dist);
 
