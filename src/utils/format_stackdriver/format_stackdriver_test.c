e684d2d2db3b3e9d626c2ab15391d70cfa34cf7d
Svetlana Shmidt <sshmidt@google.com>
2020-09-23 09:53:02 +0000

    add small unit test for stackdriver format

--- src/utils/format_stackdriver/format_stackdriver_test.c
+++ src/utils/format_stackdriver/format_stackdriver_test.c
@@ -75,6 +75,13 @@ DEF_TEST(sd_format_metric_descriptor) {
                   "stored_aggregation_metric\",\"metricKind\":\"GAUGE\","
                   "\"valueType\":\"DOUBLE\",\"labels\":[]}",
       },
+      {
+        .name = "distribution_metric",
+        .type = METRIC_TYPE_DISTRIBUTION,
+        .want = "{\"type\":\"custom.googleapis.com/collectd/"
+                  "distribution_metric\",\"metricKind\":\"CUMULATIVE\",\"valueType\":"
+                  "\"DISTRIBUTION\",\"labels\":[]}",
+      }
   };
 
   for (size_t i = 0; i < (sizeof(cases) / sizeof(cases[0])); i++) {
