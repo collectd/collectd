4da767501802c7bc219a5fd9557ba1624365ad82
Mathieu Stoffel <mathieu.stoffel@atos.net>
2021-09-09 20:24:22 +0200

    ChangeLog: Redfish plugin: complete rework and enhancement
    The Redfish plugin of Collectd was, in the facts, unusable.
    Indeed, it suffered from several heavy limitations which made it impossible to
    monitor standard "recent" IT components (such as HPC compute blades):
    
    + Only members of anonymous collections could be collected;
    + Those members had to have a `DeviceName` attribute (this criteria was
      hard-coded and, thus, not configurable).
      The value of this attribute was compulsorily the type instance associated
      with the metric;
    + There was no way to select/filter which members of an anonymous collection
      should be collected;
    + The plugin could generate segmentation errors, and induced memory leaks;
    + The test suite and the documentation associated with the plugin were greatly
      incomplete.
    
    Consequently, the Redfish plugin of Collectd was refactored and extensively
    enhanced to make it possible and easy to monitor metrics and collect information
    exposed by Redfish interfaces.
    
    Hereinbelow are the main features of this refined Redfish plugin, when compared
    to its previous implementation:
    
    + Attributes of any Redfish resource, together with attributes of non-nested
      composite objects, and attributes of members of non-nested anonymous
      collections can be collected (without any constraint on their attributes);
    + Possibility to select members of anonymous collections to be monitored
      based on their IDs, the fact that they have specific attributes, and/or the
      values of those attributes;
    + Possibility to prefix the type instance of a member of an anonymous collection
      with its ID in the collection;
    + Possibility to use the value of a configurable attribute of the members of an
      anonymous collection as their type instances;
    + As a result, by combining the two previous features, it is even possible to
      use an attribute for which the values exhibited by the members of the
      anonymous collection are not unique to define the type instance of the
      associated metrics, without any risk of overwriting;
    + The source of segmentation errors and memory leaks within the plugin were
      corrected (though, there are still some memory leaks which seems unrelated to
      the plugin);
    + A complete and effective test suite, based on an up-to-date mockup Redfish
      interface developed by the DMTF, was implemented;
    + A thorough documentation of the plugin was written.
    
    On top of that, the function `oconfig_print_tree`, which prints the `oconfig`
    configuration tree which root was specified, was implemented.
    The latter is effectively compiled in `liboconfig` only if the debug
    mode of `Collectd` is enabled.

--- src/liboconfig/oconfig.c
+++ src/liboconfig/oconfig.c
@@ -22,6 +22,7 @@
  *
  * Authors:
  *   Florian Forster <octo at collectd.org>
+ *   Mathieu Stoffel <mathieu.stoffel at atos.net>
  **/
 
 #include <assert.h>
@@ -29,6 +30,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#include "utils_llist.h"
 
 #include "oconfig.h"
 
@@ -195,3 +198,263 @@ void oconfig_free(oconfig_item_t *ci) {
   oconfig_free_all(ci);
   free(ci);
 }
+
+#if defined(COLLECT_DEBUG)
+/* Fills "indent_str" with "nb_spaces" space characters, for indentation
+ * purposes (plus the terminating null-character).
+ * No check neither on the number of spaces nor on the string specifics is
+ * performed: */
+static inline void generate_indent_str(
+    char* indent_str,
+    const uint64_t nb_spaces
+)
+{
+    memset(indent_str, ' ', nb_spaces);
+    indent_str[nb_spaces] = '\0';
+}
+
+/*******/
+
+void oconfig_print_tree(
+    const oconfig_item_t* const ci,
+    const uint64_t indent_max_lvl, 
+    const uint64_t indent_in_spaces,
+    FILE* io_stream
+)
+{
+    /**************************************************************************
+     * Checking that input parameters are compliant with pre-requisites:
+     **************************************************************************/
+    if (ci == NULL) return;
+    if (io_stream == NULL) return;
+
+    /**************************************************************************
+     * Structures:
+     **************************************************************************/
+    /* Encapsulates the address of the considered node which children are to be
+     * processed, together with the associated indentation level and the ID of
+     * the child to be processed: */
+    typedef struct node_info_t
+    {
+        const oconfig_item_t* address;
+        uint64_t indent_lvl;
+        int child_id;
+    } node_info_t;
+
+    /**************************************************************************
+     * Variables:
+     **************************************************************************/
+    /* In order to keep track of the indent level while parsing the
+     * configuration tree: */
+    char* indent = NULL;
+    uint64_t indent_lvl = 0;
+
+    /* List used as a stack to store the descending exploring path within the
+     * configuration tree: */
+    llist_t* node_stack = NULL;
+
+    /* Pointer to be used to roam the configuration tree: */
+    const oconfig_item_t* config_roamer = ci;
+
+    /**************************************************************************
+     * Initialisation:
+     **************************************************************************/
+    /* Allocating "indent": */
+    indent = calloc(indent_max_lvl * indent_in_spaces + 1, sizeof(char));
+    /***/
+    if (indent == NULL) goto lbl_oconfig_print_tree_cleanup;
+
+    /* Allocating "node_stack": */
+    node_stack = llist_create();
+    /***/
+    if (node_stack == NULL) goto lbl_oconfig_print_tree_cleanup;
+
+    /**************************************************************************
+     * Core:
+     **************************************************************************/
+    /* Printing an header: */
+    fprintf(io_stream, "\n=======[ Start of configuration tree ]=======\n\n");
+
+    /* Roaming the configuration tree starting from its root: */
+    while (config_roamer != NULL)
+    {
+        /* If the current line should be indented, generating the indentation
+         * string, and printing it: */
+        if (indent_lvl > 0)
+        {
+            generate_indent_str(indent, indent_lvl * indent_in_spaces);  
+            fprintf(io_stream, "%s", indent);
+        }
+
+        /* Printing the key of the currently considered node: */
+        fprintf(io_stream, "%s:", config_roamer->key);
+
+        /* Roaming all the values associated with the currently considered
+         * node: */
+        for (int i = 0 ; i < config_roamer->values_num ; i++)
+        {
+            /* Determining the type of the currently considered value, and
+             * printing it: */
+            switch (config_roamer->values[i].type)
+            {
+                case OCONFIG_TYPE_NUMBER:
+                    fprintf(
+                        io_stream, 
+                        " %f",
+                        config_roamer->values[i].value.number
+                    );
+                    break;
+
+                case OCONFIG_TYPE_BOOLEAN:
+                    fprintf(
+                        io_stream, 
+                        " %s",
+                        (
+                            config_roamer->values[i].value.boolean ?
+                            "true" :
+                            "false"
+                        )
+                    );
+                    break;
+
+                /* The default case corresponds to the value being a string: */
+                default:
+                    fprintf(
+                        io_stream, 
+                        " %s",
+                        config_roamer->values[i].value.string
+                    );
+                    break;
+            }
+        }
+
+        /* Newline character to end the printing of the currently considered
+         * node: */
+        fprintf(io_stream, "%s", "\n");
+
+        /* Determining which node is the next one to be considered.
+         *
+         * Branch (1)
+         * The configuration tree is roamed from top to bottom.
+         * After a node is processed, the subtrees of its children are processed
+         * in order.
+         * When going down the tree to handle the subtree associated with a
+         * child of the considered node, the latter is stored in a stack (i.e.
+         * "node_stack"). 
+         * By doing so, it is possible to process the subtrees of other chidren
+         * of the considered node once the considered subtree is processed.
+         *
+         * Branch (2)
+         * When the considered node does not have children, popping out of the
+         * stack the parent node to go back to.
+         * Its next child, and the associated subtree, then become the node and
+         * subtree to be processed.
+         *
+         * Branch(3)
+         * When the stack is empty, and the currently considered node does not
+         * have children, the whole tree was processed (i.e. terminaison
+         * criteria). */
+
+        /* Branch (1): */
+        if (config_roamer->children_num > 0)
+        {
+            /* Allocating the structure in which the information associated with
+             * the considered parent node are to be stored.
+             * It is then appended to the stack: */
+            node_info_t* node_info = malloc(sizeof(*node_info));
+            if (node_info == NULL) goto lbl_oconfig_print_tree_cleanup;
+            /***/
+            node_info->address = config_roamer;
+            node_info->indent_lvl = indent_lvl;
+            node_info->child_id = 0;
+            /***/
+            llentry_t* node_entry = llentry_create(NULL, node_info);
+            if (node_entry == NULL) goto lbl_oconfig_print_tree_cleanup;
+            /***/
+            llist_append(node_stack, node_entry);
+
+            /* The next node to be considered is the first children of the
+             * considered node: */
+            config_roamer = &(config_roamer->children[0]);
+
+            /* Increasing the indentation level since we go deeper into the
+             * configuration tree, except if the maximum indentation level was
+             * reached: */
+            if (indent_lvl < indent_max_lvl) indent_lvl++;
+        }
+        /* Branch (2): */
+        else if (llist_size(node_stack) > 0)
+        {
+            /* The stack will be roamed to find a parent node which still has
+             * at least one child which subtree should be roamed.
+             * If none is found, the tree was completely roamed (which
+             * translates in "config_roamer" being set to "NULL": */
+            config_roamer = NULL;
+
+            /* Roaming all the stack until one parent node with a child which
+             * subtree should be roamed: */
+            do
+            {
+                /* Analysing the node on top of the stack: */
+                llentry_t* node_entry = llist_tail(node_stack);
+                /***/
+                node_info_t* node_info = (node_info_t*)(node_entry->value);
+
+                /* Does the considered node still have at least one child to
+                 * process? */
+                if (node_info->address->children_num > (node_info->child_id+1))
+                {
+                    /* The next node to process was found: */
+                    config_roamer = &(
+                        node_info->address->children[node_info->child_id + 1]
+                    );
+
+                    /* Increasing the indentation level since we go deeper into
+                     * the configuration tree, except if the maximum indentation
+                     * level was reached: */
+                    if (indent_lvl < indent_max_lvl)
+                    {
+                        indent_lvl = node_info->indent_lvl + 1;
+                    }
+
+                    /* Next child to be considered: */
+                    (node_info->child_id)++;
+
+                    break;
+                }
+                else
+                {
+                    /* Since all the children of the considered parent node were
+                     * processed, it is no longer useful to keep it in the
+                     * stack, hence popping and deallocating it: */
+                    llist_remove(node_stack, node_entry);
+                    if (node_entry->value != NULL) free(node_entry->value);
+                    llentry_destroy(node_entry);
+                }
+            } while (llist_size(node_stack) > 0);
+        }
+        /* Branch (3): */
+        else config_roamer = NULL;
+    }
+
+    /* Printing a footer: */
+    fprintf(io_stream, "\n=======[ End of configuration tree ]=======\n\n");
+
+lbl_oconfig_print_tree_cleanup:
+    /* Deallocating "indent": */
+    if (indent != NULL) free(indent);
+
+    /* Deallocating "node_stack": */
+    if (node_stack != NULL)
+    {
+        /* Deallocating the content of each node: */
+        for (llentry_t* le = llist_head(node_stack); le != NULL; le = le->next)
+        {
+            if (le->value != NULL) free(le->value);
+        }
+
+        /* Deallocating the list itself: */
+        llist_destroy(node_stack);
+    }
+}
+#endif
