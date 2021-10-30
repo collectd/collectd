68222733af8c85643e0199bcc8faf7a0900cdb58
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2021-10-30 19:20:33 +0300

    write_influxdb_udp: Split formatting functions to format_influxdb
    Signed-off-by: Matwey V. Kornilov <matwey.kornilov@gmail.com>

--- src/write_influxdb_udp.c
+++ src/write_influxdb_udp.c
@@ -32,6 +32,8 @@
 #include "utils_cache.h"
 #include "utils_complain.h"
 
+#include "utils/format_influxdb/format_influxdb.h"
+
 #if HAVE_NETDB_H
 #include <netdb.h>
 #endif
@@ -70,12 +72,6 @@ typedef struct sockent {
 #define NET_DEFAULT_PACKET_SIZE 1452
 #define NET_DEFAULT_PORT "8089"
 
-typedef enum {
-  NS,
-  US,
-  MS,
-} wifxudp_time_precision_t;
-
 /*
  * Private variables
  */
@@ -83,7 +79,7 @@ typedef enum {
 static int wifxudp_config_ttl;
 static size_t wifxudp_config_packet_size = NET_DEFAULT_PACKET_SIZE;
 static bool wifxudp_config_store_rates;
-static wifxudp_time_precision_t wifxudp_config_time_precision = MS;
+static format_influxdb_time_precision_t wifxudp_config_time_precision = MS;
 
 static sockent_t *sending_sockets;
 
@@ -353,170 +349,14 @@ static void flush_buffer(void) {
   write_influxdb_udp_init_buffer();
 }
 
-static int wifxudp_escape_string(char *buffer, size_t buffer_size,
-                                 const char *string) {
-
-  if ((buffer == NULL) || (string == NULL))
-    return -EINVAL;
-
-  if (buffer_size < 3)
-    return -ENOMEM;
-
-  int dst_pos = 0;
-
-#define BUFFER_ADD(c)                                                          \
-  do {                                                                         \
-    if (dst_pos >= (buffer_size - 1)) {                                        \
-      buffer[buffer_size - 1] = '\0';                                          \
-      return -ENOMEM;                                                          \
-    }                                                                          \
-    buffer[dst_pos] = (c);                                                     \
-    dst_pos++;                                                                 \
-  } while (0)
-
-  /* Escape special characters */
-  for (int src_pos = 0; string[src_pos] != 0; src_pos++) {
-    if ((string[src_pos] == '\\') || (string[src_pos] == ' ') ||
-        (string[src_pos] == ',') || (string[src_pos] == '=') ||
-        (string[src_pos] == '"')) {
-      BUFFER_ADD('\\');
-      BUFFER_ADD(string[src_pos]);
-    } else
-      BUFFER_ADD(string[src_pos]);
-  } /* for */
-  buffer[dst_pos] = 0;
-
-#undef BUFFER_ADD
-
-  return dst_pos;
-} /* int wifxudp_escape_string */
-
-static int write_influxdb_point(char *buffer, int buffer_len,
-                                const data_set_t *ds, const value_list_t *vl) {
-  int status;
-  int offset = 0;
-  gauge_t *rates = NULL;
-  bool have_values = false;
-
-  assert(0 == strcmp(ds->type, vl->type));
-
-#define BUFFER_ADD_ESCAPE(...)                                                 \
-  do {                                                                         \
-    status = wifxudp_escape_string(buffer + offset, buffer_len - offset,       \
-                                   __VA_ARGS__);                               \
-    if (status < 0)                                                            \
-      return -1;                                                               \
-    offset += status;                                                          \
-  } while (0)
-
-#define BUFFER_ADD(...)                                                        \
-  do {                                                                         \
-    status = snprintf(buffer + offset, buffer_len - offset, __VA_ARGS__);      \
-    if ((status < 0) || (status >= (buffer_len - offset))) {                   \
-      sfree(rates);                                                            \
-      return -1;                                                               \
-    }                                                                          \
-    offset += status;                                                          \
-  } while (0)
-
-  BUFFER_ADD_ESCAPE(vl->plugin);
-  BUFFER_ADD(",host=");
-  BUFFER_ADD_ESCAPE(vl->host);
-  if (strcmp(vl->plugin_instance, "") != 0) {
-    BUFFER_ADD(",instance=");
-    BUFFER_ADD_ESCAPE(vl->plugin_instance);
-  }
-  if (strcmp(vl->type, "") != 0) {
-    BUFFER_ADD(",type=");
-    BUFFER_ADD_ESCAPE(vl->type);
-  }
-  if (strcmp(vl->type_instance, "") != 0) {
-    BUFFER_ADD(",type_instance=");
-    BUFFER_ADD_ESCAPE(vl->type_instance);
-  }
-
-  BUFFER_ADD(" ");
-  for (size_t i = 0; i < ds->ds_num; i++) {
-    if ((ds->ds[i].type != DS_TYPE_COUNTER) &&
-        (ds->ds[i].type != DS_TYPE_GAUGE) &&
-        (ds->ds[i].type != DS_TYPE_DERIVE) &&
-        (ds->ds[i].type != DS_TYPE_ABSOLUTE)) {
-      sfree(rates);
-      return -1;
-    }
-
-    if (ds->ds[i].type == DS_TYPE_GAUGE) {
-      if (isnan(vl->values[i].gauge))
-        continue;
-      if (have_values)
-        BUFFER_ADD(",");
-      BUFFER_ADD("%s=%lf", ds->ds[i].name, vl->values[i].gauge);
-      have_values = true;
-    } else if (wifxudp_config_store_rates) {
-      if (rates == NULL)
-        rates = uc_get_rate(ds, vl);
-      if (rates == NULL) {
-        WARNING("write_influxdb_udp plugin: "
-                "uc_get_rate failed.");
-        return -1;
-      }
-      if (isnan(rates[i]))
-        continue;
-      if (have_values)
-        BUFFER_ADD(",");
-      BUFFER_ADD("%s=%lf", ds->ds[i].name, rates[i]);
-      have_values = true;
-    } else if (ds->ds[i].type == DS_TYPE_COUNTER) {
-      if (have_values)
-        BUFFER_ADD(",");
-      BUFFER_ADD("%s=%" PRIu64 "i", ds->ds[i].name,
-                 (uint64_t)vl->values[i].counter);
-      have_values = true;
-    } else if (ds->ds[i].type == DS_TYPE_DERIVE) {
-      if (have_values)
-        BUFFER_ADD(",");
-      BUFFER_ADD("%s=%" PRIi64 "i", ds->ds[i].name, vl->values[i].derive);
-      have_values = true;
-    } else if (ds->ds[i].type == DS_TYPE_ABSOLUTE) {
-      if (have_values)
-        BUFFER_ADD(",");
-      BUFFER_ADD("%s=%" PRIu64 "i", ds->ds[i].name, vl->values[i].absolute);
-      have_values = true;
-    }
-
-  } /* for ds->ds_num */
-  sfree(rates);
-
-  if (!have_values)
-    return 0;
-
-  uint64_t influxdb_time = 0;
-  switch (wifxudp_config_time_precision) {
-  case NS:
-    influxdb_time = CDTIME_T_TO_NS(vl->time);
-    break;
-  case US:
-    influxdb_time = CDTIME_T_TO_US(vl->time);
-    break;
-  case MS:
-    influxdb_time = CDTIME_T_TO_MS(vl->time);
-    break;
-  }
-
-  BUFFER_ADD(" %" PRIu64 "\n", influxdb_time);
-
-#undef BUFFER_ADD_ESCAPE
-#undef BUFFER_ADD
-
-  return offset;
-} /* int write_influxdb_point */
-
 static int
 write_influxdb_udp_write(const data_set_t *ds, const value_list_t *vl,
                          user_data_t __attribute__((unused)) * user_data) {
   char buffer[NET_DEFAULT_PACKET_SIZE];
 
-  int status = write_influxdb_point(buffer, NET_DEFAULT_PACKET_SIZE, ds, vl);
+  int status = format_influxdb_value_list(buffer, NET_DEFAULT_PACKET_SIZE, ds,
+                                          vl, wifxudp_config_store_rates,
+                                          wifxudp_config_time_precision);
 
   if (status < 0) {
     ERROR("write_influxdb_udp plugin: write_influxdb_udp_write failed.");
