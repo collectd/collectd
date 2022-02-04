0cee98e8e0323a9dcfc18ab63d70fed4081d04ba
Eero Tamminen <eero.t.tamminen@intel.com>
2022-02-04 12:20:27 +0200

    Attribute unused function args as such in few of the plugins
    Based on "-O3 -Werror -Wall -Wextra -Wformat-security" output.

--- src/turbostat.c
+++ src/turbostat.c
@@ -835,7 +835,8 @@ for_all_cpus_delta(const struct thread_data *thread_new_base,
  * Package Thermal Management Sensor (PTM), and thermal event thresholds.
  */
 static int __attribute__((warn_unused_result))
-set_temperature_target(struct thread_data *t, struct core_data *c,
+set_temperature_target(struct thread_data *t,
+                       __attribute__((unused)) struct core_data *c,
                        struct pkg_data *p) {
   unsigned long long msr;
   unsigned int target_c_local;
