collectd/collectd

commit 75aeab3a42b56b68cd15df4e992bb51dd2092d38

gpu_sysman: Report memory usage ratio only when ratios requested
Signed-off-by: Eero Tamminen <eero.t.tamminen@intel.com>

src/gpu_sysman.c
@@ -1019,8 +1019,10 @@ static bool gpu_mems(gpu_device_t *gpu, unsigned int cache_idx) {
       mem_used = mem_size - mem_free;
       metric.value.gauge = mem_used;
       metric_family_metric_append(&fam_bytes, metric);
-      metric.value.gauge = mem_used / mem_size;
-      metric_family_metric_append(&fam_ratio, metric);
+      if (config.output & OUTPUT_RATIO) {
+        metric.value.gauge = mem_used / mem_size;
+        metric_family_metric_append(&fam_ratio, metric);
+      }
     } else {
       /* find min & max values for memory free from
        * (the configured number of) samples
@@ -1041,16 +1043,19 @@ static bool gpu_mems(gpu_device_t *gpu, unsigned int cache_idx) {
       metric.value.gauge = mem_used;
       metric_label_set(&metric, "function", "min");
       metric_family_metric_append(&fam_bytes, metric);
-      metric.value.gauge = mem_used / mem_size;
-      metric_family_metric_append(&fam_ratio, metric);
-
+      if (config.output & OUTPUT_RATIO) {
+        metric.value.gauge = mem_used / mem_size;
+        metric_family_metric_append(&fam_ratio, metric);
+      }
       /* smallest used amount of memory */
       mem_used = mem_size - free_min;
       metric.value.gauge = mem_used;
       metric_label_set(&metric, "function", "max");
       metric_family_metric_append(&fam_bytes, metric);
-      metric.value.gauge = mem_used / mem_size;
-      metric_family_metric_append(&fam_ratio, metric);
+      if (config.output & OUTPUT_RATIO) {
+        metric.value.gauge = mem_used / mem_size;
+        metric_family_metric_append(&fam_ratio, metric);
+      }
     }
   }
   if (ok && cache_idx == 0) {

commit f4ed340c0e2b386e049000cd45dd87d77b0298fa

gpu_sysman: Avoid log warning when ratio output is disabled
Fixes: 75aeab3a42b5

Signed-off-by: Eero Tamminen <eero.t.tamminen@intel.com>

src/gpu_sysman.c
@@ -988,7 +988,7 @@ static bool gpu_mems(gpu_device_t *gpu, unsigned int cache_idx) {
   };
   metric_t metric = {0};
 
-  bool ok = false;
+  bool reported_ratio = false, ok = false;
   for (i = 0; i < mem_count; i++) {
     /* fetch memory samples */
     if (zesMemoryGetState(mems[i], &(gpu->memory[cache_idx][i])) !=
@@ -1023,6 +1023,7 @@ static bool gpu_mems(gpu_device_t *gpu, unsigned int cache_idx) {
       if (config.output & OUTPUT_RATIO) {
         metric.value.gauge = mem_used / mem_size;
         metric_family_metric_append(&fam_ratio, metric);
+        reported_ratio = true;
       }
     } else {
       /* find min & max values for memory free from
@@ -1047,6 +1048,7 @@ static bool gpu_mems(gpu_device_t *gpu, unsigned int cache_idx) {
       if (config.output & OUTPUT_RATIO) {
         metric.value.gauge = mem_used / mem_size;
         metric_family_metric_append(&fam_ratio, metric);
+        reported_ratio = true;
       }
       /* smallest used amount of memory */
       mem_used = mem_size - free_min;
@@ -1056,13 +1058,16 @@ static bool gpu_mems(gpu_device_t *gpu, unsigned int cache_idx) {
       if (config.output & OUTPUT_RATIO) {
         metric.value.gauge = mem_used / mem_size;
         metric_family_metric_append(&fam_ratio, metric);
+        reported_ratio = true;
       }
     }
   }
   if (ok && cache_idx == 0) {
     metric_reset(&metric);
     gpu_submit(gpu, &fam_bytes);
-    gpu_submit(gpu, &fam_ratio);
+    if (reported_ratio) {
+      gpu_submit(gpu, &fam_ratio);
+    }
   }
   free(mems);
   return ok;