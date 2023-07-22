8269d287bbbedd6d21502c54a3f958d83c78c09b
Rinat Ibragimov <ibragimovrinat@mail.ru>
2023-07-23 02:43:49 +0300

    smart: use namespace id 0 for Identify Controller command
    Identify Controller command (CNS=0x01) doesn't use NSID field (see
    Figure 273, NVMe spec 2.0b). And when NSID is not used it must be set to 0
    (see Figure 87, NVMe spec 2.0b). Setting invalid namespace ID values
    forces drives to abort the command.

--- src/smart.c
+++ src/smart.c
@@ -295,7 +295,7 @@ static int get_vendor_id(const char *dev, char const *name) {
    */
   err = ioctl(fd, NVME_IOCTL_ADMIN_CMD,
               &(struct nvme_admin_cmd){.opcode = NVME_ADMIN_IDENTIFY,
-                                       .nsid = NVME_NSID_ALL,
+                                       .nsid = 0,
                                        .addr = (unsigned long)&raw_buf.c[1],
                                        .data_len = sizeof(vid),
                                        .cdw10 = 1,
