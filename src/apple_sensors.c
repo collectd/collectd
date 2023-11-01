c8429f40bb64501b91416210bb6cb884f9323c2a
Tapan Thaker <tapanth@uber.com>
2023-11-01 13:32:12 -0700

    Apply clang-format

--- src/apple_sensors.c
+++ src/apple_sensors.c
@@ -49,184 +49,173 @@
 #define SMC_KEY_FAN0_RPM_CUR "F0Ac"
 
 typedef struct {
-    char major;
-    char minor;
-    char build;
-    char reserved[1];
-    UInt16 release;
+  char major;
+  char minor;
+  char build;
+  char reserved[1];
+  UInt16 release;
 } SMCKeyData_vers_t;
 
 typedef struct {
-    UInt16 version;
-    UInt16 length;
-    UInt32 cpuPLimit;
-    UInt32 gpuPLimit;
-    UInt32 memPLimit;
+  UInt16 version;
+  UInt16 length;
+  UInt32 cpuPLimit;
+  UInt32 gpuPLimit;
+  UInt32 memPLimit;
 } SMCKeyData_pLimitData_t;
 
 typedef struct {
-    UInt32 dataSize;
-    UInt32 dataType;
-    char dataAttributes;
+  UInt32 dataSize;
+  UInt32 dataType;
+  char dataAttributes;
 } SMCKeyData_keyInfo_t;
 
 typedef char SMCBytes_t[32];
 
 typedef struct {
-    UInt32 key;
-    SMCKeyData_vers_t vers;
-    SMCKeyData_pLimitData_t pLimitData;
-    SMCKeyData_keyInfo_t keyInfo;
-    char result;
-    char status;
-    char data8;
-    UInt32 data32;
-    SMCBytes_t bytes;
+  UInt32 key;
+  SMCKeyData_vers_t vers;
+  SMCKeyData_pLimitData_t pLimitData;
+  SMCKeyData_keyInfo_t keyInfo;
+  char result;
+  char status;
+  char data8;
+  UInt32 data32;
+  SMCBytes_t bytes;
 } SMCKeyData_t;
 
 typedef char UInt32Char_t[5];
 
 typedef struct {
-    UInt32Char_t key;
-    UInt32 dataSize;
-    UInt32Char_t dataType;
-    SMCBytes_t bytes;
+  UInt32Char_t key;
+  UInt32 dataSize;
+  UInt32Char_t dataType;
+  SMCBytes_t bytes;
 } SMCVal_t;
-
 
 static io_connect_t conn;
 
-kern_return_t SMCOpen(void)
-{
-    kern_return_t result;
-    io_iterator_t iterator;
-    io_object_t device;
+kern_return_t SMCOpen(void) {
+  kern_return_t result;
+  io_iterator_t iterator;
+  io_object_t device;
 
-    CFMutableDictionaryRef matchingDictionary = IOServiceMatching("AppleSMC");
-    result = IOServiceGetMatchingServices(kIOMainPortDefault, matchingDictionary, &iterator);
-    if (result != kIOReturnSuccess) {
-        ERROR("Error: IOServiceGetMatchingServices() = %08x\n", result);
-        return 1;
-    }
+  CFMutableDictionaryRef matchingDictionary = IOServiceMatching("AppleSMC");
+  result = IOServiceGetMatchingServices(kIOMainPortDefault, matchingDictionary,
+                                        &iterator);
+  if (result != kIOReturnSuccess) {
+    ERROR("Error: IOServiceGetMatchingServices() = %08x\n", result);
+    return 1;
+  }
 
-    device = IOIteratorNext(iterator);
-    IOObjectRelease(iterator);
-    if (device == 0) {
-        ERROR("Error: no SMC found\n");
-        return 1;
-    }
+  device = IOIteratorNext(iterator);
+  IOObjectRelease(iterator);
+  if (device == 0) {
+    ERROR("Error: no SMC found\n");
+    return 1;
+  }
 
-    result = IOServiceOpen(device, mach_task_self(), 0, &conn);
-    IOObjectRelease(device);
-    if (result != kIOReturnSuccess) {
-        ERROR("Error: IOServiceOpen() = %08x\n", result);
-        return 1;
-    }
+  result = IOServiceOpen(device, mach_task_self(), 0, &conn);
+  IOObjectRelease(device);
+  if (result != kIOReturnSuccess) {
+    ERROR("Error: IOServiceOpen() = %08x\n", result);
+    return 1;
+  }
 
-    return kIOReturnSuccess;
+  return kIOReturnSuccess;
 }
 
-UInt32 _strtoul(char* str, int size, int base)
-{
-    UInt32 total = 0;
-    int i;
+UInt32 _strtoul(char *str, int size, int base) {
+  UInt32 total = 0;
+  int i;
 
-    for (i = 0; i < size; i++) {
-        if (base == 16)
-            total += str[i] << (size - 1 - i) * 8;
-        else
-            total += (unsigned char)(str[i] << (size - 1 - i) * 8);
-    }
-    return total;
+  for (i = 0; i < size; i++) {
+    if (base == 16)
+      total += str[i] << (size - 1 - i) * 8;
+    else
+      total += (unsigned char)(str[i] << (size - 1 - i) * 8);
+  }
+  return total;
 }
 
-void _ultostr(char* str, UInt32 val)
-{
-    str[0] = '\0';
-    sprintf(str, "%c%c%c%c",
-        (unsigned int)val >> 24,
-        (unsigned int)val >> 16,
-        (unsigned int)val >> 8,
-        (unsigned int)val);
+void _ultostr(char *str, UInt32 val) {
+  str[0] = '\0';
+  sprintf(str, "%c%c%c%c", (unsigned int)val >> 24, (unsigned int)val >> 16,
+          (unsigned int)val >> 8, (unsigned int)val);
 }
 
-kern_return_t SMCClose(void)
-{
-    return IOServiceClose(conn);
-}
+kern_return_t SMCClose(void) { return IOServiceClose(conn); }
 
-kern_return_t SMCCall(int index, SMCKeyData_t* inputStructure, SMCKeyData_t* outputStructure)
-{
-    size_t structureInputSize;
-    size_t structureOutputSize;
+kern_return_t SMCCall(int index, SMCKeyData_t *inputStructure,
+                      SMCKeyData_t *outputStructure) {
+  size_t structureInputSize;
+  size_t structureOutputSize;
 
-    structureInputSize = sizeof(SMCKeyData_t);
-    structureOutputSize = sizeof(SMCKeyData_t);
+  structureInputSize = sizeof(SMCKeyData_t);
+  structureOutputSize = sizeof(SMCKeyData_t);
 
 #if MAC_OS_X_VERSION_10_5
-    return IOConnectCallStructMethod(conn, index,
-        // inputStructure
-        inputStructure, structureInputSize,
-        // ouputStructure
-        outputStructure, &structureOutputSize);
+  return IOConnectCallStructMethod(conn, index,
+                                   // inputStructure
+                                   inputStructure, structureInputSize,
+                                   // ouputStructure
+                                   outputStructure, &structureOutputSize);
 #else
-    return IOConnectMethodStructureIStructureO(conn, index,
-        structureInputSize, /* structureInputSize */
-        &structureOutputSize, /* structureOutputSize */
-        inputStructure, /* inputStructure */
-        outputStructure); /* ouputStructure */
+  return IOConnectMethodStructureIStructureO(
+      conn, index, structureInputSize, /* structureInputSize */
+      &structureOutputSize,            /* structureOutputSize */
+      inputStructure,                  /* inputStructure */
+      outputStructure);                /* ouputStructure */
 #endif
 }
 
-kern_return_t SMCReadKey(UInt32Char_t key, SMCVal_t* val)
-{
-    kern_return_t result;
-    SMCKeyData_t inputStructure;
-    SMCKeyData_t outputStructure;
+kern_return_t SMCReadKey(UInt32Char_t key, SMCVal_t *val) {
+  kern_return_t result;
+  SMCKeyData_t inputStructure;
+  SMCKeyData_t outputStructure;
 
-    memset(&inputStructure, 0, sizeof(SMCKeyData_t));
-    memset(&outputStructure, 0, sizeof(SMCKeyData_t));
-    memset(val, 0, sizeof(SMCVal_t));
+  memset(&inputStructure, 0, sizeof(SMCKeyData_t));
+  memset(&outputStructure, 0, sizeof(SMCKeyData_t));
+  memset(val, 0, sizeof(SMCVal_t));
 
-    inputStructure.key = _strtoul(key, 4, 16);
-    inputStructure.data8 = SMC_CMD_READ_KEYINFO;
+  inputStructure.key = _strtoul(key, 4, 16);
+  inputStructure.data8 = SMC_CMD_READ_KEYINFO;
 
-    result = SMCCall(KERNEL_INDEX_SMC, &inputStructure, &outputStructure);
-    if (result != kIOReturnSuccess)
-        return result;
+  result = SMCCall(KERNEL_INDEX_SMC, &inputStructure, &outputStructure);
+  if (result != kIOReturnSuccess)
+    return result;
 
-    val->dataSize = outputStructure.keyInfo.dataSize;
-    _ultostr(val->dataType, outputStructure.keyInfo.dataType);
-    inputStructure.keyInfo.dataSize = val->dataSize;
-    inputStructure.data8 = SMC_CMD_READ_BYTES;
+  val->dataSize = outputStructure.keyInfo.dataSize;
+  _ultostr(val->dataType, outputStructure.keyInfo.dataType);
+  inputStructure.keyInfo.dataSize = val->dataSize;
+  inputStructure.data8 = SMC_CMD_READ_BYTES;
 
-    result = SMCCall(KERNEL_INDEX_SMC, &inputStructure, &outputStructure);
-    if (result != kIOReturnSuccess)
-        return result;
+  result = SMCCall(KERNEL_INDEX_SMC, &inputStructure, &outputStructure);
+  if (result != kIOReturnSuccess)
+    return result;
 
-    memcpy(val->bytes, outputStructure.bytes, sizeof(outputStructure.bytes));
+  memcpy(val->bytes, outputStructure.bytes, sizeof(outputStructure.bytes));
 
-    return kIOReturnSuccess;
+  return kIOReturnSuccess;
 }
 
-double SMCGetTemperature(char* key)
-{
-    SMCVal_t val;
-    kern_return_t result;
+double SMCGetTemperature(char *key) {
+  SMCVal_t val;
+  kern_return_t result;
 
-    result = SMCReadKey(key, &val);
-    if (result == kIOReturnSuccess) {
-        // read succeeded - check returned value
-        if (val.dataSize > 0) {
-            if (strcmp(val.dataType, DATATYPE_SP78) == 0) {
-                // convert sp78 value to temperature
-                int intValue = val.bytes[0] * 256 + (unsigned char)val.bytes[1];
-                return intValue / 256.0;
-            }
-        }
+  result = SMCReadKey(key, &val);
+  if (result == kIOReturnSuccess) {
+    // read succeeded - check returned value
+    if (val.dataSize > 0) {
+      if (strcmp(val.dataType, DATATYPE_SP78) == 0) {
+        // convert sp78 value to temperature
+        int intValue = val.bytes[0] * 256 + (unsigned char)val.bytes[1];
+        return intValue / 256.0;
+      }
     }
-    // read failed
-    return 0.0;
+  }
+  // read failed
+  return 0.0;
 }
 
 static void as_submit(const char *instance, const char *type, double val) {
@@ -245,25 +234,20 @@ static void as_submit(const char *instance, const char *type, double val) {
 }
 
 static int as_read(void) {
-    double cpuTemp =  SMCGetTemperature(SMC_KEY_CPU_TEMP);
-    as_submit("cpu", "temperature", cpuTemp);
-    double gpuTemp =  SMCGetTemperature(SMC_KEY_GPU_TEMP);
-    as_submit("gpu", "temperature", gpuTemp);
-    double ambientTemp =  SMCGetTemperature(SMC_KEY_AMBIENT_TEMP);
-    as_submit("ambient", "temperature", ambientTemp);
+  double cpuTemp = SMCGetTemperature(SMC_KEY_CPU_TEMP);
+  as_submit("cpu", "temperature", cpuTemp);
+  double gpuTemp = SMCGetTemperature(SMC_KEY_GPU_TEMP);
+  as_submit("gpu", "temperature", gpuTemp);
+  double ambientTemp = SMCGetTemperature(SMC_KEY_AMBIENT_TEMP);
+  as_submit("ambient", "temperature", ambientTemp);
 
-    return 0;
+  return 0;
 } /* int as_read */
 
-static int as_init(void) {
-  return SMCOpen();
-} /* int as_init */
+static int as_init(void) { return SMCOpen(); } /* int as_init */
 
 /* Close the network connection */
-static int as_shutdown(void) {
-  return SMCClose();
-} /* int as_shutdown */
-
+static int as_shutdown(void) { return SMCClose(); } /* int as_shutdown */
 
 void module_register(void) {
   plugin_register_init("apple_sensors", as_init);
