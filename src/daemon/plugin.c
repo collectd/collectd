80940742d64bc23a524615b4a64b307ddfdee786
Florian Forster <octo@collectd.org>
2024-01-26 13:13:53 +0100

    fix: Handle metric families with zero metrics gracefully.
    This allows plugins to call `plugin_dispatch_metric_family()` unconditionally.

--- src/daemon/plugin.c
+++ src/daemon/plugin.c
@@ -2100,8 +2100,12 @@ static void set_default_resource_attributes(metric_family_t *fam) {
 }
 
 EXPORT int plugin_dispatch_metric_family(metric_family_t const *fam) {
-  if ((fam == NULL) || (fam->metric.num == 0)) {
+  if (fam == NULL) {
+    ERROR("plugin_dispatch_metric_family: fam == NULL");
     return EINVAL;
+  }
+  if (fam->metric.num == 0) {
+    return 0;
   }
 
   /* Create a copy of the metric_family_t so we can modify the time and
