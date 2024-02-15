ba445325916df1484995ebf0eb41f3c43cfdce25
Eero Tamminen <eero.t.tamminen@intel.com>
2024-02-15 16:57:09 +0200

    [WIP] resource.c: WA for GCC -Wstringop-truncation bug
    With whole program analysis of Sysman test code, GCC complains that
    strncpy() called by sstrncpy() does not set terminating nil, although
    right on the next line sstrncpy() does that.
    
    Will be dropped before PR is merged upstream.
    
    Signed-off-by: Eero Tamminen <eero.t.tamminen@intel.com>

--- src/daemon/resource.c
+++ src/daemon/resource.c
@@ -51,7 +51,7 @@ static void otel_resource_attributes(void) {
     return;
   }
 
-  size_t tmp_sz = strlen(ra) + 1;
+  size_t tmp_sz = strlen(ra) + 2;
   char tmp[tmp_sz];
   sstrncpy(tmp, ra, sizeof(tmp));
 
